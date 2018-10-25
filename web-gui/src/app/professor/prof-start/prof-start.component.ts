36c4ad4f07cff449010e10da267b9065505e5f81
Philipp Bykow <philipp.bykow@mni.thm.de>
2018-10-25 20:31:01 +0200

    Professor site with placeholder

---
+++ web-gui/src/app/professor/prof-start/prof-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prof-start',
+  templateUrl: './prof-start.component.html',
+  styleUrls: ['./prof-start.component.scss']
+})
+export class ProfStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
