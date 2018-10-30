cbd065a6872777b3d429deaca9a36a22aeddcb4b
Philipp Bykow <philipp.bykow@mni.thm.de>
2018-10-30 12:36:03 +0100

    Service for database

--- web-gui/src/app/app.module.ts
+++ web-gui/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {LoginComponent} from './login/login.component';
 import {MaterialComponentsModule} from "./material-components/material-components.module";
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import {FormsModule} from "@angular/forms";
     LayoutModule,
     AppRoutingModule,
     MaterialComponentsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
