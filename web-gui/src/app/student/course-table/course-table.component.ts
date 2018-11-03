d2b2052480dbc392c93e953e28d5a8d4a4c6e5f2
Philipp Bykow <philipp.bykow@mni.thm.de>
2018-11-03 18:14:23 +0100

    Table with all courses

---
+++ web-gui/src/app/student/course-table/course-table.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { CourseTableDataSource } from './course-table-datasource';
+
+@Component({
+  selector: 'app-course-table',
+  templateUrl: './course-table.component.html',
+  styleUrls: ['./course-table.component.scss'],
+})
+export class CourseTableComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: CourseTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['name', 'id'];
+
+  ngOnInit() {
+    this.dataSource = new CourseTableDataSource(this.paginator, this.sort);
+  }
+}
