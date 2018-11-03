f7643a352028179236ec11c8d3b63cee67e2a750
Philipp Bykow <philipp.bykow@mni.thm.de>
2018-11-03 21:28:12 +0100

    Generates different students on login

--- web-gui/src/app/student/student-start/student-start.component.ts
+++ web-gui/src/app/student/student-start/student-start.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MatSnackBar} from "@angular/material";
+import {Student} from "../Student";
 
 @Component({
   selector: 'app-student-start',
@@ -8,10 +9,25 @@ import {MatSnackBar} from "@angular/material";
 })
 export class StudentStartComponent implements OnInit {
 
+  randomStudent: Student[];
+  student: Student;
+
   constructor(private snackbar: MatSnackBar) {
   }
 
   ngOnInit() {
+
+    // Generating Random Students to simulate different login
+    this.randomStudent = [
+      {fName: "Lukas", sName: "Boehm", mNumber: 37465486},
+      {fName: "Manuela", sName: "Kohl", mNumber: 4390584},
+      {fName: "Jürgen", sName: "Fisher", mNumber: 5342545},
+      {fName: "Christin", sName: "Kuhn", mNumber: 7564757},
+      {fName: "Michelle", sName: "Eberhardt", mNumber: 1209384},
+      {fName: "Mathias", sName: "Strauss", mNumber: 198172},
+      {fName: "Bernd", sName: "Schreiber", mNumber: 8927314},
+    ];
+    this.student = this.randomStudent[Math.floor(Math.random() * this.randomStudent.length)];
   }
 
 
