e3cbb6034125abb53e959dcdb883fe5bfe2a6e8c
Philipp Bykow <philipp.bykow@mni.thm.de>
2018-11-03 21:18:06 +0100

    Routes for searching and showing course

--- web-gui/src/app/student/student-routing.module.ts
+++ web-gui/src/app/student/student-routing.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {StudentStartComponent} from "./student-start/student-start.component";
+import {StudentSearchComponent} from "./student-search/student-search.component";
+import {StudentCourseComponent} from "./student-course/student-course.component";
 
 const routes: Routes = [
   {path: '', component: StudentStartComponent},
-  {path: 'test', component: StudentStartComponent, outlet: 'content'}
+  {path: 'search', component: StudentSearchComponent},
+  {path: 'course/:id', component: StudentCourseComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class StudentRoutingModule { }
+export class StudentRoutingModule {
+}
