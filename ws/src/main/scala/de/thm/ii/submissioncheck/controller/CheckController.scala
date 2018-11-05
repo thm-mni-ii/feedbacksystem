30ab94e909d42c8b10745762e465bfec0230dd45
Allan Karlson <beesforever@gmx.de>
2018-11-05 14:53:32 +0100

    Add Check Controller

---
+++ ws/src/main/scala/de/thm/ii/submissioncheck/controller/CheckController.scala
@@ -0,0 +1,53 @@
+package de.thm.ii.submissioncheck.controller
+
+import org.springframework.web.bind.annotation._
+import de.thm.ii.submissioncheck.services.{ClientService, UserService}
+import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.beans.factory.annotation.{Autowired, Value}
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
+
+
+/**
+  * CheckController
+  *
+  * @author Benjamin Manns
+  */
+@RestController
+@RequestMapping(path = Array("/api/v1"))
+class CheckController {
+
+  private val logger: Logger = LoggerFactory.getLogger(classOf[ClientService])
+
+  @Autowired
+  private var kafkaTemplate: KafkaTemplate[String, String] = null
+
+
+  private var topicName: String = "check_request"
+
+
+
+  /**
+    * sendCheck protoype
+    * @param data Users Input
+    * @return String
+    */
+  @RequestMapping(value = Array("/check"), method = Array(RequestMethod.POST))
+  def sendCheck(data: String): String = {
+
+    kafkaTemplate.send(topicName, "ncjidlbwafudbuwia!")
+    kafkaTemplate.flush()
+    "Check was send"
+  }
+
+  /**
+    * Listen on "check_answer"
+    * @param msg Answer from service
+    */
+  @KafkaListener(topics = Array("check_answer"))
+  def listener(msg: String): Unit = {
+    logger.warn("Get: " + msg)
+  }
+
+
+}
