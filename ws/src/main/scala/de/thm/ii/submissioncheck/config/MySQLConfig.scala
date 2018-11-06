5f92dadc34534620d09ef756df0c46337dd946fc
Allan Karlson <beesforever@gmx.de>
2018-11-06 11:48:17 +0100

    #22: resolve Travis Errors

--- ws/src/main/scala/de/thm/ii/submissioncheck/config/MySQLConfig.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/config/MySQLConfig.scala
@@ -2,18 +2,27 @@ package de.thm.ii.submissioncheck.config
 
 import java.sql.{Connection, DriverManager, Statement}
 
+/**
+  * MySQLConfig connects to MySQL 8 DB
+  */
 class MySQLConfig {
 
   // TODO Load DB from Input
+  /** MySQL 8 URL */
   val url = "jdbc:mysql://localhost:3308/db1"
 
-  // Since MySQL 8 we need another driver
+  /** Since MySQL 8 we need another driver */
   val driver = "com.mysql.cj.jdbc.Driver"
 
   // TODO Load from config file...
+  /** DB Username */
   val username = "root"
+
+  //TODO Load Password from config file
+  /** DB Password */
   val password = "example"
 
+  /** MySQL connection reference */
   var connection: Connection = _
 
   try {
@@ -23,6 +32,10 @@ class MySQLConfig {
     case e: Exception => e.printStackTrace
   }
 
-  def getConnector = connection
+  /**
+    * getter Method for MySQL DB
+    * @return MySQL 8 Connector
+    */
+  def getConnector:Connection = connection
 
 }
