8909510d3c4834c8fc56c91de54f94e18a01b798
Allan Karlson <beesforever@gmx.de>
2018-11-06 10:50:10 +0100

    #35: resolve scala doc checker error

--- secrettoken-checker/src/main/scala/de/thm/ii/submissioncheck/BashExec.scala
+++ secrettoken-checker/src/main/scala/de/thm/ii/submissioncheck/BashExec.scala
@@ -7,24 +7,37 @@ import scala.sys.process._
   *
   * @author Vlad Sokyrskyy
   *
-  * @param scriptpathc
-  * @param tokenc
+  * @param scriptpathc path of shell script
+  * @param tokenc shell script parameter
   */
 class BashExec(val scriptpathc: String, val tokenc: String) {
 
+  /**
+    * Class instance scriptpath
+    */
   var scriptpath = scriptpathc
+
+  /**
+    * Class instance tokenc
+    */
   var token = tokenc
+
+  /**
+    * Class instance output
+    */
   var output = ""
 
-
+  /**
+    * exec()
+    * @return exit code
+    */
   def exec() : Int = {
 
     output = Process("bash", Seq(scriptpath, token)).!!
     val exitCode = Process("bash", Seq(scriptpath, token)).!
 
-    return exitCode
+    exitCode
     //return 0
   }
-
 
 }
