2be4dd2be7b1dcfd838a8a1ee15d32d8d011b485
vlad-sokyrsky <43994940+vlad-sokyrsky@users.noreply.github.com>
2018-11-08 21:34:08 +0100

    added script for comparing a name with an md5 hash

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Here we take the name of the user and a token which should be the md5 hash of the name
+# then we compare the the generated md5 hash with the token and exit 0 if identical and 1 if not
+# with appropriate messages
+
+name=$1
+token="$2 $3"
+
+md5name=$(echo -n ${name} | md5sum | xargs)
+
+
+if [ "$token" == "$md5name" ] 
+then
+echo correct: token and md5 hash are identical
+exit 0
+else
+echo fault: token and md5 hash are not identical
+exit 1
+fi
