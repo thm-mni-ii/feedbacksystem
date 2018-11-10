a1219a3d0337940ff43452a59064fa9872b467f2
Philipp Bykow <philippb.3run@gmail.com>
2018-11-10 19:48:07 +0100

    Placeholder for lecture tasks

--- web-gui/src/app/student/student-course/student-course.component.ts
+++ web-gui/src/app/student/student-course/student-course.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 /**
  * Component for showing a specific course and his tasks.
@@ -9,11 +11,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './student-course.component.html',
   styleUrls: ['./student-course.component.scss']
 })
-export class StudentCourseComponent implements OnInit {
+export class StudentCourseComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+  }
+
+  private sub: Subscription;
+  id: number;
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    })
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 
 }
