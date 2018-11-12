52c841344929cfc8c4c1090b6afd45d4e50db020
Philipp Bykow <philippb.3run@gmail.com>
2018-11-12 15:22:24 +0100

    Added guard in routes

--- web-gui/src/app/app-routing.module.ts
+++ web-gui/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'user', loadChildren: './student/student.module#StudentModule'},
-  {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
-  {path: 'prof', loadChildren: './professor/professor.module#ProfessorModule'}
+  {path: 'user', loadChildren: './student/student.module#StudentModule', canActivate: [AuthGuard]},
+  {path: 'admin', loadChildren: './admin/admin.module#AdminModule', canActivate: [AuthGuard]},
+  {path: 'prof', loadChildren: './professor/professor.module#ProfessorModule', canActivate: [AuthGuard]}
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes,{enableTracing: true})
+    RouterModule.forRoot(routes, {enableTracing: false})
   ],
   exports: [
     RouterModule
