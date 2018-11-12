2e4c27d0b5890e5955a14107c17f4cea04f5ed87
Philipp Bykow <philippb.3run@gmail.com>
2018-11-12 10:50:10 +0100

    Placeholder login

--- web-gui/src/app/service/auth.service.ts
+++ web-gui/src/app/service/auth.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
+/**
+ * Service that manages login and logout for an user
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +14,12 @@ export class AuthService {
 
 
   login(username: string, password: string) {
-    //TODO: Implement login
+    this.http.post("/api/v1/login", {},
+      {params: new HttpParams().append("username", username).append("password", password)}).subscribe(data => {
+
+        //TODO: Save usertoken
+        console.log(data);
+    })
   }
 
 
