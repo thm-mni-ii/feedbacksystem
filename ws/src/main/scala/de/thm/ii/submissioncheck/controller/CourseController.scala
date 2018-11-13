597a1830e49bad6660e71b43ada24bfa500699a4
Allan Karlson <beesforever@gmx.de>
2018-11-13 15:55:22 +0100

    #45: grant a user to edit a course

--- ws/src/main/scala/de/thm/ii/submissioncheck/controller/CourseController.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/controller/CourseController.scala
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation._
 
 @RestController
-@RequestMapping(path = Array("/api/v1/courses"))
+@RequestMapping(path = Array("/api/v1/courses/"))
 class CourseController {
 
   /** Class field to perform JWT Auth*/
@@ -66,9 +66,9 @@ class CourseController {
     * @param request Request Header containing Headers
     * @return JSON
     */
-  @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.GET), consumes = Array("application/json"))
+  @RequestMapping(value = Array("{id}"), method = Array(RequestMethod.GET), consumes = Array("application/json"))
   @ResponseBody
-  def getCourse(@PathVariable("id") courseid: String, request:HttpServletRequest): util.Map[String, String] = {
+  def getCourse(@PathVariable("id") courseid: Integer, request:HttpServletRequest): util.Map[String, String] = {
     // If admin -> all, if prof -->
     val user:User = userService.verfiyUserByHeaderToken(request)
     if(user == null)
@@ -78,4 +78,43 @@ class CourseController {
     courseService.getCourseDetailes(courseid, user)
   }
 
+  /**
+    * grantCourse allows course creators and editors to grant further rights to other user
+    * @param courseid unique identification for a course
+    * @param request Request Header containing Headers
+    * @param jsonNode contains JSON request
+    * @return JSON
+    */
+  @RequestMapping(value = Array("{id}/grant"), method = Array(RequestMethod.POST), consumes = Array("application/json"))
+  @ResponseBody
+  def grantCourse(@PathVariable("id") courseid: Integer, request:HttpServletRequest, @RequestBody jsonNode:JsonNode): util.Map[String, Boolean] = {
+    try{
+      var username = jsonNode.get("username").asText()
+      var grant_type = jsonNode.get("grant_type").asText()
+
+      val user:User = userService.verfiyUserByHeaderToken(request)
+      if(user == null)
+      {
+        throw new UnauthorizedException
+      }
+      if(!this.courseService.isPermittedForCourse(courseid,user))
+      {
+        throw new UnauthorizedException
+      }
+      else{
+        val userToGrant:User = userService.loadUserFromDB(username)
+        if(userToGrant == null){
+          throw new BadRequestException("Please provid a valid username")
+        }
+        else{
+          courseService.grandUserToACourse(grant_type, courseid, userToGrant)
+        }
+      }
+    }
+    catch {
+      case e: NullPointerException => {
+        throw new BadRequestException("Please provide: username, grant_type")
+      }
+    }
+  }
 }
