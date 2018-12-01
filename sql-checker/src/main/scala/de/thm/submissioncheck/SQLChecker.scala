78be0cdbe709c8ed9b72d58457749aee0a256ae0
vlad-sokyrsky <43994940+vlad-sokyrsky@users.noreply.github.com>
2018-12-01 23:13:20 +0100

    style checked

--- sql-checker/src/main/scala/de/thm/submissioncheck/SQLChecker.scala
+++ sql-checker/src/main/scala/de/thm/submissioncheck/SQLChecker.scala
@@ -2,7 +2,6 @@ package de.thm.submissioncheck
 
 /**
   * @author Vlad Sokyrsky
+  * The Communication with Kafka and the databases will be added here
   */
-object SQLChecker extends App{
- //The Communication with Kafka and the databases will be added here 
-}
+object SQLChecker extends App
