36e9b1879c9ef08734f5537872e0cd045696ff7b
Allan Karlson <beesforever@gmx.de>
2018-12-01 08:59:06 +0100

    #61: Add a testsystem Controller

--- ws/src/main/scala/de/thm/ii/submissioncheck/model/User.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/model/User.scala
@@ -7,8 +7,9 @@ package de.thm.ii.submissioncheck.model
   * @param userid local DB's userid
   * @param username User's username
   * @param role User's role name
+  * @param roleid User's role id
   */
-class User(val userid: Int, val username: String, val role: String) {
+class User(val userid: Int, val username: String, val role: String, val roleid: Int) {
   /**
     * Return User as Map. Simply answer in HTTPResonses
     *
@@ -16,6 +17,6 @@ class User(val userid: Int, val username: String, val role: String) {
     * @return Map / JSON of User Data
     */
   def asMap(): Map[String, String] = {
-    Map("userid" -> this.userid.toString, "username" -> this.username, "role" -> this.role)
+    Map("userid" -> this.userid.toString, "username" -> this.username, "role" -> this.role, "roleid" -> this.roleid)
   }
 }
