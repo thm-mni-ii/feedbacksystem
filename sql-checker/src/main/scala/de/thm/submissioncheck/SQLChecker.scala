ae8e8335732eefbb77cfbef198641aa72c598ade
vlad-sokyrsky <43994940+vlad-sokyrsky@users.noreply.github.com>
2018-12-01 22:58:21 +0100

    Update SQLChecker.scala

--- sql-checker/src/main/scala/de/thm/submissioncheck/SQLChecker.scala
+++ sql-checker/src/main/scala/de/thm/submissioncheck/SQLChecker.scala
@@ -4,21 +4,5 @@ package de.thm.submissioncheck
   * @author Vlad Sokyrsky
   */
 object SQLChecker extends App{
-  /*
-  var s = new TaskQuery("ok", "select *\nfrom hotel")
-  var sx = Array(s)
-  var task1: SQLTask = new SQLTask("sql-checker1", sx, "")
-*/
-  /**
-    * Class instance dbid
-    */
-  var dbid: Int = 0
-
-  /**
-    * create a new id for a database
-    * @return dbid
-    */
-  def newID(): Int = {
-    dbid + 1
-  }
+ //The Communication with Kafka and the databases will be added here 
 }
