9c257844ecf5ed904a66e4499951e97acb9b4cda
Allan Karlson <beesforever@gmx.de>
2018-12-02 13:48:06 +0100

    #61: First finish about testsystem registration, deletion, ...as a REST interface

--- ws/src/main/scala/de/thm/ii/submissioncheck/services/TestsystemService.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/services/TestsystemService.scala
@@ -10,7 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
 
 /**
-  * CourseService provides interaction with DB
+  * TestsystemService provides interaction with DB table testsystem
   *
   * @author Benjamin Manns
   */
@@ -19,18 +19,20 @@ class TestsystemService {
   @Autowired
   private implicit val jdbc: JdbcTemplate = null
 
-  object TestsystemLabels {
-    val id = "testsystem_id"
-    val name = "name"
-    val description = "description"
-    val supported_formats = "supported_formats"
-    val machine_port = "machine_port"
-    val machine_ip = "machine_ip"
-  }
-
-  def insertTestsystem(id_string: String, name: String, description: String, supportedFormats: String, machinePort: Int, machineIp: String): Map[String, String] = {
-    var parsedIDString = id_string.toLowerCase.replace(" ","")
-
+  /**
+    * create and insert a testsystem information
+    * @author Benjamin Manns
+    * @param id_string unique identification for a testsystem
+    * @param name testsystem name
+    * @param description testsystem description
+    * @param supportedFormats which format does a testystem provide
+    * @param machinePort port of docker instance
+    * @param machineIp ip of docker instance
+    * @return inserted primary key
+    */
+  def insertTestsystem(id_string: String, name: String, description: String, supportedFormats: String, machinePort: Int,
+                       machineIp: String): Map[String, String] = {
+    val parsedIDString = id_string.toLowerCase.replace(" ", "")
     try{
       var num = DB.update(
         "insert into testsystem (testsystem_id, name, description, supported_formats, machine_port, machine_ip) values (?,?,?,?,?,?)",
@@ -39,20 +41,57 @@ class TestsystemService {
       Map(TestsystemLabels.id -> parsedIDString)
     }
     catch {
-      case _: SQLException => throw new BadRequestException("Provided testsystem name may not be unique.")
+      case _: Exception => throw new BadRequestException("Provided testsystem name may not be unique.")
     }
-
   }
 
-  def updateTestsystem(id_string: String, name: String, description: String, supportedFormats: String, machinePort: Int, mashineIp: String): Boolean = {
-    DB.update("update testsystem set name = ?, description = ?, supported_formats = ?, machine_port = ?, machine_ip = ? where testsystem_id = ?",
-      name, description, supportedFormats, machinePort, mashineIp, id_string) == 1
+  /**
+    * update testsystem information
+    * @author Benjamin Manns
+    * @param id_string unique identification for a testsystem
+    * @param name testsystem name
+    * @param description testsystem description
+    * @param supportedFormats which format does a testystem provide
+    * @param machine_port port of docker instance
+    * @param machine_ip ip of docker instance
+    * @return if update worked out
+    */
+  def updateTestsystem(id_string: String, name: String, description: String, supportedFormats: String, machine_port: Int, machine_ip: String): Boolean = {
+    var ok = true
+    if (name != null) {
+      val num = DB.update("update testsystem set name = ? where testsystem_id = ?", name, id_string)
+    }
+    if (description != null) {
+      ok = ok && (DB.update("update testsystem set description = ? where testsystem_id = ?", description, id_string) == 1)
+    }
+    if (supportedFormats != null) {
+      ok = ok && (DB.update("update testsystem set supported_formats = ? where testsystem_id = ?", supportedFormats, id_string) == 1)
+    }
+    if (machine_port > 0) {
+      ok = ok && (DB.update("update testsystem set machine_port = ? where testsystem_id = ?", machine_port, id_string) == 1)
+    }
+    if (machine_ip != null) {
+      ok = ok && (DB.update("update testsystem set machine_ip = ? where testsystem_id = ?", machine_ip, id_string) == 1)
+    }
+    ok
   }
 
+  /**
+    * delete a testsystem
+    * @author Benjamin Manns
+    * @param id_string unique identification for a testsystem
+    * @return if it worked out
+    */
   def deleteTestsystem(id_string: String): Boolean = {
     DB.update("delete from testsystem  where testsystem_id = ?", id_string) == 1
   }
 
+  /**
+    * get information about one testsystem
+    * @author Benjamin Manns
+    * @param id_string unique identification for a testsystem
+    * @return Scala Map
+    */
   def getTestsystem(id_string: String): Option[Map[String, String]] = {
     val list = DB.query("select * from testsystem  where testsystem_id = ?", (res, _) => {
       Map(TestsystemLabels.id -> res.getString(TestsystemLabels.id),
@@ -63,9 +102,13 @@ class TestsystemService {
         TestsystemLabels.machine_port -> res.getString(TestsystemLabels.machine_port))
     }, id_string)
     list.headOption
-
   }
 
+  /**
+    * get all testsystems
+    * @author Benjamin Manns
+    * @return List of Scala Maps
+    */
   def getTestsystems(): List[Map[String, String]] = {
     DB.query("select * from testsystem", (res, _) => {
       Map(TestsystemLabels.id -> res.getString(TestsystemLabels.id),
@@ -76,5 +119,4 @@ class TestsystemService {
         TestsystemLabels.machine_port -> res.getString(TestsystemLabels.machine_port))
     })
   }
-
 }
