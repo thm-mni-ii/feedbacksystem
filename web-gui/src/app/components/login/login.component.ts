519575bb729fdee803bcdbf77fba047a8c72eb41
Philipp Bykow <philippb.3run@gmail.com>
2018-12-02 12:18:46 +0100

    Moved files for better structural order

--- web-gui/src/app/components/login/login.component.ts
+++ web-gui/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import {AuthService} from "../service/auth.service";
+import {AuthService} from "../../service/auth.service";
 
 @Component({
   selector: 'app-login',
--- web-gui/src/app/modules/professor/prof-start/prof-start.component.ts
+++ web-gui/src/app/modules/professor/prof-start/prof-start.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MatSnackBar} from "@angular/material";
-import {AuthService} from "../../service/auth.service";
+import {AuthService} from "../../../service/auth.service";
 
 @Component({
   selector: 'app-prof-start',
