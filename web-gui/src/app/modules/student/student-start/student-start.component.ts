a4bff4b9e44999721b603d15058c195c874b56fd
Philipp Bykow <philippb.3run@gmail.com>
2018-12-07 19:54:30 +0100

    Shows username when logged in

--- web-gui/src/app/modules/student/student-start/student-start.component.ts
+++ web-gui/src/app/modules/student/student-start/student-start.component.ts
@@ -1,11 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MatSnackBar} from "@angular/material";
-import {Student} from "../Student";
 import {AuthService} from "../../../service/auth.service";
 
 /**
- * Component that shows the courses from a student.
- * This is the first component that the student sees after login.
+ * Component that shows the subscribed courses of a student.
  */
 @Component({
   selector: 'app-student-start',
@@ -14,25 +12,13 @@ import {AuthService} from "../../../service/auth.service";
 })
 export class StudentStartComponent implements OnInit {
 
-  randomStudent: Student[];
-  student: Student;
+  user: String;
 
   constructor(private snackbar: MatSnackBar, private auth: AuthService) {
   }
 
   ngOnInit() {
-
-    // Generating Random Students to simulate different login
-    this.randomStudent = [
-      {fName: "Lukas", sName: "Boehm", mNumber: 37465486},
-      {fName: "Manuela", sName: "Kohl", mNumber: 4390584},
-      {fName: "Jürgen", sName: "Fisher", mNumber: 5342545},
-      {fName: "Christin", sName: "Kuhn", mNumber: 7564757},
-      {fName: "Michelle", sName: "Eberhardt", mNumber: 1209384},
-      {fName: "Mathias", sName: "Strauss", mNumber: 198172},
-      {fName: "Bernd", sName: "Schreiber", mNumber: 8927314},
-    ];
-    this.student = this.randomStudent[Math.floor(Math.random() * this.randomStudent.length)];
+    this.user = this.auth.getUsername();
   }
 
 
