6064036a91cf645a788455fceb235630ad4c4c60
Philipp Bykow <philippb.3run@gmail.com>
2018-12-10 22:11:04 +0100

    Show course name in dialog

--- web-gui/src/app/modules/student/student-course/student-course-dialog/student-course-dialog.component.ts
+++ web-gui/src/app/modules/student/student-course/student-course-dialog/student-course-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material";
 
 @Component({
   selector: 'app-student-course-dialog',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentCourseDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
   }
