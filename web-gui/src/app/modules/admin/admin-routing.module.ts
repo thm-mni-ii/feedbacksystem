969cdfcbca0185a4567a8aba99838e28f58ac60c
Philipp Bykow <philippb.3run@gmail.com>
2018-12-30 22:08:07 +0100

    Work on admin site

--- web-gui/src/app/modules/admin/admin-routing.module.ts
+++ web-gui/src/app/modules/admin/admin-routing.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {AdminStartComponent} from "./admin-start/admin-start.component";
+import {AdminDashboardComponent} from "./admin-dashboard/admin-dashboard.component";
+import {AdminUserManagementComponent} from "./admin-user-management/admin-user-management.component";
 
 const routes: Routes = [
-  {path: '', component: AdminStartComponent}
+  {
+    path: '', component: AdminStartComponent, children: [
+      {path: 'dashboard', component: AdminDashboardComponent},
+      {path: 'user-management', component: AdminUserManagementComponent}
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {
+}
