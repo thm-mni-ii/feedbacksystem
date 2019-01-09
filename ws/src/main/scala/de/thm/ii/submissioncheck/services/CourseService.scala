8e6e17e6a10099115c4ee3c3d825f88fafd8210c
Allan Karlson <beesforever@gmx.de>
2019-01-09 22:25:50 +0100

    #134: also provide information in /courses/all

--- ws/src/main/scala/de/thm/ii/submissioncheck/services/CourseService.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/services/CourseService.scala
@@ -238,7 +238,8 @@ class CourseService {
         CourseDBLabels.course_modul_id -> res.getString(CourseDBLabels.course_modul_id),
         CourseDBLabels.course_semester -> res.getString(CourseDBLabels.course_semester),
         RoleDBLabels.role_name -> res.getString(RoleDBLabels.role_name),
-        "course_docent" -> getCourseDocent(res.getInt(CourseDBLabels.courseid)))
+        "course_docent" -> getCourseDocent(res.getInt(CourseDBLabels.courseid)),
+        "course_tutor" -> getCourseTutor(res.getInt(CourseDBLabels.courseid)))
     }, user.userid)
   }
 
