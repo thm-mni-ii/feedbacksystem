b5b5fa9b40645109b22279563aa27c3750b9ae88
Allan Karlson <beesforever@gmx.de>
2019-01-10 11:52:47 +0100

    #137: allow course update to be more flexible and rename fields to be more specific

--- ws/src/main/scala/de/thm/ii/submissioncheck/services/CourseDBLabels.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/services/CourseDBLabels.scala
@@ -13,12 +13,14 @@ object CourseDBLabels {
   val description: String = "course_description"
   /** holds label creator*/
   val creator: String = "creator"
-  /** holds label standard_task_type*/
-  val standard_task_type: String = "standard_task_type"
+  /** holds label standard_task_typ*/
+  val standard_task_typ: String = "standard_task_typ"
   /** holds label course_modul_id*/
   val course_modul_id: String = "course_modul_id"
   /** holds label course_semester*/
   val course_semester: String = "course_semester"
   /** holds label course_end_date*/
   val course_end_date: String = "course_end_date"
+  /** holds label personalised_submission*/
+  val personalised_submission: String = "personalised_submission"
 }
