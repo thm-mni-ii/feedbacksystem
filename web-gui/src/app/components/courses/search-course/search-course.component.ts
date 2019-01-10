0bbda2f8a4ef2e40f7155f97246d4fcdbe946f40
Philipp Bykow <philippb.3run@gmail.com>
2019-01-10 23:40:44 +0100

    Searching for courses

--- web-gui/src/app/components/courses/search-course/search-course.component.ts
+++ web-gui/src/app/components/courses/search-course/search-course.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DatabaseService} from '../../../service/database.service';
+import {CourseTableItem} from '../../student/student-list/course-table/course-table-datasource';
+import {FormControl} from '@angular/forms';
+import {flatMap, map, startWith} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-search-course',
@@ -7,9 +14,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchCourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: DatabaseService, private snackBar: MatSnackBar, private router: Router) {
+  }
+
+  private courses: CourseTableItem[];
+  myControl: FormControl = new FormControl('');
+  filteredOptions: Observable<CourseTableItem[]>;
+
+  cardCourses: CourseTableItem[];
+  subCourses: CourseTableItem[];
+
 
   ngOnInit() {
+    this.db.getAllCourses().pipe(
+      flatMap(courses => {
+        this.courses = courses;
+        this.filteredOptions = this.myControl.valueChanges
+          .pipe(
+            startWith<string | CourseTableItem>(''),
+            map(value => typeof value === 'string' ? value : value.course_name),
+            map(name => name ? this._filter(name) : this.courses.slice())
+          );
+        return this.filteredOptions;
+      })).subscribe(filteredCourses => {
+      this.cardCourses = filteredCourses;
+    });
+
+    this.db.getUserCourses().subscribe(subCourses => this.subCourses = subCourses);
+
+
   }
+
+  displayFn(course?: CourseTableItem): string | undefined {
+    return course ? course.course_name : undefined;
+  }
+
+  private _filter(name: string): CourseTableItem[] {
+    const filterValue = name.toLowerCase();
+
+    return this.courses.filter(option => {
+      return option.course_name.toLowerCase().indexOf(filterValue) === 0 ||
+        option.course_modul_id.toLowerCase().indexOf(filterValue) === 0;
+    });
+  }
+
+  /**
+   * Subscribe to course
+   * @param coursename Name of course
+   * @param courseID The id of course to subscribe
+   */
+  joinCourse(coursename: string, courseID: number) {
+    this.db.subscribeCourse(courseID).pipe(
+      flatMap(msg => {
+        if (msg.success) {
+          this.snackBar.open('Kurs ' + coursename + ' beigetreten', 'OK', {duration: 3000});
+        }
+        return this.db.getUserCourses();
+      })
+    ).subscribe(courses => {
+        this.subCourses = courses;
+      }
+    );
+  }
+
+
+  /**
+   * Navigates to subbed course
+   * @param courseID The course to go to
+   */
+  goToCourse(courseID: number) {
+    this.router.navigate(['courses', courseID]);
+  }
+
+  /**
+   * Check if course is already subbed
+   * @param courseID The id to check for
+   */
+  hasSubbed(courseID: number): boolean {
+    let isSub = false;
+    this.subCourses.find(subCourse => {
+      isSub = subCourse.course_id === courseID;
+      return isSub;
+    });
+    return isSub;
+  }
+
 
 }
