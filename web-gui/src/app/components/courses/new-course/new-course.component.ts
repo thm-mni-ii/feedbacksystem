376606ed4e5bbb52137dac1d131100d2e34018f8
Philipp Bykow <philippb.3run@gmail.com>
2019-01-12 12:27:23 +0100

    User data optional values

--- web-gui/src/app/components/courses/new-course/new-course.component.ts
+++ web-gui/src/app/components/courses/new-course/new-course.component.ts
@@ -4,6 +4,7 @@ import {TypesService} from '../../../service/types.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {MatSnackBar, MatStepper} from '@angular/material';
+import {TitlebarService} from "../../../service/titlebar.service";
 
 @Component({
   selector: 'app-new-course',
@@ -13,7 +14,7 @@ import {MatSnackBar, MatStepper} from '@angular/material';
 export class NewCourseComponent implements OnInit, OnDestroy {
 
   constructor(private db: DatabaseService, private types: TypesService, private _formBuilder: FormBuilder,
-              private snackBar: MatSnackBar) {
+              private snackBar: MatSnackBar, private titlebar: TitlebarService) {
   }
 
   @ViewChild('stepper') stepper: MatStepper;
@@ -25,18 +26,21 @@ export class NewCourseComponent implements OnInit, OnDestroy {
   thirdFG: FormGroup;
   fourthFG: FormGroup;
   fifthFG: FormGroup;
-  sixthFG: FormGroup;
 
   newCourseName: string;
   newCourseDescription: string;
   newCourseType: string;
   newCourseSemester: string;
   newCourseModuleID: string;
-  newCoursePrivatUserData: boolean;
+  newCoursePrivatUserData: string;
 
   taskTypes: string[];
 
   ngOnInit() {
+    this.titlebar.emitTitle('Neuen Kurs erstellen');
+    this.newCoursePrivatUserData = 'false';
+
+
     // TODO Replace with route for types
     this.taskTypes = this.types.getTypes();
 
@@ -45,55 +49,44 @@ export class NewCourseComponent implements OnInit, OnDestroy {
       firstCtrl: ['', Validators.required]
     });
     this.secondFG = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      secondCtrl: ['']
     });
     this.thirdFG = this._formBuilder.group({
       thirdCtrl: ['', Validators.required]
     });
     this.fourthFG = this._formBuilder.group({
-      fourthCtrl: ['', Validators.required]
+      fourthCtrl: ['']
     });
     this.fifthFG = this._formBuilder.group({
-      fifthCtrl: ['', Validators.required]
+      fifthCtrl: ['']
     });
-    this.sixthFG = this._formBuilder.group({
-      sixthCtrl: ['', Validators.required]
-    });
+
 
     this.subscription.add(this.firstFG.valueChanges.subscribe(
       (inputStep1: { firstCtrl: string }) => {
         this.newCourseName = inputStep1.firstCtrl;
-        return this.secondFG.valueChanges;
       }));
 
     this.subscription.add(this.secondFG.valueChanges.subscribe(
       (inputStep2: { secondCtrl: string }) => {
         this.newCourseDescription = inputStep2.secondCtrl;
-        return this.thirdFG.valueChanges;
       }));
 
     this.subscription.add(this.thirdFG.valueChanges.subscribe(
       (inputStep3: { thirdCtrl: string }) => {
         this.newCourseType = inputStep3.thirdCtrl;
-        return this.fourthFG.valueChanges;
       }));
 
     this.subscription.add(this.fourthFG.valueChanges.subscribe(
       (inputStep4: { fourthCtrl: string }) => {
         this.newCourseSemester = inputStep4.fourthCtrl;
-        return this.fifthFG.valueChanges;
       }));
 
     this.subscription.add(this.fifthFG.valueChanges.subscribe(
       (inputStep5: { fifthCtrl: string }) => {
         this.newCourseModuleID = inputStep5.fifthCtrl;
-        return this.sixthFG.valueChanges;
       }));
 
-    this.subscription.add(this.sixthFG.valueChanges.subscribe(
-      (inputStep6: { sixthCtrl: boolean }) => {
-        this.newCoursePrivatUserData = inputStep6.sixthCtrl;
-      }));
   }
 
   ngOnDestroy(): void {
@@ -101,8 +94,23 @@ export class NewCourseComponent implements OnInit, OnDestroy {
   }
 
   createCourse() {
+    if (!this.newCourseDescription) {
+      this.newCourseDescription = '';
+    }
+
+    if (!this.newCourseModuleID) {
+      this.newCourseModuleID = '';
+    }
+
+    if (!this.newCourseSemester) {
+      this.newCourseSemester = '';
+    }
+
+    let privateUserData: boolean;
+    privateUserData = this.newCoursePrivatUserData === 'true';
+
     this.db.createCourse(this.newCourseName, this.newCourseDescription, this.newCourseType, this.newCourseSemester,
-      this.newCourseModuleID, this.newCoursePrivatUserData).subscribe(() => {
+      this.newCourseModuleID, privateUserData).subscribe(() => {
       this.snackBar.open('Kurs ' + this.newCourseName + ' wurde erstellt', 'OK',
         {duration: 5000});
       this.stepper.reset();
