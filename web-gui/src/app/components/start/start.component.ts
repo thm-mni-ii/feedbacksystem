dac4418382bb99e26fd52ddd92b6d8315798c98d
Philipp Bykow <philippb.3run@gmail.com>
2019-01-12 15:14:49 +0100

    Change title to site currently on

--- web-gui/src/app/components/start/start.component.ts
+++ web-gui/src/app/components/start/start.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from '../../service/user.service';
 import {Router} from '@angular/router';
 import {AuthService} from '../../service/auth.service';
+import {TitlebarService} from '../../service/titlebar.service';
+import {Subscription} from 'rxjs';
 
 /**
  * Root component shows sidenav and titlebar
@@ -11,11 +13,15 @@ import {AuthService} from '../../service/auth.service';
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
-export class StartComponent implements OnInit {
+export class StartComponent implements OnInit, OnDestroy {
 
-  constructor(private user: UserService, private router: Router, private auth: AuthService) {
+  constructor(private user: UserService, private router: Router, private auth: AuthService,
+              private titlebar: TitlebarService) {
   }
 
+  private sub: Subscription;
+
+  title: string;
   userRole: string;
   isAdmin: boolean;
   isDocent: boolean;
@@ -33,7 +39,7 @@ export class StartComponent implements OnInit {
     switch (this.userRole) {
       case 'admin':
         this.isAdmin = true;
-        this.router.navigate(['admin', 'dashboard']);
+        // this.router.navigate(['admin', 'dashboard']);
         break;
       case 'moderator':
         this.isModerator = true;
@@ -51,11 +57,17 @@ export class StartComponent implements OnInit {
         this.isStudent = true;
         break;
     }
+
+    this.sub = this.titlebar.getTitle().subscribe(title => this.title = title);
   }
 
   logout() {
     this.auth.logout();
     this.router.navigate(['login']);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
