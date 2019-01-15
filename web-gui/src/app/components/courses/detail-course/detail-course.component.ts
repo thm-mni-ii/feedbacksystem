4b3d63cb120d3e1e60e1c4b9168a84502b130750
Philipp Bykow <philippb.3run@gmail.com>
2019-01-15 13:52:10 +0100

    Update, delete of task

--- web-gui/src/app/components/courses/detail-course/detail-course.component.ts
+++ web-gui/src/app/components/courses/detail-course/detail-course.component.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {delay, flatMap, map, mergeMap, retry, retryWhen, take} from 'rxjs/operators';
+import {delay, flatMap, retryWhen, take} from 'rxjs/operators';
 import {ActivatedRoute, Router} from '@angular/router';
 import {TitlebarService} from '../../../service/titlebar.service';
-import {CourseTask, DetailedCourseInformation, Testsystem} from '../../../interfaces/HttpInterfaces';
+import {CourseTask, DetailedCourseInformation} from '../../../interfaces/HttpInterfaces';
 import {DatabaseService} from '../../../service/database.service';
 import {MatDialog, MatSnackBar} from '@angular/material';
 import {NewtaskDialogComponent} from './newtask-dialog/newtask-dialog.component';
 import {UserService} from '../../../service/user.service';
 import {ExitCourseComponent} from './exit-course/exit-course.component';
-import {Observable, of, throwError, timer} from 'rxjs';
+import {of, throwError} from 'rxjs';
 
 @Component({
   selector: 'app-detail-course',
@@ -30,7 +30,6 @@ export class DetailCourseComponent implements OnInit {
   userRole: string;
   submissionAsFile: boolean;
 
-
   ngOnInit() {
     this.userRole = this.user.getUserRole();
 
@@ -46,22 +45,83 @@ export class DetailCourseComponent implements OnInit {
     });
   }
 
+  /**
+   * Creates a new task in course with dialog
+   * @param course The course data for dialog
+   */
   createTask(course: DetailedCourseInformation) {
     this.dialog.open(NewtaskDialogComponent, {
       height: '400px',
       width: '600px',
       data: {courseID: course.course_id}
-    }).afterClosed()
-      .subscribe(result => {
-        console.log(result);
-      });
+    }).afterClosed().pipe(
+      flatMap(value => {
+        if (value.success) {
+          this.snackbar.open('Erstellung der Aufgabe erfolgreich', 'OK', {duration: 3000});
+          return this.db.getCourseDetail(course.course_id);
+        }
+      })
+    ).subscribe(course_detail => {
+      this.courseTasks = course_detail.tasks;
+    });
+
+
   }
 
+  /**
+   * Docent updates task
+   * @param task
+   */
+  updateTask(task: CourseTask) {
+    this.dialog.open(NewtaskDialogComponent, {
+      height: '400px',
+      width: '600px',
+      data: {
+        task: task
+      }
+    }).afterClosed().pipe(
+      flatMap(value => {
+        if (value.success) {
+          this.snackbar.open('Update der Aufgabe ' + task.task_name + ' erfolgreich', 'OK', {duration: 3000});
+          return this.db.getCourseDetail(this.courseDetail.course_id);
+        }
+      })
+    ).subscribe(course_detail => {
+      this.courseTasks = course_detail.tasks;
+    });
+  }
+
+  /**
+   * Docent deletes task
+   * @param task
+   */
+  deleteTask(task: CourseTask) {
+    this.db.deleteTask(task.task_id).pipe(
+      flatMap(value => {
+        if (value.success) {
+          this.snackbar.open('Aufgabe ' + task.task_name + ' wurde gelöscht', 'OK', {duration: 3000});
+          return this.db.getCourseDetail(this.courseDetail.course_id);
+        }
+      })
+    ).subscribe(course_detail => {
+      this.courseTasks = course_detail.tasks;
+    });
+  }
+
+  /**
+   * Get file user wants to submit
+   * @param file The file user submits
+   */
   getSubmissionFile(file: File) {
     this.submissionData = file;
   }
 
 
+  /**
+   * Submission of user solution
+   * @param courseID Current course
+   * @param currentTask The current task for submission
+   */
   submission(courseID: number, currentTask: CourseTask) {
     this.db.submitTask(currentTask.task_id, this.submissionData).subscribe(res => {
       if (res.success) {
@@ -69,7 +129,6 @@ export class DetailCourseComponent implements OnInit {
 
         this.db.getTaskResult(currentTask.task_id).pipe(
           flatMap(value => {
-            console.log(value);
             if (value.passed == null || typeof value.passed === undefined) {
               return throwError('No result yet');
             }
@@ -78,9 +137,7 @@ export class DetailCourseComponent implements OnInit {
           retryWhen(errors => errors.pipe(delay(10000), take(10)))
         ).subscribe(taskResult => {
           this.processing = false;
-          const oldTask = this.courseTasks[this.courseTasks.indexOf(currentTask)];
-          console.log(oldTask);
-          console.log(taskResult);
+          this.courseTasks[this.courseTasks.indexOf(currentTask)] = taskResult;
         });
 
       }
@@ -88,6 +145,11 @@ export class DetailCourseComponent implements OnInit {
 
   }
 
+  /**
+   * Unsubscribe course
+   * @param courseName The name to show user
+   * @param courseID The id of current course
+   */
   exitCourse(courseName: string, courseID: number) {
     this.dialog.open(ExitCourseComponent, {
       data: {coursename: courseName}
@@ -106,5 +168,6 @@ export class DetailCourseComponent implements OnInit {
       }
     });
   }
+
 
 }
