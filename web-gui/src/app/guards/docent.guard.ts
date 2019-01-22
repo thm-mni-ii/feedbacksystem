5a923188352f13c88c90d0a3afbdb024a54ee0cf
Philipp Bykow <philippb.3run@gmail.com>
2019-01-22 15:01:19 +0100

    Fix Guards for security

---
+++ web-gui/src/app/guards/docent.guard.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {UserService} from '../service/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocentGuard implements CanActivate {
+
+
+  constructor(private user: UserService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.user.getUserRole() === 4;
+  }
+
+}
