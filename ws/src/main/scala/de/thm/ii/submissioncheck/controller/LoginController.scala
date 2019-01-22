d7049a20be69ba771d54e37e57261b0aa2d7e7ca
Allan Karlson <beesforever@gmx.de>
2019-01-22 12:03:18 +0100

    redirect to login page

--- ws/src/main/scala/de/thm/ii/submissioncheck/controller/LoginController.scala
+++ ws/src/main/scala/de/thm/ii/submissioncheck/controller/LoginController.scala
@@ -79,7 +79,7 @@ class LoginController extends CasClientConfigurerAdapter {
       case e: Throwable => {
         logger.error("Error: ", e)
         response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY)
-        response.setHeader("Location", CLIENT_HOST_URL)
+        response.setHeader("Location", CLIENT_HOST_URL + "/login")
         "error"
       }
     }
