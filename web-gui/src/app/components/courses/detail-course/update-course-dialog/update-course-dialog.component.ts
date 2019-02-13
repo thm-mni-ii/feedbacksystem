2a6a12a97bfe08a1409af04ed0aa448b741796bb
Philipp Bykow <philippb.3run@gmail.com>
2019-02-13 18:33:21 +0100

    Fix show which values optional

--- web-gui/src/app/components/courses/detail-course/update-course-dialog/update-course-dialog.component.ts
+++ web-gui/src/app/components/courses/detail-course/update-course-dialog/update-course-dialog.component.ts
@@ -29,7 +29,6 @@ export class UpdateCourseDialogComponent implements OnInit {
   ngOnInit() {
     this.courseDetails = this.data.data;
     this.testsystems$ = this.db.getTestsystemTypes();
-    console.log(this.courseDetails);
 
     this.name = this.courseDetails.course_name;
     this.description = this.courseDetails.course_description;
