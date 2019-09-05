c767e8a53da4ba821b67ac987a5d96333ec13012
Allan Karlson <beesforever@gmx.de>
2019-09-05 08:11:52 +0200

    #272: deployment strategy with dev versions

--- ci/docker-deploy.sh
+++ ci/docker-deploy.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
 tag=$1
+branch=$2
+
+function dockerPush(){
+    branch=$2
+    if [ "$branch" = "master" ]
+        then
+            tag=$1
+        else
+            tag="dev-"$1
+        fi
+
+     echo "tag is: "$tag
+
+
+    docker tag feedbacksystem_secrettokenchecker thmmniii/secrettokenchecker:$tag
+    docker tag feedbacksystem_ws thmmniii/ws:$tag
+    docker tag feedbacksystem_sqlchecker thmmniii/sqlchecker:$tag
+    docker tag feedbacksystem_bashenv thmmniii/bashenv:$tag
+
+    docker push thmmniii/ws:$tag
+    docker push thmmniii/secrettokenchecker:$tag
+    docker push thmmniii/sqlchecker:$tag
+    docker push thmmniii/bash:$tag
+}
+
 
 echo "START DOCKER DEPLOY"
 docker-compose build
@@ -10,26 +35,8 @@ docker login --password $DOCKER_PWD  --username $DOCKER_LOGIN
 echo "DOCKER IMAGES"
 docker images
 
-docker tag feedbacksystem_secrettokenchecker thmmniii/secrettokenchecker:$tag
-docker tag feedbacksystem_ws thmmniii/ws:$tag
-docker tag feedbacksystem_sqlchecker thmmniii/sqlchecker:$tag
-docker tag feedbacksystem_bash1 thmmniii/bash:$tag
+dockerPush $tag $branch
+dockerPush latest $branch
 
-docker push thmmniii/ws:$tag
-docker push thmmniii/secrettokenchecker:$tag
-docker push thmmniii/sqlchecker:$tag
-docker push thmmniii/bash:$tag
-
-tag=latest
-
-docker tag feedbacksystem_secrettokenchecker thmmniii/secrettokenchecker:$tag
-docker tag feedbacksystem_ws thmmniii/ws:$tag
-docker tag feedbacksystem_sqlchecker thmmniii/sqlchecker:$tag
-docker tag feedbacksystem_bash1 thmmniii/bash:$tag
-
-docker push thmmniii/ws:$tag
-docker push thmmniii/secrettokenchecker:$tag
-docker push thmmniii/sqlchecker:$tag
-docker push thmmniii/bash:$tag
 
 
