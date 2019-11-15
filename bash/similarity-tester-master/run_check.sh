d9f603667f9f5165634316535d470620397e2c6a
Allan Karlson <beesforever@gmx.de>
2019-11-15 09:47:35 +0100

    #298: use the -S option only compare new to old

--- bash/similarity-tester-master/run_check.sh
+++ bash/similarity-tester-master/run_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/sim/sim_text  -e -p -s -R /upload-dir/plagiat/new/*  /upload-dir/plagiat/old/*
+/opt/sim/sim_text  -e -p -S -s -R /upload-dir/plagiat/new/*  /upload-dir/plagiat/old/*
