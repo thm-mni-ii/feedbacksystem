605b9afaead3966c8334257d877285cc4ccb088b
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2020-04-19 17:48:46 +0200

    use application.yml

--- ws/src/main/scala/de/thm/ii/fbs/Application.scala
+++ ws/src/main/scala/de/thm/ii/fbs/Application.scala
@@ -29,9 +29,7 @@ import scala.io.{Codec, Source}
   * @author Andrej Sajenko
   */
 @SpringBootApplication
-@PropertySource(value = Array("file:/usr/local/ws/application.yml",
-  "file:${confdir}/ws/application_dev.properties",
-  "file:/usr/local/appconfig/application.properties"),
+@PropertySource(value = Array("file:/usr/local/ws/conf/application.yml"),
   ignoreResourceNotFound = true)
 class Application {
   private val logger = LoggerFactory.getLogger(this.getClass)
