74365d43849fb2890911a5dc0ad3dc7d00ed1094
Max Stephan <max.stephan@mni.thm.de>
2020-10-05 12:54:33 +0200

    master-runner: added Runner Exception

---
+++ master-runner/src/main/scala/de/thm/ii/fbs/util/RunnerException.scala
@@ -0,0 +1,7 @@
+package de.thm.ii.fbs.util
+
+/** Exception wrapper for Base Runner purpose
+  *
+  * @param message an exception message
+  */
+class RunnerException(message: String) extends RuntimeException(message)
