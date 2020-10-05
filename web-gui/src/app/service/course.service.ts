15c0eaa4d20f331968f68db860bb9c9f84422870
Kimberly Munjal <kimberly-annalena.munjal@mni.thm.de>
2020-10-05 20:18:49 +0200

    Started refactoring course-detail, added methods to course service

--- web-gui/src/app/service/course.service.ts
+++ web-gui/src/app/service/course.service.ts
@@ -11,6 +11,10 @@ import {Course} from "../model/Course";
 export class CourseService {
   constructor() { }
 
+  getCourse(cid: number): Observable<Course> {
+    return of(COURSE.pop())
+  }
+
   getCourseList(): Observable<Course[]> {
     return of(COURSE)
   }
