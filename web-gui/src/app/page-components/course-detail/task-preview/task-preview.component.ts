e2b9b55f7d697f5070e566ec298e79528a2a6994
Kimberly Munjal <kimberly-annalena.munjal@mni.thm.de>
2020-10-06 19:25:51 +0200

    refactoring of course detail nearly ready, outsourced task preview and adjusted services and html

--- web-gui/src/app/page-components/course-detail/task-preview/task-preview.component.ts
+++ web-gui/src/app/page-components/course-detail/task-preview/task-preview.component.ts
@@ -1,8 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MatTabChangeEvent} from "@angular/material/tabs";
-import {CourseTaskEvaluation} from "../../../model/HttpInterfaces";
-import {DomSanitizer} from "@angular/platform-browser";
-import {MiscService} from "../../../service/misc.service";
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
+import {AuthService} from "../../../service/auth.service";
+import {TaskService} from "../../../service/task.service";
+import {CourseService} from "../../../service/course.service";
+import {Task} from "../../../model/Task";
+import {Course} from "../../../model/Course";
+import {Submission} from "../../../model/Submission";
+import {JWTToken} from "../../../model/JWTToken";
 
 @Component({
   selector: 'app-task-preview',
@@ -10,41 +15,88 @@ import {MiscService} from "../../../service/misc.service";
   styleUrls: ['./task-preview.component.scss']
 })
 export class TaskPreviewComponent implements OnInit {
-
-  @Input() taskResults: CourseTaskEvaluation[];
-  @Input() taskPassed: string;
-
-  public taskResultList: any = [];
-
-  constructor(private sanitizer: DomSanitizer, private misc: MiscService) {
-    this.taskResultList = []
+  constructor(private route: ActivatedRoute, private dialog: MatDialog, private router: Router,
+              private taskService: TaskService, private courseService: CourseService,
+              private authService: AuthService) {
   }
 
-  parseResultString(resultstring: string): any[]{
-    if (resultstring == null) return [];
-    try {
-      let obj = JSON.parse(resultstring);
-      if (obj == null)
-        return null;
-      else
-        return obj
-    } catch (e) {
-      return [];
-    }
-  }
+  task: Task;
+  tasks: Task[];
+  course: Course;
+  submissions: Submission[];
+  token: JWTToken;
+  submissionStatus: boolean;
 
-  public isBase64(data){
-    return this.misc.isBase64(data)
-  }
-
-  getImageOfData(data){
-    return this.sanitizer.bypassSecurityTrustUrl("data:Image/*;base64,"+data);
-  }
 
   ngOnInit() {
+    this.submissionStatus = null;
+
+    this.route.params.subscribe(
+      params => {
+        let courseID = +params['id'];
+        let taskID = params.taskid;
+        this.loadInformation(courseID, taskID);
+      }, error => this.router.navigate(['404'])
+    );
+    if (this.submissions != null) {
+      this.submissionStatus = this.getStatus();
+    }
+    this.taskService.getAllTasks(this.course.id).subscribe(
+      tasks => {
+        this.tasks = tasks;
+      }
+    )
   }
 
-  tabChanged(event: MatTabChangeEvent) {
-
+  /**
+   * function that gets initial Information from services
+   * @param cid CourseID
+   * @param tid TaskID
+   */
+  private loadInformation(cid: number, tid: number) {
+    this.taskService.getTask(cid, tid).subscribe(
+      task => {
+        if (Object.keys(task).length == 0) {
+          this.router.navigate(['404']);
+        }
+        this.task = task;
+      }
+    );
+    this.courseService.getCourse(cid).subscribe(
+      course => {
+        if (Object.keys(course).length == 0) {
+          this.router.navigate(['404']);
+        }
+        this.course = course;
+      }
+    );
+    this.getSubmissions(cid, tid);
+    this.token = this.authService.getToken()
   }
+
+  getSubmissions(cid: number, tid: number) {
+    this.taskService.getAllSubmissions(1, cid, tid).subscribe(
+      submissions => {
+        // TODO: error handling
+        if (submissions != null) {
+          this.submissions = submissions;
+        } else this.submissions = null
+      }
+    );
+  }
+
+  // true if the user has passed the task successfully
+  private getStatus(): boolean {
+    if (this.submissions.length == 0) return null
+    for (let sub of this.submissions) {
+      if (!sub.done || sub.results.find(element => element.exitCode != 0)) return false
+    }
+    return true
+  }
+
+  /*public isAuthorized(roles: String[]) {
+    // if (roles.find(role => role == 'dozent')) return true
+    return true
+    // return this.userRole === 'docent' || this.userRole === 'admin' || this.userRole === 'moderator' || this.userRole === 'tutor';
+  }*/
 }
