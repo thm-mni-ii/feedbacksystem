f36d562a0e0ad857e92a17d479daf7b91474e476
Helene Weingaertner <helene.weingaertner@mni.thm.de>
2020-10-09 00:04:56 +0200

    Added submission service to task-preview.

--- web-gui/src/app/page-components/course-detail/task-preview/task-preview.component.ts
+++ web-gui/src/app/page-components/course-detail/task-preview/task-preview.component.ts
@@ -1,21 +1,55 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
 import {AuthService} from "../../../service/auth.service";
+import {CourseService} from "../../../service/course.service";
 import {Task} from "../../../model/Task";
+import {Submission} from "../../../model/Submission";
 import {SubmissionService} from "../../../service/submission.service";
-import {Observable, of} from "rxjs"
 
 @Component({
   selector: 'app-task-preview',
   templateUrl: './task-preview.component.html',
   styleUrls: ['./task-preview.component.scss']
 })
-export class TaskPreviewComponent {
+export class TaskPreviewComponent implements OnInit {
   @Input() courseId: number
   @Input() task: Task
 
-  constructor(private authService: AuthService, private submissionService: SubmissionService) {}
+  constructor(private route: ActivatedRoute, private dialog: MatDialog, private router: Router,
+              private submissionService: SubmissionService, private courseService: CourseService,
+              private authService: AuthService) {
+  }
 
-  submissionStatus(): Observable<boolean> {
-    return of(null)
+  submissions: Submission[];
+  submissionStatus: boolean;
+
+  ngOnInit() {
+    this.submissionStatus = null;
+    this.getSubmissions(this.courseId, this.task.id)
+    if (this.submissions != null) {
+      this.submissionStatus = this.getStatus();
+    }
+  }
+
+  getSubmissions(cid: number, tid: number) {
+    this.submissionService.getAllSubmissions(this.authService.getToken().id, cid, tid).subscribe(
+      submissions => {
+        // TODO: error handling
+        if (submissions != null) {
+          this.submissions = submissions;
+        } else this.submissions = null
+      }
+    );
+  }
+
+  // true if the user has passed the task successfully
+  private getStatus(): boolean {
+    if (this.submissions.length == 0) return null
+    for (let sub of this.submissions) {
+      if (!sub.done || sub.results.find(element => element.exitCode != 0)) return false
+    }
+    return true
   }
 }
+
