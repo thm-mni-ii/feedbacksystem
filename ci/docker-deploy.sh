9e403de1fb8c63ae221d28f04b12e3547b533c75
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2020-10-13 17:28:47 +0200

    clean up build

--- ci/docker-deploy.sh
+++ ci/docker-deploy.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 tag=$1
+branch=$2
 
 function dockerPush(){
     tag=$1
     echo "tag is: "$tag
 
-    docker tag feedbacksystem_secrettokenchecker thmmniii/secrettokenchecker:$tag
-    docker tag feedbacksystem_ws thmmniii/ws:$tag
-    docker tag feedbacksystem_sqlchecker thmmniii/sqlchecker:$tag
+    docker tag feedbacksystem_master-runner thmmniii/master-runner:$tag
+    docker tag feedbacksystem_http thmmniii/http:$tag
     docker tag feedbacksystem_bashenv thmmniii/bashenv:$tag
-    docker tag feedbacksystem_nodeenv thmmniii/nodeenv:$tag
 
-    docker push thmmniii/ws:$tag
-    docker push thmmniii/secrettokenchecker:$tag
-    docker push thmmniii/sqlchecker:$tag
+    docker push thmmniii/http:$tag
+    docker push thmmniii/master-runner:$tag
     docker push thmmniii/bashenv:$tag
-    docker push thmmniii/nodeenv:$tag
 }
 
 echo "START DOCKER DEPLOY"
@@ -27,7 +24,7 @@ docker login --password $DOCKER_PWD  --username $DOCKER_LOGIN
 echo "DOCKER IMAGES"
 docker images
 
-if [[ -z "$tag" ]]
+if [[ -z "$tag" || "dev" == "$branch" ]]
     then
       dockerPush dev-latest
     else
