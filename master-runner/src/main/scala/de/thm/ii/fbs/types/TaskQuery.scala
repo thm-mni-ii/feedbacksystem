54d33e9f6e8ba64ea81d6778cd7b0535268aa575
Max Stephan <max.stephan@mni.thm.de>
2020-10-13 15:53:47 +0200

    master-runner: added SqlRunArgs and TaskQuery

---
+++ master-runner/src/main/scala/de/thm/ii/fbs/types/TaskQuery.scala
@@ -0,0 +1,23 @@
+package de.thm.ii.fbs.types
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+/**
+  * Class that holds the Task sections
+  *
+  * @param sections the reference Queries
+  */
+class TaskQueries(@JsonProperty("sections") val sections: Array[TaskQuery])
+
+/**
+  *
+  * This class represents the Queries in an sql task
+  * and have descriptions containing information why they
+  * would be incorrect
+  *
+  * @param description description of Query (mistakes or 'ok')
+  * @param query       string of the sql query
+  * @param order       order fix or variable
+  * @author Vlad Sokyrskyy
+  */
+class TaskQuery(@JsonProperty("description") val description: String, @JsonProperty("query") var query: String, @JsonProperty("order") val order: String)
