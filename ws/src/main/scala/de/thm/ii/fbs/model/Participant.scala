f4f6cbd74ee4681f4139c5d88d6dfee4eaafd59e
simon s <simschn20@gmail.com>
2020-11-04 12:19:18 +0100

    Add: Overwrite hash and equals function for participant to fix non removed users after role change

--- ws/src/main/scala/de/thm/ii/fbs/model/Participant.scala
+++ ws/src/main/scala/de/thm/ii/fbs/model/Participant.scala
@@ -1,5 +1,7 @@
 package de.thm.ii.fbs.model
 
+import java.security.Principal
+
 import org.json.JSONObject
 
 /**
@@ -20,4 +22,20 @@ case class Participant(user: User, role: CourseRole.Value, visible: Boolean = fa
     * @return user toJson
     */
   val toJson: JSONObject = user.toJson().put("courseRole", role)
+
+  /**
+    * @return user hashcode
+    */
+  override def hashCode(): Int = user.hashCode()
+
+  /**
+    * @param other object to check equalness with
+    * @return boolean indication equalness
+    */
+  override def equals(other: Any): Boolean = other match {
+    case that: User => user.username.equals(that.username)
+    case that: Principal => user.username.equals(that.getName)
+    case that: Participant => user.username.equals(that.user.username)
+    case _ => false
+  }
 }
