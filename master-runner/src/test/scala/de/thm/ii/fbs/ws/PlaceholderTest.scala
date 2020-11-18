71b9eb23d60ddafd8645949df1c61d837cac5aa6
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2020-11-18 13:31:26 +0100

    Add coverage
    Signed-off-by: Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>

---
+++ master-runner/src/test/scala/de/thm/ii/fbs/ws/PlaceholderTest.scala
@@ -0,0 +1,16 @@
+package de.thm.ii.fbs.ws
+
+import org.junit.{Assert, Test}
+
+/**
+  * This is a placeholder test just to have a test. When there a real tests this test can be removed.
+  */
+class PlaceholderTest {
+  @Test
+  /**
+    * This is a placeholder testcase that tests that the addition of one and one equals 2
+    */
+  def placeholderTest(): Unit = {
+    Assert.assertEquals(1 + 1, 2)
+  }
+}
