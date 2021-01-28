6126888698cdb1ae5e78b2dc5088bba8ba1a0a84
Max Stephan <max.stephan@mni.thm.de>
2021-01-28 17:42:14 +0100

    [ws] added FormulaService

---
+++ ws/src/main/scala/de/thm/ii/fbs/model/ValidateFormula.scala
@@ -0,0 +1,9 @@
+package de.thm.ii.fbs.model
+
+/**
+  * Validate Formula result Object
+  *
+  * @param result the Formula check result
+  * @param msg    the result message
+  */
+class ValidateFormula(val result: Boolean, val msg: String)
