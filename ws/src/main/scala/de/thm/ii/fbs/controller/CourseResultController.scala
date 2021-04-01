b213d66a73ab15ea4db1ae500f2dd8b6b55140b6
Max Stephan <max.stephan@mni.thm.de>
2021-04-01 18:37:06 +0200

    [ws][CourseResultController] improve performance

--- ws/src/main/scala/de/thm/ii/fbs/controller/CourseResultController.scala
+++ ws/src/main/scala/de/thm/ii/fbs/controller/CourseResultController.scala
@@ -1,9 +1,11 @@
 package de.thm.ii.fbs.controller
 
 import de.thm.ii.fbs.controller.exception.ForbiddenException
-import de.thm.ii.fbs.model.{CourseResult, CourseRole, GlobalRole, TaskResult}
-import de.thm.ii.fbs.services.persistance.{CourseRegistrationService, SubmissionService, TaskService}
+import de.thm.ii.fbs.model.{CourseEvaluationResult, CourseResult, CourseRole, GlobalRole, TaskResult}
+import de.thm.ii.fbs.services.evaluation.EvaluationResultService
+import de.thm.ii.fbs.services.persistance.{CourseRegistrationService, CourseResultService, EvaluationContainerService}
 import de.thm.ii.fbs.services.security.AuthService
+
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation._
@@ -18,11 +20,10 @@ class CourseResultController {
   @Autowired
   private val authService: AuthService = null
   @Autowired
+  private val coureResultService: CourseResultService = null
+  @Autowired
   private val courseRegistration: CourseRegistrationService = null
-  @Autowired
-  private val taskService: TaskService = null
-  @Autowired
-  private val submissionService: SubmissionService = null
+
   /**
     * Returns the course results of all participants of a course.
     * @param cid Course id
@@ -39,20 +40,7 @@ class CourseResultController {
       .exists(p => p.role == CourseRole.DOCENT || p.role == CourseRole.TUTOR)
 
     if (privilegedByCourse || user.globalRole == GlobalRole.ADMIN || user.globalRole == GlobalRole.MODERATOR) {
-      val participants = courseRegistration.getParticipants(cid)
-      val tasks = taskService.getAll(cid)
-
-      participants.map(participant => {
-        val user = participant.user
-        val results = tasks.map(task => {
-          val submissions = submissionService.getAll(user.id, cid, task.id)
-          val attempts = submissions.length
-          val passed = submissions.exists(submission => submission.done && !submission.results.exists(_.exitCode != 0))
-          TaskResult(task, attempts, passed)
-        })
-        val passed = results.forall(_.passed)
-        CourseResult(user, passed, results)
-      })
+      coureResultService.getAll(cid)
     } else {
       throw new ForbiddenException()
     }
