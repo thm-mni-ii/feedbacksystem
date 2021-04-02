d6366d6268da607fb27845ed3f8f6f62ba977925
Max Stephan <max.stephan@mni.thm.de>
2021-04-02 11:11:29 +0200

    [ws][CourseResultService] Bug Fix for Tasks with Multiple Checkers

--- ws/src/main/scala/de/thm/ii/fbs/services/persistance/CourseResultService.scala
+++ ws/src/main/scala/de/thm/ii/fbs/services/persistance/CourseResultService.scala
@@ -23,16 +23,18 @@ class CourseResultService {
     * @param cid Course id
     * @return all Course Results
     */
-  def getAll(cid: Int): List[CourseResult] = DB.query("SELECT user_id, prename, surname, email, username, global_role, alias," +
-    " group_concat(CONCAT_WS(\";\", attempts, passed, task_id, name, description, deadline, media_type) ORDER BY task_id) as results, " +
-    "(count(CASE WHEN passed >= 1 THEN 1 END) = count(task_id)) as \"passed\" " +
-    "from (SELECT *, count(DISTINCT submission_id) as \"attempts\", count(CASE WHEN exit_code = 0 THEN 1 END) as \"passed\" " +
+  def getAll(cid: Int): List[CourseResult] = DB.query("SELECT user_id, prename, surname, email, username, global_role, alias, " +
+    "group_concat(CONCAT_WS(\";\", attempts, passed, task_id, name, description, deadline, media_type) ORDER BY task_id) as results, " +
+    "(count(CASE WHEN passed >= 1 THEN 1 END) = count(task_id)) as \"passed\" from (SELECT *, count(DISTINCT submission_id) as \"attempts\", " +
+    "count(CASE WHEN passed_checker >= 1 THEN 1 END) as passed " +
+    "from (SELECT *, count(CASE WHEN exit_code = 0 THEN 1 END) = count(Distinct configuration_id) as \"passed_checker\" " +
     "from user LEFT JOIN user_course using (user_id) " +
     "JOIN task using (course_id) " +
     "LEFT JOIN user_task_submission using (user_id, task_id) " +
     "LEFT join checkrunner_configuration using (task_id) " +
     "LEFT JOIN checker_result using (submission_id, configuration_id) " +
-    "where course_id = ? group by user_id, task_id order by task_id, user_id) results group by user_id;", (res, _) => parseResult(res), cid)
+    "where course_id = ? group by user_id, task_id, submission_id order by task_id, user_id) " +
+    "results_by_submission group by user_id, task_id) results  group by user_id;", (res, _) => parseResult(res), cid)
 
   private def parseResult(res: ResultSet): CourseResult = CourseResult(
     courseRegistration.parseUserResult(res), res.getBoolean("passed"), parseTasksResult(res.getString("results"))
