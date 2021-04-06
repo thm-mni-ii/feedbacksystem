bcf000db4ff5abe410e8b92301158bc0f3b94f9c
Max Stephan <max.stephan@mni.thm.de>
2021-04-06 08:27:24 +0200

    [ws][CourseResultService] improve sql query

--- ws/src/main/scala/de/thm/ii/fbs/services/persistance/CourseResultService.scala
+++ ws/src/main/scala/de/thm/ii/fbs/services/persistance/CourseResultService.scala
@@ -1,11 +1,14 @@
 package de.thm.ii.fbs.services.persistance
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import de.thm.ii.fbs.model.{CourseResult, Task, TaskResult}
 import de.thm.ii.fbs.util.DB
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
 
+import java.io.Reader
 import java.sql.ResultSet
 
 /**
@@ -17,14 +20,15 @@ class CourseResultService {
   private implicit val jdbc: JdbcTemplate = null
   @Autowired
   private val courseRegistration: CourseRegistrationService = null
-
+  private val mapper = new ObjectMapper
   /**
     * Get All Course Results
     * @param cid Course id
     * @return all Course Results
     */
   def getAll(cid: Int): List[CourseResult] = DB.query("SELECT user_id, prename, surname, email, username, global_role, alias, " +
-    "group_concat(CONCAT_WS(\"ţ\", attempts, passed, task_id, name, description, deadline, media_type) ORDER BY task_id SEPARATOR 'ŧ') as results, " +
+    "JSON_ARRAYAGG(JSON_OBJECT(\"task\", JSON_OBJECT(\"id\", task_id, \"name\", name, \"deadline\", deadline, \"mediaType\", media_type, " +
+    "\"description\", description), \"attempts\", attempts, \"passed\", passed)) as results, " +
     "(count(CASE WHEN passed >= 1 THEN 1 END) = count(task_id)) as \"passed\" from (SELECT *, count(DISTINCT submission_id) as \"attempts\", " +
     "count(CASE WHEN passed_checker >= 1 THEN 1 END) as passed " +
     "from (SELECT *, count(CASE WHEN exit_code = 0 THEN 1 END) = count(Distinct configuration_id) as \"passed_checker\" " +
@@ -36,28 +40,10 @@ class CourseResultService {
     "where course_id = ? group by user_id, task_id, submission_id order by task_id, user_id) " +
     "results_by_submission group by user_id, task_id) results  group by user_id;", (res, _) => parseResult(res), cid)
 
-  private def parseResult(res: ResultSet): CourseResult = CourseResult(
-    courseRegistration.parseUserResult(res), res.getBoolean("passed"), parseTasksResult(res.getString("results"))
-  )
-
-  private def parseTasksResult(tasks: String): List[TaskResult] = {
-    if (tasks == null) {
-      List.empty[TaskResult]
-    } else {
-      tasks.split("ŧ").map(parseTaskResult).toList
-    }
-  }
-
-  private def parseTaskResult(taskResult: String): TaskResult = {
-    val taskList = taskResult.split("ţ")
-    val task = Task(
-      id = Integer.parseInt(taskList(2)),
-      name = taskList(3),
-      description = taskList(4),
-      deadline = taskList(5),
-      mediaType = if (taskList.length > 6) taskList(6) else ""
+  private def parseResult(res: ResultSet): CourseResult = {
+    val tasks = mapper.readValue(res.getString("results"), classOf[Array[TaskResult]]).toList
+    CourseResult(
+      courseRegistration.parseUserResult(res), res.getBoolean("passed"), tasks
     )
-
-    TaskResult(task, Integer.parseInt(taskList(0)), Integer.parseInt(taskList(1)) >= 1)
   }
 }
