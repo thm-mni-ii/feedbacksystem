940594d7ff7bd3c1dc0e487c44c284751d6f9b03
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-04-09 11:44:34 +0200

    Rename package - persistance -> persistence (typo)

--- ws/src/main/scala/de/thm/ii/fbs/services/persistence/CourseResultService.scala
+++ ws/src/main/scala/de/thm/ii/fbs/services/persistence/CourseResultService.scala
@@ -18,50 +18,31 @@ class CourseResultService {
   private implicit val jdbc: JdbcTemplate = null
   @Autowired
   private val courseRegistration: CourseRegistrationService = null
-  @Autowired
-  private val objectMapper: ObjectMapper = null
+  private val mapper = new ObjectMapper
 
   /**
     * Get All Course Results
     * @param cid Course id
     * @return all Course Results
     */
-  def getAll(cid: Int): List[CourseResult] = DB.query("""
-    |select u.user_id
-    |     ,u.prename
-    |     ,u.surname
-    |     ,u.email
-    |     ,u.username
-    |     ,u.global_role
-    |     ,u.alias
-    |     ,JSON_ARRAYAGG(JSON_OBJECT("task", JSON_OBJECT("id", t.task_id,
-    |                                                    "name", t.name,
-    |                                                    "deadline", t.deadline,
-    |                                                    "mediaType", t.media_type,
-    |                                                    "description", t.description),
-    |                                                    "attempts", coalesce(submissions.attempts, 0),
-    |                                                    "passed", coalesce(submissions.passed, 0)
-    |    )) as results
-    |     ,IF(sum(passed) = count(distinct t.task_id), 1, 0) as passed
-    |from user u
-    |         left join user_course uc using (user_id)
-    |         inner join task t using (course_id)
-    |         left join (
-    |            select uts.user_id
-    |                 ,uts.task_id
-    |                 ,count(distinct uts.submission_id) as attempts
-    |                 ,max(IF(cr.exit_code = 0, 1, 0)) as passed
-    |            from user_task_submission uts
-    |                     left join checkrunner_configuration cc using (task_id)
-    |                     left join checker_result cr using (submission_id, configuration_id)
-    |            group by uts.user_id, uts.task_id
-    |) as submissions using (user_id, task_id)
-    |where course_id = ?
-    |group by u.user_id
-    |order by u.user_id, t.task_id;
-    |""".stripMargin, (res, _) => parseResult(res), cid)
+  def getAll(cid: Int): List[CourseResult] = DB.query("SELECT user_id, prename, surname, email, username, global_role, alias, " +
+    "JSON_ARRAYAGG(JSON_OBJECT(\"task\", JSON_OBJECT(\"id\", task_id, \"name\", name, \"deadline\", deadline, \"mediaType\", media_type, " +
+    "\"description\", description), \"attempts\", attempts, \"passed\", passed)) as results, " +
+    "(count(CASE WHEN passed >= 1 THEN 1 END) = count(task_id)) as \"passed\" from (SELECT *, count(DISTINCT submission_id) as \"attempts\", " +
+    "count(CASE WHEN passed_checker >= 1 THEN 1 END) as passed " +
+    "from (SELECT *, count(CASE WHEN exit_code = 0 THEN 1 END) = count(Distinct configuration_id) as \"passed_checker\" " +
+    "from user LEFT JOIN user_course using (user_id) " +
+    "JOIN task using (course_id) " +
+    "LEFT JOIN user_task_submission using (user_id, task_id) " +
+    "LEFT join checkrunner_configuration using (task_id) " +
+    "LEFT JOIN checker_result using (submission_id, configuration_id) " +
+    "where course_id = ? group by user_id, task_id, submission_id order by user_id, task_id) " +
+    "results_by_submission group by user_id, task_id) results  group by user_id;", (res, _) => parseResult(res), cid)
 
-  private def parseResult(res: ResultSet): CourseResult = CourseResult(
-    courseRegistration.parseUserResult(res), res.getBoolean("passed"), objectMapper.readValue(res.getString("results"), classOf[Array[TaskResult]]).toList
-  )
+  private def parseResult(res: ResultSet): CourseResult = {
+    val tasks = mapper.readValue(res.getString("results"), classOf[Array[TaskResult]]).toList.sortBy(f => f.task.id)
+    CourseResult(
+      courseRegistration.parseUserResult(res), res.getBoolean("passed"), tasks
+    )
+  }
 }
