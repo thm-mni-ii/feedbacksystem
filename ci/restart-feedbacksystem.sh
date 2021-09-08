84e766dc17185d6ecc4f37750318be40bceafddf
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-09-08 19:39:44 +0200

    Update restart-feedbacksystem.sh
    Only pull images that were possibly updated in this repo.

--- ci/restart-feedbacksystem.sh
+++ ci/restart-feedbacksystem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $FEEDBACKSYSTEM_DIR
-docker-compose pull
-docker-compose up -d --remove-orphans
+docker-compose pull bashenv master-runner http
+docker-compose up -d --remove-orphans bashenv master-runner http
