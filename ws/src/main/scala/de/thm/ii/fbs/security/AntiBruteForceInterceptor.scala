34a9ab940efffd4b06a11ccfa1b1af32dcef4327
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2022-02-14 10:25:35 +0100

    feat: add lock to prevent any race condition attacks

--- ws/src/main/scala/de/thm/ii/fbs/security/AntiBruteForceInterceptor.scala
+++ ws/src/main/scala/de/thm/ii/fbs/security/AntiBruteForceInterceptor.scala
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 
 import java.util.Date
-import java.util.concurrent.{ConcurrentHashMap, ConcurrentMap}
+import java.util.concurrent.locks.{Lock, ReentrantLock}
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import scala.collection.mutable
 
 @Component
 class AntiBruteForceInterceptor extends HandlerInterceptor {
@@ -20,7 +21,8 @@ class AntiBruteForceInterceptor extends HandlerInterceptor {
   private val maxAttempts: Int = 10
   @Value("${security.antiBruteForce.protectedPaths}")
   private val protectedPathsString: String = "/api/v1/login/ldap,/api/v1/login/local"
-  private val logins: ConcurrentMap[String, LoginAttempts] = new ConcurrentHashMap()
+  private val logins: mutable.Map[String, LoginAttempts] = mutable.HashMap()
+  private val lock: Lock = new ReentrantLock(true)
 
   override def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean = {
     super.preHandle(request, response, handler)
@@ -31,7 +33,8 @@ class AntiBruteForceInterceptor extends HandlerInterceptor {
       val filteredProxies = proxies.slice(0, proxies.length - trustedProxyCount)
       val ip = filteredProxies.last
 
-      var lastLoginAttempt = logins.getOrDefault(ip, LoginAttempts())
+      lock.lock()
+      var lastLoginAttempt = logins.getOrElse(ip, LoginAttempts())
       lastLoginAttempt.lastAttempt match {
         case Some(lastAttempt) =>
           if ((new Date().getTime - lastAttempt.getTime) / 1000 > interval) {
@@ -42,10 +45,12 @@ class AntiBruteForceInterceptor extends HandlerInterceptor {
 
       if (lastLoginAttempt.attempts >= maxAttempts) {
         response.sendError(429, "Too Many Requests")
+        lock.unlock()
         false
       } else {
         val currentLoginAttempt = LoginAttempts(lastLoginAttempt.attempts + 1, Some(new Date()))
         logins.put(ip, currentLoginAttempt)
+        lock.unlock()
         true
       }
     } else {
