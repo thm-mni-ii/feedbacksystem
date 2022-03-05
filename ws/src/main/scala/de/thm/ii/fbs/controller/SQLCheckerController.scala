231654412606479a79c591edfaa1e96196730f8f
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2022-03-05 11:58:01 +0100

    feat: implement sql checker routes

--- ws/src/main/scala/de/thm/ii/fbs/controller/SQLCheckerController.scala
+++ ws/src/main/scala/de/thm/ii/fbs/controller/SQLCheckerController.scala
@@ -1,11 +1,13 @@
 package de.thm.ii.fbs.controller
 
-import de.thm.ii.fbs.controller.exception.ForbiddenException
+import com.fasterxml.jackson.databind.node.{ArrayNode, ObjectNode}
+import de.thm.ii.fbs.controller.exception.{BadRequestException, ForbiddenException}
 import de.thm.ii.fbs.model.{CourseRole, GlobalRole, SQLCheckerQuery}
 import de.thm.ii.fbs.services.persistence.{CourseRegistrationService, SQLCheckerService, TaskService}
 import de.thm.ii.fbs.services.security.AuthService
+import org.json.{JSONArray, JSONObject}
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.{CrossOrigin, GetMapping, PathVariable, RequestMapping, ResponseBody, RestController}
+import org.springframework.web.bind.annotation.{CrossOrigin, GetMapping, PathVariable, RequestMapping, RequestParam, ResponseBody, RestController}
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
@@ -28,26 +30,85 @@ class SQLCheckerController {
   /**
     * Get Queries
     */
-  @GetMapping(value = Array("/{taskID}/queries"))
+  @GetMapping(value = Array("/{taskID}/queries/sumUpCorrect"))
   @ResponseBody
-  def getSubtaskStatistics(@PathVariable("taskID") taskID: Int, req: HttpServletRequest, res: HttpServletResponse): Seq[SQLCheckerQuery] = {
+  def sumUpCorrect(@PathVariable("taskID") taskID: Int, @RequestParam returns: String, req: HttpServletRequest, res: HttpServletResponse): ObjectNode = {
+    returns match {
+      case "tables" | "attributes" => {}
+      case _ => {
+        throw new BadRequestException("returns must be tables or attributes")
+      }
+    }
+
+    this.authorize(taskID, req, res)
+
+    sqlCheckerService.sumUpCorrect(taskID, returns)
+  }
+
+  /**
+    * Get Queries
+    */
+  @GetMapping(value = Array("/{taskID}/queries/sumUpCorrectCombined"))
+  @ResponseBody
+  def sumUpCorrectCombined(@PathVariable("taskID") taskID: Int, @RequestParam returns: String,
+                           req: HttpServletRequest, res: HttpServletResponse): ObjectNode = {
+    returns match {
+      case "tables" | "attributes" => {}
+      case _ => {
+        throw new BadRequestException("returns must be tables or attributes")
+      }
+    }
+
+    this.authorize(taskID, req, res)
+
+    sqlCheckerService.sumUpCorrectCombined(taskID, returns)
+  }
+
+  /**
+    * Get Queries
+    */
+  @GetMapping(value = Array("/{taskID}/queries/listByType"))
+  @ResponseBody
+  def listByType(@PathVariable("taskID") taskID: Int, @RequestParam returns: String, req: HttpServletRequest, res: HttpServletResponse): ArrayNode = {
+    returns match {
+      case "tables" | "attributes" => {}
+      case _ => {
+        throw new BadRequestException("returns must be tables or attributes")
+      }
+    }
+
+    this.authorize(taskID, req, res)
+
+    sqlCheckerService.listByType(taskID, returns)
+  }
+
+  /**
+    * Get Queries
+    */
+  @GetMapping(value = Array("/{taskID}/queries/listByTypes"))
+  @ResponseBody
+  def listByTypes(@PathVariable("taskID") taskID: Int, @RequestParam tables: Boolean, @RequestParam attributes: Boolean,
+                  req: HttpServletRequest, res: HttpServletResponse): ArrayNode = {
+    this.authorize(taskID, req, res)
+
+    sqlCheckerService.listByTypes(taskID, tables, attributes)
+  }
+
+  private def authorize(taskID: Int, req: HttpServletRequest, res: HttpServletResponse) = {
     val auth = authService.authorize(req, res)
 
     val task = taskService.getOne(taskID)
 
-    val courseID = task match {
-      case Some(task) => task.courseID
-      case _ => throw new ForbiddenException()
+    val privilegedByCourse = task match {
+      case Some(task) => courseRegistration.getParticipants(task.courseID).find(_.user.id == auth.id)
+        .exists(p => p.role == CourseRole.DOCENT || p.role == CourseRole.TUTOR)
+      case _ => false
     }
 
-    val privilegedByCourse = courseRegistration.getParticipants(courseID).find(_.user.id == auth.id)
-      .exists(p => p.role == CourseRole.DOCENT || p.role == CourseRole.TUTOR)
     val privileged = privilegedByCourse || auth.globalRole == GlobalRole.ADMIN || auth.globalRole == GlobalRole.MODERATOR
 
     if (!privileged) {
       throw new ForbiddenException()
     }
-
-    sqlCheckerService.getQueriesForTask(taskID)
   }
 }
--- ws/src/main/scala/de/thm/ii/fbs/services/persistence/SQLCheckerService.scala
+++ ws/src/main/scala/de/thm/ii/fbs/services/persistence/SQLCheckerService.scala
@@ -1,9 +1,13 @@
 package de.thm.ii.fbs.services.persistence
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.{ArrayNode, ObjectNode}
 import de.thm.ii.fbs.model.SQLCheckerQuery
+import org.json.{JSONArray, JSONObject}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria.where
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Query.query
 import org.springframework.stereotype.Component
 
@@ -11,12 +15,64 @@ import scala.jdk.javaapi.CollectionConverters
 
 @Component
 class SQLCheckerService {
+  val CollectionName = "Queries"
   @Autowired
   private val mongodbTemplate: MongoTemplate = null
+  @Autowired
+  private val mapper: ObjectMapper = null
 
-  def getQueriesForTask(taskID: Int): Seq[SQLCheckerQuery] = {
-    val queryQuery = query(where("taskNumber").is(taskID))
-    val resultList = mongodbTemplate.find(queryQuery, classOf[SQLCheckerQuery], "Queries")
-    CollectionConverters.asScala(resultList).toSeq
+  def sumUpCorrect(taskID: Int, returns: String): ObjectNode = {
+    val attributeName = returns match {
+      case "tables" => "tablesRight"
+      case "attributes" => "attributesRight"
+    }
+
+    val trueCount = mongodbTemplate.count(buildSumUpCorrectQuery(taskID, attributeName, additionalAttributeValue = true), CollectionName)
+    val falseCount = mongodbTemplate.count(buildSumUpCorrectQuery(taskID, attributeName, additionalAttributeValue = false), CollectionName)
+
+    mapper.createObjectNode().put("trueCount", trueCount).put("falseCount", falseCount)
   }
+
+  def sumUpCorrectCombined(taskID: Int, returns: String): ObjectNode = {
+    val tablesRight = returns == "tables"
+    val attributesRight = returns == "attributes"
+
+    val trueCount = mongodbTemplate.count(buildSumUpCorrectCombinedQuery(taskID, tablesRight = true, attributesRight = true), CollectionName)
+    val falseCount = mongodbTemplate.count(buildSumUpCorrectCombinedQuery(taskID, tablesRight, attributesRight), CollectionName)
+
+    mapper.createObjectNode().put("trueCount", trueCount).put("falseCount", falseCount)
+  }
+
+  def listByType(taskID: Int, returns: String): ArrayNode = {
+    val attributeName = returns match {
+      case "tables" => "tablesRight"
+      case "attributes" => "attributesRight"
+    }
+
+    val result = mongodbTemplate.find(buildSumUpCorrectQuery(taskID, attributeName, additionalAttributeValue = false), classOf[ObjectNode], CollectionName)
+
+    mapper.createArrayNode().addAll(result)
+  }
+
+  def listByTypes(taskID: Int, tables: Boolean, attributes: Boolean): ArrayNode = {
+    val result = mongodbTemplate.find(buildSumUpCorrectCombinedQuery(taskID, tables, attributes), classOf[ObjectNode], CollectionName)
+
+    mapper.createArrayNode().addAll(result)
+  }
+
+  private def buildCoreQuery(taskID: Int) =
+    new Query()
+      .addCriteria(where("taskNumber").is(taskID))
+      .addCriteria(where("queryRight").is(false))
+      .addCriteria(where("parsable").is(true))
+
+  private def buildSumUpCorrectQuery(taskID: Int, additionalAttributeName: String, additionalAttributeValue: Boolean) =
+    buildCoreQuery(taskID)
+      .addCriteria(where(additionalAttributeName).is(additionalAttributeValue))
+
+  private def buildSumUpCorrectCombinedQuery(taskID: Int, tablesRight: Boolean, attributesRight: Boolean) =
+    buildCoreQuery(taskID)
+      .addCriteria(where("tablesRight").is(tablesRight))
+      .addCriteria(where("attributesRight").is(attributesRight))
+
 }
