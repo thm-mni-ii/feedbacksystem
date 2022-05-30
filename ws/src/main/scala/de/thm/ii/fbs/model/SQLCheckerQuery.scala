e9ecbcb4ecc9a2ee3a62c6331d2858556aa97ab1
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2022-05-30 14:55:38 +0200

    fix: user java Optional type instead of scala Option type

--- ws/src/main/scala/de/thm/ii/fbs/model/SQLCheckerQuery.scala
+++ ws/src/main/scala/de/thm/ii/fbs/model/SQLCheckerQuery.scala
@@ -1,5 +1,7 @@
 package de.thm.ii.fbs.model
 
+import java.util.Optional
+
 case class SQLCheckerQuery(id: String, taskNumber: String, statement: String, queryRight: Boolean, parsable: Boolean,
-                           tablesRight: Option[Boolean], attributesRight: Option[Boolean], whereAttributesRight: Option[Boolean],
-                           stringsRight: Option[Boolean], userId: Int, attempt: Int)
+                           tablesRight: Optional[Boolean], attributesRight: Optional[Boolean], whereAttributesRight: Optional[Boolean],
+                           stringsRight: Optional[Boolean], userId: Int, attempt: Int)
