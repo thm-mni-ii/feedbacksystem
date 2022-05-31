5f0707cd58f46d3a06e0bb403d1e23b62064bfdd
Max Stephan <max.stephan@mni.thm.de>
2022-05-31 17:41:07 +0200

    [SQL Checker] Support PostgreSQL queries (#641)
    * [master-runner][gradle] added postgresql connector
    
    * [master-runner] added DBConnections
    
    * [master-runner][SQLRunner] use DBConnections
    
    * [master-runner][DBConnections] build correct db path
    
    * [master-runner][SQLRunner] close connections after run
    
    * [master-runner][DBConnections] hadle path that ends with `/`
    
    * [master-runner][SQLRunner] get db from config
    
    * [docker-compose] added psql db & config
    
    * [master-runner][config] adopt to changes
    
    * [master-runner][SQLRunnerService] fix mysql queries
    
    * [master-runner][DBConnections] not close operationCon
    
    the operationCon pool is shared between all instannces/runns and dose not have to be closed
    
    * [master-runner][DBConnections] clean up
    
    * [master-runner][SQLRunnerService] proteced -> private
    
    * [master-runner][TaskQuery] clean up
    
    * [master-runner][DBConnections] use db secific data sources
    
    * [master-runner][SQLRunnerService] use static key names
    
    * [master-runner][SQLRunnerVerticle] fix style error

---
+++ master-runner/src/main/scala/de/thm/ii/fbs/util/DBConnections.scala
@@ -0,0 +1,46 @@
+package de.thm.ii.fbs.util
+
+import io.vertx.core.json.JsonObject
+import io.vertx.scala.ext.jdbc.JDBCClient
+import io.vertx.scala.core.Vertx
+
+case class DBConnections(vertx: Vertx, defaultConfig: JsonObject) {
+  var operationCon: JDBCClient = JDBCClient.createShared(vertx, defaultConfig, defaultConfig.getString("dataSourceName"))
+  var submissionQueryCon: Option[JDBCClient] = None
+  var solutionQueryCon: Option[JDBCClient] = None
+
+  def initQuery(dbName: String, isSolution: Boolean = false): Unit = {
+    val config = defaultConfig.copy()
+    config.put("url", buildNewUrl(config.getString("url"), dbName))
+
+    if (isSolution) {
+     solutionQueryCon = Option(JDBCClient.create(vertx, config))
+    } else {
+      submissionQueryCon = Option(JDBCClient.create(vertx, config))
+    }
+  }
+
+  def close(): Unit = {
+    closeOptional(submissionQueryCon)
+    closeOptional(solutionQueryCon)
+  }
+
+  private def buildNewUrl(url: String, dbName: String) = {
+    val parts = url.split('?')
+    if (parts.length > 1) {
+      // Append / if url not ends with /
+      val dbUrl = f"${if (parts(0).endsWith("/")) "" else "/"}$dbName"
+
+      f"${parts(0)}$dbUrl?${parts(1)}"
+    } else {
+      f"$url/$dbName"
+    }
+  }
+
+  private def closeOptional(con: Option[JDBCClient]): Unit = {
+    con match {
+      case Some(c) => c.close()
+      case _ =>
+    }
+  }
+}
