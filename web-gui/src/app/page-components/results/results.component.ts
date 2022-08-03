b9cfb2d36c4063445737046702b743bb4886f4b8
hermannlallah <96632288+hermannlallah@users.noreply.github.com>
2022-08-03 18:59:52 +0200

    Tab component for Task result  (#612)
    * tab for task result
    
    * chore: remove commented code
    
    * scroll tasks
    
    * selected task updated
    
    * update tabs behaviours
    
    * [angular.json] clean up
    
    * [web-gui][results] disable animation
    
    * [web-gui][results] fix inital selection scroling
    
    * [web-gui][results] fix scroll position
    
    Co-authored-by: Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
    Co-authored-by: aimeenobou <Aingong92@gmail.com>
    Co-authored-by: Max Stephan <max.stephan@mni.thm.de>

--- web-gui/src/app/page-components/results/results.component.ts
+++ web-gui/src/app/page-components/results/results.component.ts
@@ -1,14 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy} from '@angular/core';
 import {Submission} from '../../model/Submission';
 import {MatTableDataSource} from '@angular/material/table';
 import {CheckResult} from '../../model/CheckResult';
+import {SubmissionService} from "../../service/submission.service";
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnDestroy {
   dataSource = new MatTableDataSource<CheckResult>();
   columns = ['checkerType', 'resultText', 'exitCode'];
 
@@ -23,14 +24,36 @@ export class ResultsComponent {
 
   tableViewAsGrid = false;
 
-  @Input() set submissions(subs: Submission[]) {
-    this.allSubmissions = subs;
-    this.display(subs[subs.length - 1]);
+  index: number;
+
+  @Input() set submissions(submissions: Submission[]) {
+    this.allSubmissions = submissions;
+    this.selectLast();
+    this.display(submissions[submissions.length - 1]);
   }
 
   @Input() displayTables: boolean;
 
   @Input() context: {uid: number, cid: number, tid: number};
+
+  subscription: any;
+
+  constructor(private submissionService: SubmissionService) {
+    this.subscription = this.submissionService.getFileSubmissionEmitter()
+      .subscribe(isFileSubmitted => {
+        if(isFileSubmitted)
+          this.selectLast();
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  handleSubmission(event): void {
+    const submission = this.allSubmissions.find(item => this.allSubmissions.indexOf(item) == event.index);
+    this.display(submission);
+  }
 
   display(submission: Submission) {
     if (submission === undefined) {
@@ -62,5 +85,9 @@ export class ResultsComponent {
 
   toggleTableView() {
     this.tableViewAsGrid = !this.tableViewAsGrid;
+  }
+
+  selectLast() {
+    setTimeout(() => (this.index = this.allSubmissions.length), 1);
   }
 }
