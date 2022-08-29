7bcbf5e1d1d524cb55c097c2e9a1fa97a71f8f5b
jonas <jonas.woelfer@mnd.thm.de>
2022-08-29 10:04:55 +0200

    Added SQL-Checker

---
+++ modules/fbs-sql-checker/README.md
@@ -0,0 +1,21 @@
+It is an online tutoring system for module "Datenbankensysteme" at Technische Hochschule Mittelhessen. SQL-Checker is connected to the Feedbacksystem of THM and is helping tutors and professors to correct students' answers. After a student sends an answer at Feedbacksystem, it will be sent to the SQL-Checker. If the answer was wrong, the SQL-Checker helps to find the mistake.
+
+To start SQL checker make sure that You installed Docker and python3. 
+
+First build a docker container with an SQL-Checker.
+
+<code>sudo docker build -t sql-checker</code>
+
+After the build ended succesfully start the container with following. Take care to replace "API" through an API where the JSON that should be analysed is saved.  
+
+<code>sudo docker run -e api="API" -e mongodb="localhost" sql-checker
+</code>
+
+For example, if Your API is at http://127.0.0.1:5000/answer,
+
+type 
+
+<code>sudo docker run -e api="http://127.0.0.1:5000/answer" -e mongodb="localhost" sql-checker
+</code>
+
+to start the application.
---
+++ modules/fbs-sql-checker/api/Main.py
@@ -0,0 +1,44 @@
+#Main.py
+
+import sys
+import requests
+from JSONCreator import parseSingleStatUploadDB
+from JSONCreator import TestParseSingleStatUploadDB
+import json
+import string
+import random
+
+#The following Code is for productive purposes
+
+client = sys.argv[2]
+if (len(sys.argv) < 3):
+        print("Zu wenige Argumente übergeben.")
+        print("Geben Sie als Argument die API an, von der die zu analysierende JSON aufgerufen werden soll.")
+else:
+        urlAnswer = sys.argv[1]
+        answer = requests.get(urlAnswer, verify=False)
+        print(answer.json())
+        parseSingleStatUploadDB(answer.json(), client)
+'''
+
+#The following Code is for debugging purposes
+
+client = "Your_Mongo_DB" #Define MongoDB for debugging purposes
+for x in range(1):
+  testId = (''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(8))) #Generate random ID for the MongoDB
+idJson = "{id:'%s'}" % (testId)
+print(idJson)
+testdic = {
+        "submission": "SELECT customerName, customercity, customermail, ordertotal,salestotal FROM onlinecustomers as s inner join mitarbeiter as m on m=s WHERE salesId IS NULL order by name desc",
+        "passed": False, #True if submission produced the right return in SQL-Runner
+        "resultText": "OK",
+        "userId": 1,
+        "attempt": 1,
+        "tid": 1,
+        "sid": testId,
+        "isSol": False, #True solution is from docent
+}
+
+parseSingleStatUploadDB(testdic, client)
+#TestParseSingleStatUploadDB(testdic)
+'''
