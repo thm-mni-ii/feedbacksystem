1e792863b7f08952d6be75b0a28eb0a5399d085a
Jonas Wölfer <83070806+Jonas960@users.noreply.github.com>
2022-08-30 15:37:36 +0200

    Relocate SQL-Checker from GitLab to GitHub (#729)
    * Added SQL-Checker
    
    * Added SQL-Checker
    
    * Added SQL-Checker
    
    * Added blank line and the end of file

---
+++ modules/fbs-sql-checker/api/SelAttributeChecker.py
@@ -0,0 +1,138 @@
+#SelAttributeChecker.py
+
+import ProAttributeChecker as AC
+from Parser import *
+
+selectCommands = ["sum", "count", "round", "distinct", "sec_to_time", "avg", "max", "min", "time_to_sec", "like",
+                  "between", "div", "year", "mul"]
+equals = ["eq", "neq", "gte", "gt", "lt", "lte"]
+
+literal = []
+
+# Returns SelectionAttributes as a list if there is a where
+def selectWhere(json_file):
+    listTables = []
+    if ("where" in json_file):
+        isWhere = True
+        for val1 in json_file["where"]:
+            if (isinstance(json_file["where"][val1], str)):
+                if ("." in json_file["where"][val1]):
+                    listTables.append(json_file["where"][val1].split(".")[1].lower())
+                else:
+                    listTables.append(json_file["where"][val1].lower())
+            if (isinstance(json_file["where"][val1], dict)):
+                for val2 in json_file["where"][val1]:
+                    if (isinstance(val2, str)):
+                        if ((AC.singleSelectDict(val2) != [])):
+                            listTables.extend(AC.singleSelectDict(val2))
+                        elif ("." in val2):
+                            listTables.append(val2.split(".")[1].lower())
+                        else:
+                            listTables.append(val2.lower())
+            for i in range(len(json_file["where"][val1])):
+                if (isinstance(json_file["where"][val1][i], dict)):
+                    for val3 in json_file["where"][val1][i]:
+                        if (val3 in selectCommands):
+                            for val4 in json_file["where"][val1][i][val3]:
+                                if (isinstance(val4, str)):
+                                    if ("." in val4):
+                                        listTables.append(val4.split(".")[1].lower())
+                                    else:
+                                        listTables.append(val4.lower())
+            if (isinstance(json_file["where"][val1], list) and (len(json_file["where"][val1]) > 1)):
+                if (isinstance(json_file["where"][val1][1], dict)):
+                    if (AC.singleSelectDict(json_file["where"][val1][1]) != []):
+                        listTables.extend(AC.singleSelectDict(json_file["where"][val1][1]))
+                    if ("literal" in json_file["where"][val1][1]):
+                        literal.append(json_file["where"][val1][1]["literal"])
+                for elem in json_file["where"][val1]:
+                    if (isinstance(elem, str)):
+                        if ("." in elem):
+                            listTables.append(elem.split(".")[1].lower())
+                        else:
+                            listTables.append(elem.lower())
+            for i in range(len(json_file["where"][val1])):
+                if (not isinstance(json_file["where"][val1][i], int)):
+                    for elem in json_file["where"][val1][i]:
+                        if (elem in equals):
+                            if (isinstance(json_file["where"][val1][i][elem], list)):
+                                for j in range(len(json_file["where"][val1][i][elem])):
+                                    if (isinstance(json_file["where"][val1][i][elem][j], str)):
+                                        if ("." in json_file["where"][val1][i][elem][j]):
+                                            listTables.append(
+                                                json_file["where"][val1][i][elem][j].split(".")[1])
+                                        else:
+                                            listTables.append(json_file["where"][val1][i][elem][j])
+                                    if (isinstance(json_file["where"][val1][i][elem][j], dict)):
+                                        for elem1 in json_file["where"][val1][i][elem][j]:
+                                            if (elem1 in selectCommands):
+                                                if (isinstance(json_file["where"][val1][i][elem][j][elem1], str)):
+                                                    if ("." in json_file["where"][val1][i][elem][j][elem1]):
+                                                        listTables.append(
+                                                            json_file["where"][val1][i][elem][j][elem1].split(".")[
+                                                                1])
+                                                    else:
+                                                        listTables.append(
+                                                            json_file["where"][val1][i][elem][j][elem1])
+                                            if (elem1 == "literal"):
+                                                literal.append(json_file["where"][val1][i][elem][j][elem1])
+                                        listTables.extend(AC.singleSelectDict(json_file["where"][val1][i][elem][j]))
+                        if (elem == "where"):
+                            listTables.extend(selectWhere(json_file["where"][val1][i]))
+    return set(listTables)
+
+
+# returns SelectionAttributes for a statement and uses herefor different arts of sql-statements
+def extractSelAttributes(json_file, client):
+    whereAttributes = []
+    json_file = parse_query(json_file, client)
+    try:
+        if ((selectWhere(json_file) is not None) and (selectWhere(json_file) != [])):
+            whereAttributes.extend([selectWhere(json_file)])
+    except Exception as e:
+        # print(e)
+        whereAttributes = [["Unknown"]]
+    if len(whereAttributes) > 0:
+        return list(whereAttributes[0])
+    else:
+        return list(whereAttributes)
+
+
+def extractOrderBy(json_file, client):
+    json_file = parse_query(json_file, client)
+    groupBy = []
+    groupByList = list(iterate(json_file, 'orderby'))
+    for s in groupByList:
+        value = []
+        value.append(s['value'])
+        try:
+            value.append(s['sort'])
+        except Exception as e:
+            value.append('asc')
+        groupBy.append(value)
+    if len(groupBy) == 0:
+        groupBy = "Unknown"
+    return groupBy
+
+
+def extractGroupBy(json_file, client):
+    json_file = parse_query(json_file, client)
+    groupBy = []
+    groupByList = list(iterate(json_file, 'groupby'))
+    for s in groupByList:
+        groupBy.append(s['value'])
+    if len(groupBy) == 0:
+        groupBy = "Unknown"
+    return groupBy
+
+def iterate(data, param):
+    if isinstance(data, list):
+        for item in data:
+            yield from iterate(item, param)
+    elif isinstance(data, dict):
+        for key, item in data.items():
+            if key == param:
+                yield item
+            else:
+                yield from iterate(item, param)
+
---
+++ modules/fbs-sql-checker/api/TableChecker.py
@@ -0,0 +1,133 @@
+#TableChecker.py
+
+import Formatting as f
+from Parser import *
+
+# Return a single From and a where-clause
+def isSingleFromWhere(json_file):
+    listTables = []
+    if (not (f.isString(json_file)) and (f.isFrom(json_file))):
+        if (isinstance(json_file['from'], str)):
+            if (json_file['from'].count(json_file['from']) == 1):
+                listTables.append(json_file['from'].lower())
+        if (isinstance(json_file['from'], dict)):
+            if ("value" in json_file["from"]):
+                if (isinstance(json_file["from"]["value"], str)):
+                    listTables.append(json_file['from']['value'].lower())
+    for val in json_file:
+        if (val == "where"):
+            if (isinstance(json_file[val], list)):
+                for val1 in json_file[val]:
+                    if (val1 == "in"):
+                        for i in range(len(json_file[val][val1])):
+                            listTables.extend(isSingleFromWhere(json_file[val][val1][i]))
+            elif (isinstance(json_file[val], dict)):
+                for val1 in json_file["where"]:
+                    for val2 in json_file["where"][val1]:
+                        if (val2 == "exists"):
+                            if (isinstance(json_file["where"][val1][val2], dict)):
+                                if ((isSingleFromWhere(json_file["where"][val1][val2]) != []) and (
+                                        isSingleFromWhere(json_file["where"][val1][val2]) not in listTables)):
+                                    listTables.extend(isSingleFromWhere(json_file["where"][val1][val2]))
+                    if (isinstance(json_file["where"][val1], list)):
+                        if (len(json_file["where"][val1]) > 1):
+                            if (isinstance(json_file["where"][val1][1], dict)):
+                                if ((isSingleFromWhere(val2) != []) and (isSingleFromWhere(val2) != None)):
+                                    for elem in isSingleFromWhere(val2):
+                                        if (elem not in listTables):
+                                            listTables.append(elem)
+                                elif ((isJoin(val2) != []) and (isJoin(val2) not in listTables) and (
+                                        isJoin(val2) != None)):
+                                    listTables.append(isJoin(val2))
+    if ((isJoin(json_file) != []) and (isJoin(json_file) not in listTables) and (isJoin(json_file) != None)):
+        for elem in isJoin(json_file):
+            if (elem not in listTables):
+                listTables.append(elem)
+    return (listTables)
+
+
+# Return Tables as a List if there is a join --> works for a python List too
+def isJoin(json_file):
+    listTables = []
+    listJoins = []
+    returnList = []
+    isJoinVar = False
+    joinType = ""
+    if not (f.isString(json_file)) and (f.isFrom(json_file)):
+        for val in json_file['from']:
+            if (isinstance(val, str)) and ((val != "value") and (val != "name")) and (len(val) > 1):
+                listTables.append(val.lower())
+            if isinstance(val, dict):
+                for element in val:
+                    if element == "value":
+                        listTables.append(val[element].lower())
+                    if isinstance(val[element], str) and ((element != "name") and (element != "using")):
+                        listTables.append(val[element].lower())
+                    if isJoinVar==True:
+                        for val1 in val[element]:
+                            insert = []
+                            insert.append(joinType)
+                            for val in val[element][val1]:
+                                insert.append(val)
+                            listJoins.append(insert)
+                        isJoinVar = False
+                    if element == "inner join" or element == "left join" or element == "join":
+                        isJoinVar = True
+                        joinType = element
+                        for val1 in val[element]:
+                            if (val1 == "value"):
+                                listTables.append(val[element][val1].lower())
+            if val == "value":
+                if isinstance(json_file['from']['value'], dict):
+                    if ((isSingleFromWhere(json_file['from']['value']) not in listTables) and (
+                            isSingleFromWhere(json_file['from']['value']) != None) and (
+                            isSingleFromWhere(json_file['from']['value']) != [])):
+                        listTables.extend(isSingleFromWhere(json_file['from']['value']))
+                for elem in json_file['from']["value"]:
+                    if ((isUnion(json_file['from']["value"]) != None) and (
+                            isUnion(json_file["from"]["value"]) != []) and (
+                            isUnion(json_file["from"]["value"]) not in listTables)):
+                        listTables.extend(isUnion(json_file['from']["value"]))
+                    if ((isJoin(json_file['from']["value"]) != None) and (
+                            isJoin(json_file["from"]["value"]) != []) and (
+                            isJoin(json_file["from"]["value"]) not in listTables)):
+                        listTables.extend(isJoin(json_file['from']["value"]))
+        returnList.append(sorted(list(set(listTables))))
+        returnList.append(listJoins)
+        return returnList
+
+
+# Returns tables as a List if it is a union
+def isUnion(json_file):
+    listTables = []
+    if not (f.isString(json_file)):
+        for val in json_file:
+            if val == "union":
+                for i in range(len(json_file[val])):
+                    if ((isSingleFromWhere(json_file[val][i])) is not None):
+                        listTables.extend(isSingleFromWhere(json_file[val][i]))
+                    else:
+                        listTables.append(isJoin(json_file[val][i]))
+                return sorted(set(listTables))
+            else:
+                return None
+
+
+# return tables for a statement and uses therefor different arts of sql-statements
+def extractTables(json_file, client):
+    tables = []
+    json_file = parse_query(json_file, client)
+    try:
+        if isSingleFromWhere(json_file) is not None and isSingleFromWhere(json_file) is not []:
+            tables = isSingleFromWhere(json_file)
+        elif isJoin(json_file) is not None and isJoin(json_file) is not []:
+            tables = isJoin(json_file)
+        else:
+            tables = isUnion(json_file)
+    except Exception as e:
+        tables = ["Unknown"]
+    if type(tables[0]) == str:
+        tables[0] = [tables[0]]
+    if len(tables[1]) == 0:
+        tables[1].append("Empty")
+    return tables
