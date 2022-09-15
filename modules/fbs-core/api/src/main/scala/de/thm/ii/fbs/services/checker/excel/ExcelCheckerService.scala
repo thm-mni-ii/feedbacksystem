6d38bc9187ad2d90b4b4dfea583faa27ce5aec6b
Max Stephan <max.stephan@mni.thm.de>
2022-09-15 14:24:29 +0200

    [Excel-Checker] allow to specify multiple output fields

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/checker/excel/ExcelCheckerService.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/checker/excel/ExcelCheckerService.scala
@@ -1,7 +1,7 @@
 package de.thm.ii.fbs.services.checker.excel
 
 import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
-import de.thm.ii.fbs.model.{CheckrunnerConfiguration, ExcelMediaInformation, ExcelMediaInformationTasks, ExtendedInfoExcel, User}
+import de.thm.ii.fbs.model.{CheckrunnerConfiguration, ExcelMediaInformation, ExcelMediaInformationCheck, ExcelMediaInformationTasks, ExtendedInfoExcel, ExtendedInfoExcelObject, User}
 import de.thm.ii.fbs.services.checker.`trait`.CheckerService
 import de.thm.ii.fbs.services.persistence.{CheckrunnerSubTaskService, StorageService, SubmissionService, TaskService}
 import de.thm.ii.fbs.util.ScalaObjectMapper
@@ -14,8 +14,6 @@ import java.io.File
 
 @Service
 class ExcelCheckerService extends CheckerService {
-  @Autowired
-  private val taskService: TaskService = null
   @Autowired
   private val submissionService: SubmissionService = null
   @Autowired
@@ -41,7 +39,7 @@ class ExcelCheckerService extends CheckerService {
       val submissionFile = this.getSubmissionFile(submission.id)
       val mainFile = this.getMainFile(cc.id)
 
-      val results = excelMediaInformation.tasks.map(t => this.checkTask(mainFile, submissionFile, t))
+      val results = excelMediaInformation.tasks.map(t => this.checkTasks(mainFile, submissionFile, t))
       val exitCode = if (results.forall(r => r.success)) {
         0
       } else {
@@ -58,17 +56,38 @@ class ExcelCheckerService extends CheckerService {
     }
   }
 
-  private def checkTask(submissionFile: File, mainFile: File, excelMediaInformation: ExcelMediaInformation): CheckResult = {
+  private def checkTasks(submissionFile: File, mainFile: File, excelMediaInformation: ExcelMediaInformation): CheckResult = {
     try {
-      val userRes = this.excelService.getFields(submissionFile, excelMediaInformation)
-      val expectedRes = this.excelService.getFields(mainFile, excelMediaInformation)
-
-      this.compare(userRes, expectedRes)
+      // If Config file is old -> use old field names
+      if (excelMediaInformation.checkFields.isEmpty && excelMediaInformation.outputFields.nonEmpty) {
+        this.checkTask(submissionFile, mainFile, excelMediaInformation,
+          ExcelMediaInformationCheck(range = excelMediaInformation.outputFields, hideInvalidFields = excelMediaInformation.hideInvalidFields)
+        )
+      } else {
+        excelMediaInformation.checkFields.map(c => checkTask(submissionFile, mainFile, excelMediaInformation, c)).reduce((c1, c2) => {
+          CheckResult(c1.success && c2.success,
+            c1.invalidFields.appendedAll(c2.invalidFields),
+            mergeExtendedInfo(c1.extendedInfoExcel, c2.extendedInfoExcel),
+            c1.errorMsg.appendedAll(c2.errorMsg))
+        })
+      }
     } catch {
       case e: NotImplementedFunctionException => CheckResult(errorMsg = f"Die Excel-Funktion '${e.getMessage}' wird nicht unterstützt")
       case _: NullPointerException => CheckResult(errorMsg = "Ungültige Konfiguration")
       case e: Throwable => CheckResult(errorMsg = f"Bei der Überprüfung ist ein Fehler aufgetretten: '${e.getMessage}'")
     }
+  }
+
+  private def checkTask(
+                         submissionFile: File,
+                         mainFile: File,
+                         excelMediaInformation: ExcelMediaInformation,
+                         checkFields: ExcelMediaInformationCheck
+                       ): CheckResult = {
+      val userRes = this.excelService.getFields(submissionFile, excelMediaInformation, checkFields)
+      val expectedRes = this.excelService.getFields(mainFile, excelMediaInformation, checkFields)
+
+      this.compare(userRes, expectedRes)
   }
 
   private def getSubmissionFile(submissionID: Int): File = {
@@ -153,6 +172,15 @@ class ExcelCheckerService extends CheckerService {
     val secondaryFilePath = this.storageService.pathToSecondaryFile(ccId).get.toString
     val file = new File(secondaryFilePath)
     objectMapper.readValue(file, classOf[ExcelMediaInformationTasks])
+  }
+
+  private def mergeExtendedInfo(extRes1: ExtendedInfoExcel, extRes2: ExtendedInfoExcel): ExtendedInfoExcel = {
+    ExtendedInfoExcel(expected = mergeExtendedInfoExcelObject(extRes1.expected, extRes2.expected),
+                      result = mergeExtendedInfoExcelObject(extRes1.result, extRes2.result))
+  }
+
+  private def mergeExtendedInfoExcelObject(obj1: ExtendedInfoExcelObject, obj2: ExtendedInfoExcelObject): ExtendedInfoExcelObject = {
+    ExtendedInfoExcelObject(rows = obj1.rows ++ obj2.rows, head = obj1.head ++ obj2.head)
   }
 
   case class CheckResult(success: Boolean = false,
