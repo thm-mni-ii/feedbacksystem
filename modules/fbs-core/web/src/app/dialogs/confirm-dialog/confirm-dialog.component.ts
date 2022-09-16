af5062614268a2ff7b0c251a687166549b551ae9
nicosoethe <104082549+nicosoethe@users.noreply.github.com>
2022-09-16 12:53:04 +0200

    format with prettier

--- modules/fbs-core/web/src/app/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ modules/fbs-core/web/src/app/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -8,7 +8,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 })
 export class ConfirmDialogComponent {
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string, confirmText?: string, closeText?: string },
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string;
+      message: string;
+      confirmText?: string;
+      closeText?: string;
+    },
     public dialogRef: MatDialogRef<ConfirmDialogComponent>
   ) {}
 
