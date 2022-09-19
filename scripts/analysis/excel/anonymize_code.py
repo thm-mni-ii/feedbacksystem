8a5b69735a859deaed727b0020051ad7a22a3dd9
Maximilian Richter <maxi.richter00@web.de>
2022-09-19 16:34:53 +0200

    incorporated the requested changes

--- scripts/analysis/excel/anonymize_code.py
+++ scripts/analysis/excel/anonymize_code.py
@@ -1,22 +1,19 @@
 from pathlib import Path
 from openpyxl import load_workbook
 
-i = 1
+def anonymize(path):
+    path = Path(path)
+    for i, input in enumerate(path.glob("*.xlsx*")): # rglob if directory includes subfolders and you want to access those aswell
+        wb = load_workbook(filename=input)
+        ws = wb["1 - Stammblatt"]
 
-# Directory of Excel-files, that you want to anonymize
-path = Path("Enter directory here")
-for input in list(path.glob("*.xlsx*")): # rglob if directory includes subfolders and you want to access those aswell
-    wb = load_workbook(filename=input)
-    ws = wb.active
+        # Cells which include personal data and the values they're replaced with
+        ws["B5"] = f"Benutzerkennung Anonym {i}"
+        ws["B6"] = f"Nachname Anonym {i}"
+        ws["B7"] = f"Vorname Anonym {i}"
+        ws["B8"] = f"Geburtsdatum Anonym {i}"
+        ws["B10"].hyperlink = None
+        ws["B10"] = f"E-Mail Anonym {i}"
 
-    # Cells which include personal data and the values they're replaced with
-    ws["B5"] = f"Benutzerkennung Anonym {i}"
-    ws["B6"] = f"Nachname Anonym {i}"
-    ws["B7"] = f"Vorname Anonym {i}"
-    ws["B8"] = f"Geburtsdatum Anonym {i}"
-    ws["B10"].hyperlink = None
-    ws["B10"] = f"E-Mail Anonym {i}"
-
-    i += 1
-    # Saving the changes in workbook 
-    wb.save(input)
+        # Saving the changes in workbook 
+        wb.save(input)
