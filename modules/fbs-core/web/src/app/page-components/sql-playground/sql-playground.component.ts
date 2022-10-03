e0b90857b76173b7f4fa993e788bc08788b69a28
nicosoethe <104082549+nicosoethe@users.noreply.github.com>
2022-10-03 16:51:04 +0200

    format with prettier

--- modules/fbs-core/web/src/app/page-components/sql-playground/sql-playground.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/sql-playground.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TitlebarService } from "../../service/titlebar.service";
 
 /**
