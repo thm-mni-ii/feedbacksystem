1e248dbe21a897c718ff2993d74067c1c0be1c21
Max Stephan <max.stephan@mni.thm.de>
2022-10-06 20:24:32 +0200

    [scripts][ci][migration] check insert number

--- scripts/ci/validate-migrations.sh
+++ scripts/ci/validate-migrations.sh
@@ -7,16 +7,28 @@ currentFilesCount=0
 
 for filePath in "$folderName"/*
 do
+    error=false
     fileName=${filePath##*/}
     migrationNumber=${requiredMigrationNumbers[$currentFilesCount]}
+    migrationNumberNoLeadingZero=$(expr $migrationNumber + 0)
+    migrationInsertQuery="INSERT INTO migration (number) VALUES ($migrationNumberNoLeadingZero);"
     
     # Validate File name
     if [[ $fileName != "$migrationNumber"* ]]; then
         echo "::error file=$filePath::Invalid file name '$fileName'. The file name must start with '$migrationNumber'."
-        exit 1
+        error=true
     fi
     
-    # TODO: Validate insert number
+    # Validate insert number
+    if ! grep -iq "$migrationInsertQuery" $filePath; then
+        echo "::error file=$filePath::Missing or invalid migration number is added to the database in the migration file '$fileName'. The SQL Statement '$migrationInsertQuery' must be executed."
+        error=true
+    fi
+    
+    # Exit on Error
+    if [ "$error" = true ]; then
+        exit 1
+    fi
     
     
     ((++currentFilesCount))
