5bf750ca1b0212dde30d3c28841df573fffc2e73
nicosoethe <104082549+nicosoethe@users.noreply.github.com>
2022-10-06 13:48:26 +0200

    format with prettier

--- modules/fbs-core/web/src/app/page-components/bordered-container/bordered-container.component.ts
+++ modules/fbs-core/web/src/app/page-components/bordered-container/bordered-container.component.ts
@@ -6,5 +6,5 @@ import { Component, Input } from "@angular/core";
   styleUrls: ["./bordered-container.component.scss"],
 })
 export class BorderedContainerComponent {
-    @Input() title?: string;
+  @Input() title?: string;
 }
