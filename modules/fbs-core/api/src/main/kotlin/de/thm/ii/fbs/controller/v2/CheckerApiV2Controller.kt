dd54cd2473df6a48af5223233f869b7bdf1df22f
Jonas Kuche <Zitrone44@users.noreply.github.com>
2022-10-18 11:19:12 +0200

    fix: remove `@CrossOrigin`
    Co-authored-by: Max Stephan <max.stephan@mni.thm.de>

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/controller/v2/CheckerApiV2Controller.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/controller/v2/CheckerApiV2Controller.kt
@@ -3,7 +3,6 @@ package de.thm.ii.fbs.controller.v2
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin
 @RequestMapping(path = ["/api/v2/checker"])
 class CheckerApiV2Controller {
     @GetMapping(value = ["/test"])
