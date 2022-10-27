b3e263bd5253b895f71f892ded0b76ae6c88bec7
SiarNoozai <siyer@outlook.de>
2022-10-27 13:42:41 +0200

    almost done with everything

--- modules/fbs-core/web/src/app/page-components/sql-playground/sql-playground.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/sql-playground.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, QueryList, ViewChildren } from "@angular/core";
 import { TitlebarService } from "../../service/titlebar.service";
+import { MatTabsModule, MatTabGroup, MatTab } from "@angular/material/tabs";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Label } from "ng2-charts";
+import { MatLabel } from "@angular/material/form-field";
 
 /**
  * This component is for the sql playground
@@ -11,6 +15,17 @@ import { TitlebarService } from "../../service/titlebar.service";
 })
 export class SqlPlaygroundComponent implements OnInit {
   constructor(private titlebar: TitlebarService) {}
+  @ViewChildren(MatTab, { read: MatTab })
+  public tabNodes: QueryList<MatTab>;
+  public tabs = [{name:undefined}];
+
+  closeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+
+  addTab(index: number) {
+    this.tabs.push({ name: "Query_" + index });
+  }
 
   ngOnInit() {
     this.titlebar.emitTitle("SQL Playground");
