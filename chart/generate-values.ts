24c044f79ee99c3b50828786c300b07c55e60965
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2022-10-31 18:52:54 +0100

    feat(chart): add digital classroom

--- chart/generate-values.ts
+++ chart/generate-values.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 import { stringify } from "https://deno.land/std@0.161.0/encoding/yaml.ts";
 import { encode } from "https://deno.land/std@0.161.0/encoding/hex.ts";
 
@@ -7,7 +8,7 @@ function randomHex(lenght = 16): string {
   return new TextDecoder().decode(encode(bs));
 }
 
-const values = {
+const values: any = {
   common: {
     config: {
       hostname: prompt("Enter Hostname:"),
@@ -36,6 +37,21 @@ const values = {
       postgresPassword: randomHex(),
     },
   },
+  digitalClassroom: {
+    enabled: false,
+  },
 };
+
+if (confirm("Enable digital classroom:")) {
+  values.digitalClassroom.enabled = true;
+  values.digitalClassroom.config = {
+    jwtSecret: randomHex(),
+    secret: randomHex(),
+    bbb: {
+      url: prompt("Enter BBB Url:"),
+      bbb: prompt("Enter BBB secret:"),
+    },
+  };
+}
 
 console.log(stringify(values));
