66c7e24ae92c1bf14f316a7fd5f0b4c97e497d43
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2022-10-31 16:00:59 +0100

    feat: add core

---
+++ chart/README.md
@@ -0,0 +1,8 @@
+# Feedbackssystem Helm Chart
+
+## Setup
+
+1. Prepare Kubernetes + Helm
+2. Add Repository
+3. Generate values `deno run --reload=https://raw.githubusercontent.com https://raw.githubusercontent.com/thm-mni-ii/feedbacksystem/dev/chart/generate-values.ts > values.yaml`
+4. Install `helm install -n <namepsace> --create-namespace -f values.yaml feedbacksystem feedbackssystem/feedbacksystem`
---
+++ chart/generate-values.ts
@@ -0,0 +1,29 @@
+import { stringify } from "https://deno.land/std@0.161.0/encoding/yaml.ts";
+import { encode } from "https://deno.land/std@0.161.0/encoding/hex.ts";
+
+function randomHex(lenght = 16): string {
+  const bs = new Uint8Array(lenght);
+  crypto.getRandomValues(bs);
+  return new TextDecoder().decode(encode(bs));
+}
+
+const values = {
+  common: {
+    config: {
+      hostname: prompt("Enter Hostname:"),
+    },
+  },
+  core: {
+    config: {
+      jwtSecret: randomHex(),
+    },
+  },
+  mysql: {
+    auth: {
+      password: randomHex(),
+      rootPassword: randomHex(),
+    },
+  },
+};
+
+console.log(stringify(values));
