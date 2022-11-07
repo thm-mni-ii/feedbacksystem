9b747cd6dc8407eb34cb0137b9399893da2c2f82
SiarNoozai <siyer@outlook.de>
2022-11-07 17:48:04 +0100

    most of the requirements are done

--- modules/fbs-core/web/src/app/page-components/sql-playground/sql-input-tabs/sql-input-tabs.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/sql-input-tabs/sql-input-tabs.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit, QueryList, ViewChildren } from "@angular/core";
+import { Component, QueryList, ViewChildren } from "@angular/core";
 import { MatTab } from "@angular/material/tabs";
+import { MatDialog } from "@angular/material/dialog";
+import { ConfirmDialogComponent } from "src/app/dialogs/confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: "app-sql-input-tabs",
   templateUrl: "./sql-input-tabs.component.html",
   styleUrls: ["./sql-input-tabs.component.scss"],
 })
-export class SqlInputTabsComponent implements OnInit {
-  constructor() {}
+export class SqlInputTabsComponent {
+  constructor(private dialog: MatDialog) {}
   @ViewChildren(MatTab, { read: MatTab })
   public tabNodes: QueryList<MatTab>;
   public tabs = [{ name: undefined }];
 
   closeTab(index: number) {
-    this.tabs.splice(index, 1);
+    this.openConfirmDialog(
+      "Möchtest du wirklich diesen Query schließen?",
+      "Achtung der Inhalt wird nicht gespeichert!"
+    ).subscribe((result) => {
+      if (result == true) {
+        this.tabs.splice(index, 1);
+      }
+    });
   }
 
   addTab(index: number) {
     this.tabs.push({ name: "Query_" + index });
   }
 
-  ngOnInit(): void {}
+  private openConfirmDialog(title: string, message: string) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: title,
+        message: message,
+      },
+    });
+    return dialogRef.afterClosed();
+  }
 }
