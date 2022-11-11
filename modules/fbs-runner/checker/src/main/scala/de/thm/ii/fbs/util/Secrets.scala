da5ff1ee942bfc4bff9fe2d9aec92e4d9f89f4e7
Max Stephan <max.stephan@mni.thm.de>
2022-11-11 13:54:31 +0100

    feat(Secrets): added generateHMAC

--- modules/fbs-runner/checker/src/main/scala/de/thm/ii/fbs/util/Secrets.scala
+++ modules/fbs-runner/checker/src/main/scala/de/thm/ii/fbs/util/Secrets.scala
@@ -3,7 +3,9 @@ package de.thm.ii.fbs.util
 import java.security.SecureRandom
 import java.text.SimpleDateFormat
 import java.util.Calendar
-import javax.xml.bind.DatatypeConverter
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import scala.util.Properties
 
 /**
   * Secrets holds static data used for Security operations
@@ -11,22 +13,8 @@ import javax.xml.bind.DatatypeConverter
   * @author Benjamin Manns
   */
 object Secrets {
-  /** currently constant but later dynamicall content used for SUPER_SECRET_KEY */
-  private val SUPER_SECRET_KEY_PLAIN: String = "uigbduhegafudegufqu8o3q4tgru4ieubfiel"
-
-  /** Base64 Binary Key */
-  private val SUPER_SECRET_KEY: String = DatatypeConverter.printBase64Binary(SUPER_SECRET_KEY_PLAIN.getBytes)
-
-  /**
-    * getSuperSecretKey delivers a secure key. This is only valid for a defined time
-    *
-    * @author Benjamin Manns
-    * @return Secure Key as String
-    */
-  def getSuperSecretKey: String = {
-    // TODO change key by the time
-    SUPER_SECRET_KEY
-  }
+  /** Secret used to generate the HMAC* */
+  private final val HMAC_SECRET = Properties.envOrElse("HMAC_SECRET", "uigbduhegafudegufqu8o3q4tgru4ieubfiel")
 
   /**
     * creates a random String based contain current time and random String
@@ -66,5 +54,19 @@ object Secrets {
     val buffer = Array[Byte](32)
     new SecureRandom().nextBytes(buffer)
     md.digest(buffer).map("%02x".format(_)).mkString
+  }
+
+  /**
+    * Generates a HMAC from a string
+    *
+    * @param value The Value Used to generate the HMAC
+    * @return
+    */
+  def generateHMAC(value: String): String = {
+    val secret = new SecretKeySpec(HMAC_SECRET.getBytes, "SHA256")
+    val mac = Mac.getInstance("SHA256")
+    mac.init(secret)
+    val hashString: Array[Byte] = mac.doFinal(value.getBytes)
+    new String(hashString.map(_.toChar))
   }
 }
