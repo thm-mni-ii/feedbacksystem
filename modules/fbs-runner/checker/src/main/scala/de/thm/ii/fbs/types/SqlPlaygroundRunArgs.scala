c3dbdc8d140b828b7f6cde1b7d8c1cb70ccfe876
Max Stephan <max.stephan@mni.thm.de>
2022-11-11 15:25:30 +0100

    feat(SqlPlaygroundRunArgs): ignore runner type property

--- modules/fbs-runner/checker/src/main/scala/de/thm/ii/fbs/types/SqlPlaygroundRunArgs.scala
+++ modules/fbs-runner/checker/src/main/scala/de/thm/ii/fbs/types/SqlPlaygroundRunArgs.scala
@@ -1,7 +1,8 @@
 package de.thm.ii.fbs.types
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.{JsonIgnoreProperties, JsonProperty}
 
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignore runner Type
 case class SqlPlaygroundRunArgs(@JsonProperty("executionId") executionId: Int,
                                 @JsonProperty("user") user: User,
                                 @JsonProperty("statement") statement: String,
