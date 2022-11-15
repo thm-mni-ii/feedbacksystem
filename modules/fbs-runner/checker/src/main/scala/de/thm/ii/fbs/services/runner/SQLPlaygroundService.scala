bf0ede755c99fac481b471dc9d692be326f88e6a
Max Stephan <max.stephan@mni.thm.de>
2022-11-15 09:21:10 +0100

    Extension of the SQL Checker to cover persistent databases (#947)
    * refactor(SqlRunner): move dbTypes to own object
    
    * feat(DBOperationsService): allow to not drop db while creation
    
    * feat(Secrets): added generateHMAC
    
    * feat(runner): added SQLPlaygroundVerticle
    
    * feat(SqlPlaygroundRunArgs): fix json annotation
    
    * feat(SQLPlayground Database): add json annotation
    
    * feat(ExtendedResultService): make buildTableJson public
    
    * feat(SqlPlaygroundRunArgs): ignore runner type property
    
    * feat(SqlPlaygroundService): fix not parsable results
    
    * feat(SQLPlaygroundVerticel): use correct RUN ADDRESS
    
    * feat(Secrets): fix HMAC algorithms & byte transformation
    
    * feat(HttpVerticle): remove resultType
    
    * feat(DBConnections): allow to skip user deletion
    
    * feat(SQLPlaygroundService): added execution mode
    
    * feat(SQLPlaygroundService): fix default db Type
    
    * feat(DbOperationsService): added getDatabaseInformation
    
    * feat(SqlPlaygroundVertical): create own datasource
    
    * fix(ExtendedResultService): not fail if result is empty
    
    * feat: added DatabaseInformationService
    
    * feat(SQLPlaygroundService): get and return database information
    
    * docs(DatabaseInformationService): typo
    
    * feat(SQLPlaygroundService): parse multiple query results
    
    * feat(SQLPlaygroundVerticle): catch transform erros
    
    * feat(DBOperationsService): added createDBIfNotExist and createUserIfNotExist
    
    * feat(SQLPlaygroundVerticle): create db user & db if needed
    
    * refactor(PsqlOperationsService): fix linting errors

---
+++ modules/fbs-runner/checker/src/main/scala/de/thm/ii/fbs/services/runner/SQLPlaygroundService.scala
@@ -0,0 +1,95 @@
+package de.thm.ii.fbs.services.runner
+
+import de.thm.ii.fbs.services.ExtendedResultsService
+import de.thm.ii.fbs.services.db.{DBOperationsService, PsqlOperationsService}
+import de.thm.ii.fbs.types.{OutputJsonStructure, SqlPlaygroundRunArgs}
+import de.thm.ii.fbs.util.{DBTypes, DatabaseInformationService, PlaygroundDBConnections, SqlPlaygroundMode}
+import io.vertx.core.json.JsonObject
+import io.vertx.scala.ext.sql.ResultSet
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object SQLPlaygroundService {
+  final val PLAYGROUND_RESULT_TYPE = "playground"
+  private final val outputJsonStructure = List(
+    OutputJsonStructure("tables", Option("columns")),
+    OutputJsonStructure("constrains", Option("constrains")),
+    OutputJsonStructure("views"),
+    OutputJsonStructure("routines"),
+    OutputJsonStructure("triggers")
+  )
+
+  def isPlaygroundResult(res: JsonObject): Boolean = res.getString("resultType", "").equals(PLAYGROUND_RESULT_TYPE)
+
+  /**
+    * Transforms the Result into an JsonObject.
+    *
+    * @param statementResult   The Statement Results
+    * @param informationResult The Database information Result
+    * @param error             is an Error occurred
+    * @param errorMsg          the Error Message
+    */
+  def transformResult(sqlRunArgs: SqlPlaygroundRunArgs,
+                      statementResult: Option[ResultSet],
+                      informationResult: Option[ResultSet],
+                      error: Boolean = false,
+                      errorMsg: Option[String] = None): JsonObject = {
+    val res = new JsonObject()
+
+    res.put("executionId", sqlRunArgs.executionId)
+      .put("result", ExtendedResultsService.buildMultiResultTable(statementResult))
+      .put("databaseInformation", DatabaseInformationService.buildOutputJson(getInformationResultValue(informationResult), outputJsonStructure))
+      .put("error", error)
+      .put("errorMsg", errorMsg.getOrElse(""))
+      .put("mode", sqlRunArgs.mode)
+      .put("resultType", PLAYGROUND_RESULT_TYPE)
+  }
+
+  /**
+    * Default information Result to empty Json Object
+    */
+  private def getInformationResultValue(informationResult: Option[ResultSet]): JsonObject = {
+    if (informationResult.isDefined) {
+      informationResult.get.asJava.toJson
+    } else {
+      new JsonObject()
+    }
+  }
+}
+
+class SQLPlaygroundService(val sqlRunArgs: SqlPlaygroundRunArgs, val con: PlaygroundDBConnections, val queryTimeout: Int) {
+  private def isPsql: Boolean = DBTypes.isPsql(sqlRunArgs.database.dbType)
+
+  private def initDBOperations(): DBOperationsService = {
+    // Currently only PostgresSql is Supported
+    if (!isPsql) throw new Error("Invalid DBType. Currently only Psql is Supported")
+
+    val dbName = s"playground_db_${sqlRunArgs.database.id.toString}"
+    val username = s"playground_user_${sqlRunArgs.user.id.toString}"
+    new PsqlOperationsService(dbName, username, queryTimeout)
+  }
+
+  def executeStatement(): Future[(ResultSet, ResultSet)] = {
+    val dbOperations = initDBOperations()
+    val deleteDatabase = SqlPlaygroundMode.shouldDeleteDatabase(sqlRunArgs.mode)
+
+    con.initCon(dbOperations).flatMap(_ => {
+      executeAndCollectInformation(dbOperations)
+    }) transform {
+      case s@Success(_) =>
+        con.close(dbOperations, deleteDatabase)
+        s
+      case Failure(cause) =>
+        con.close(dbOperations, deleteDatabase)
+        Failure(throw cause)
+    }
+  }
+
+  private def executeAndCollectInformation(dbOperations: DBOperationsService): Future[(ResultSet, ResultSet)] = {
+    dbOperations.queryFutureWithTimeout(con.queryCon.get, sqlRunArgs.statement).flatMap(statementResult =>
+      dbOperations.getDatabaseInformation(con.queryCon.get).map(informationResult => (statementResult, informationResult))
+    )
+  }
+}
