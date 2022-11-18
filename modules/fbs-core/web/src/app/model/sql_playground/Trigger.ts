979ada38f5f93fd2e96292b6fc4f85c53eeffd4a
Johannes Kunz <57532417+Medienkunzt@users.noreply.github.com>
2022-11-18 17:54:19 +0100

    get current version (#968)
    * created files
    
    * [SQL Playground] Error while storing larger Database Informations (#958)
    
    fix(SQL Playground): use correct datatype to store database information and results
    
    * docs(SQL Playground api): fix database creation method (#959)
    
    * [Exel-Checker] Result visualization (#937)
    
    * added errorcount per subtask and visualizations
    
    * fixed problem where mistakes were found
    when solution file  was compared to itself
    
    * working on tables
    
    * added views
    
    * added trigger
    
    * added routines
    
    * added interfaces
    
    * split into own components
    
    * added seperator line
    
    * added seperator line
    
    * prettier
    
    * prettier
    
    Co-authored-by: nicosoethe <ni.soethe@gmail.com>
    Co-authored-by: Max Stephan <max.stephan@mni.thm.de>
    Co-authored-by: MaxRchtr <113101526+MaxRchtr@users.noreply.github.com>
    Co-authored-by: nicosoethe <104082549+nicosoethe@users.noreply.github.com>

---
+++ modules/fbs-core/web/src/app/model/sql_playground/Trigger.ts
@@ -0,0 +1,16 @@
+export interface Trigger {
+  name: string;
+  action: TriggerAction;
+  event: TriggerEvent;
+}
+
+export interface TriggerAction {
+  statement: string;
+  orientation: string;
+  timing: string;
+}
+
+export interface TriggerEvent {
+  manipulation: string;
+  objectTable: string;
+}
--- scripts/analysis/excel/analyze_code.py
+++ scripts/analysis/excel/analyze_code.py
@@ -2,7 +2,10 @@ import os
 from pathlib import Path
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# class for relevant area and index of worksheet thats being analyzed
 class CheckerWorksheet:
     def __init__(self, start_row, end_row, start_col, end_col, j):
         self.start_row = start_row
@@ -11,34 +14,55 @@ class CheckerWorksheet:
         self.end_col = end_col
         self.j = j
 
-
+# class for subtask with its amount of errors and the sheet index
 class Subtask:
-    def __init__(self, sum=0, formula=0, number=0, format=0):
+    def __init__(self, sum=0, formula=0, number=0, format=0, sheet_index=0):
         self.sum = sum
         self.formula = formula
         self.number = number
         self.format = format
+        self.sheet_index = sheet_index
 
+    # method for counting errors in a subtask
     def classify_error(self, start_row, end_row, start_col, end_col, wb, wb2, wb_sol, wb_sol2, j): 
-            ws = wb.worksheets[j]
-            ws_sol = wb_sol.worksheets[j]
-            ws2 = wb2.worksheets[j]
-            ws_sol2 = wb_sol2.worksheets[j]
-            for row in range(start_row, end_row):
-                for col in range(start_col, end_col):
-                    char = get_column_letter(col)
-                    if ws[char + str(row)].value != ws_sol[char + str(row)].value:
-                        if ws_sol[char + str(row)].value != ws_sol2[char + str(row)].value:
-                            self.sum += 1
-                            self.formula += 1
-                        if ws_sol[char + str(row)].value == ws_sol2[char + str(row)].value and ws2[char + str(row)].value != ws_sol2[char + str(row)].value:
-                            self.sum += 1
-                            self.number += 1
-                    if ws[char + str(row)].font != ws_sol[char + str(row)].font:
+        ws = wb.worksheets[j]
+        ws_sol = wb_sol.worksheets[j]
+        ws2 = wb2.worksheets[j]
+        ws_sol2 = wb_sol2.worksheets[j]
+        for row in range(start_row, end_row):
+            for col in range(start_col, end_col):
+                char = get_column_letter(col)
+                if ws[char + str(row)].value != ws_sol[char + str(row)].value:
+                    if ws_sol[char + str(row)].value != ws_sol2[char + str(row)].value:
                         self.sum += 1
-                        self.format += 1
-            
-            return self.sum, self.formula, self.number, self.format
+                        self.formula += 1
+                    if ws_sol[char + str(row)].value == ws_sol2[char + str(row)].value and ws2[char + str(row)].value != ws_sol2[char + str(row)].value:
+                        self.sum += 1
+                        self.number += 1
+                if (ws[char + str(row)].font.name != ws_sol[char + str(row)].font.name or
+                    ws[char + str(row)].font.sz != ws_sol[char + str(row)].font.sz or 
+                    ws[char + str(row)].font.b != ws_sol[char + str(row)].font.b or
+                    ws[char + str(row)].font.i != ws_sol[char + str(row)].font.i or
+                    ws[char + str(row)].font.charset != ws_sol[char + str(row)].font.charset or
+                    ws[char + str(row)].font.u != ws_sol[char + str(row)].font.u or
+                    ws[char + str(row)].font.strike != ws_sol[char + str(row)].font.strike or
+                    ws[char + str(row)].font.color != ws_sol[char + str(row)].font.color or
+                    ws[char + str(row)].font.scheme != ws_sol[char + str(row)].font.scheme or
+                    ws[char + str(row)].font.family != ws_sol[char + str(row)].font.family or
+                    ws[char + str(row)].font.size != ws_sol[char + str(row)].font.size or
+                    ws[char + str(row)].font.bold != ws_sol[char + str(row)].font.bold or
+                    ws[char + str(row)].font.italic != ws_sol[char + str(row)].font.italic or
+                    ws[char + str(row)].font.strikethrough != ws_sol[char + str(row)].font.strikethrough or
+                    ws[char + str(row)].font.underline != ws_sol[char + str(row)].font.underline or
+                    ws[char + str(row)].font.vertAlign != ws_sol[char + str(row)].font.vertAlign or
+                    ws[char + str(row)].font.outline != ws_sol[char + str(row)].font.outline or
+                    ws[char + str(row)].font.shadow != ws_sol[char + str(row)].font.shadow or
+                    ws[char + str(row)].font.condense != ws_sol[char + str(row)].font.condense or
+                    ws[char + str(row)].font.extend != ws_sol[char + str(row)].font.extend):
+                    self.sum += 1
+                    self.format += 1
+        self.sheet_index = j
+        return self.sum, self.formula, self.number, self.format, self.sheet_index
         
 
 def analyze(path, path_sol):
@@ -47,6 +71,7 @@ def analyze(path, path_sol):
     path_sol = Path(path_sol)
     file_count = 0
     total_sum = total_formula = total_number = total_format = 0
+    overall = overall_formula = overall_number = overall_format = 0
    
     # counts ".xlsx"-files in directory, thats supposed to be analyzed
     for file in os.listdir(path):
@@ -57,6 +82,7 @@ def analyze(path, path_sol):
     wb_sol = load_workbook(path_sol)
     wb_sol2 = load_workbook(path_sol, data_only=True)
 
+    # creating worksheet objects with relevant values and inserting them into a list
     sheet_def = []
     sheet = CheckerWorksheet(1, 34, 2, 16, 1)
     sheet_def.append(sheet)
@@ -87,7 +113,8 @@ def analyze(path, path_sol):
     sheet = CheckerWorksheet(1, 10, 1, 5, 14) # Placeholder, sheet will be skipped: only graph to compare
     sheet_def.append(sheet)
 
-
+    # looping over submissions in given directory and analysing them
+    # results are stored in "subtask"-objects and inserted into a list
     for i, input in enumerate(path.glob("*.xlsx*")): # rglob if directory includes subfolders and you want to access those aswell
         wb = load_workbook(filename=input)
         wb2 = load_workbook(filename=input, data_only=True)
@@ -105,12 +132,48 @@ def analyze(path, path_sol):
             print("Studentenabgabe hat zu viele Sheets!")
 
 
-    for entry in res:
-        total_sum += entry.sum
-        total_formula += entry.formula
-        total_number += entry.number
-        total_format += entry.format
+    # iterating over list of "subtask"-objects and counting errors of subtasks with the same index 
+    n = 1
+    while n < len(wb_sol.worksheets):
+        for entry in res:
+            if n == entry.sheet_index:
+                total_sum += entry.sum
+                total_formula += entry.formula
+                total_number += entry.number
+                total_format += entry.format
+
+        print(f"In Unteraufgabe {n} wurden Insgesamt {total_sum} Fehler gemacht!")
+        print(f"Davon waren {total_formula} Formel-, {total_number} Nummern- & {total_format} Formatfehler!")
+
+        # visualisation of errortypes per subtask as pie chart
+        if n != 11 and n != 13 and n != 14 and total_sum > 0:
+            data = [total_formula, total_number, total_format]
+            labels = ["formula errors", "number errors" , "format errors"]
+
+            colors = sns.color_palette('pastel')[0:5]
+            plt.pie(data, labels = labels, colors = colors, autopct='%.0f%%')
+            plt.show()
+
+        n += 1
+        overall += total_sum
+        overall_formula += total_formula
+        overall_number += total_number
+        overall_format += total_format
+
+        total_sum = 0
+        total_formula = 0
+        total_number = 0
+        total_format = 0
 
 
-    print(f"In allen Abgaben wurden insgesamt {total_sum} Fehler gemacht!") 
-    print(f"Davon waren {total_formula} Formel-, {total_number} Nummern- & {total_format} Formatfehler!")
+    print(f"In allen Abgaben wurden somit insgesamt {overall} Fehler gemacht!") 
+    print(f"Davon waren {overall_formula} Formel-, {overall_number} Nummern- & {overall_format} Formatfehler!")
+    
+    if overall > 0:
+        data = [overall_formula, overall_number, overall_format]
+        labels = ["formula errors", "number errors" , "format errors"]
+
+        # visualisation of errortypes for total errorcount of all submissions as pie chart
+        colors = sns.color_palette('pastel')[0:5]
+        plt.pie(data, labels = labels, colors = colors, autopct='%.0f%%')
+        plt.show()
