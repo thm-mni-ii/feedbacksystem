c0316c61c1dd7901cdff4d27374bce8eef32d660
Medienkunzt <monoxotix@gmail.com>
2022-11-23 18:39:35 +0100

    fix undefined table constraint

--- modules/fbs-core/web/src/app/page-components/sql-playground/db-scheme/db-scheme-tables/db-scheme-tables.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/db-scheme/db-scheme-tables/db-scheme-tables.component.ts
@@ -18,22 +18,27 @@ export class DbSchemeTablesComponent
   ngOnChanges() {
     if (this.tables !== undefined && this.constraints !== undefined) {
       this.tables.forEach((table) => {
-        let tableConstraints = this.constraints.filter(
-          (constraint) => constraint.table === table.name
-        );
-        table.constraints = tableConstraints[0];
-        table.constraints.constraints.sort((a, b) =>
-          a.type > b.type ? -1 : 1
-        );
-
-        table.columns.forEach((column) => {
-          let isPk = table.constraints.constraints.filter(
-            (constraint) =>
-              constraint.columnName == column.name &&
-              constraint.type == "PRIMARY KEY"
+        if (this.constraints !== undefined) {
+          let tableConstraints = this.constraints.filter(
+            (constraint) => constraint.table === table.name
           );
-          column.isPrimaryKey = isPk.length > 0;
-        });
+
+          if (tableConstraints[0] !== undefined) {
+            table.constraints = tableConstraints[0];
+            table.constraints.constraints.sort((a, b) =>
+              a.type > b.type ? -1 : 1
+            );
+
+            table.columns.forEach((column) => {
+              let isPk = table.constraints.constraints.filter(
+                (constraint) =>
+                  constraint.columnName == column.name &&
+                  constraint.type == "PRIMARY KEY"
+              );
+              column.isPrimaryKey = isPk.length > 0;
+            });
+          }
+        }
       });
     }
   }
