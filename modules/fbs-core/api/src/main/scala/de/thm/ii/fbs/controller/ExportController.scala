50371a575eeecfb480a5cef2d375ca5639f252a0
Max Stephan <max.stephan@mni.thm.de>
2022-11-29 13:58:34 +0100

    feat(ExportController): improve task export route

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/controller/ExportController.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/controller/ExportController.scala
@@ -3,7 +3,7 @@ package de.thm.ii.fbs.controller
 import de.thm.ii.fbs.controller.exception.ForbiddenException
 import de.thm.ii.fbs.model.{CourseRole, GlobalRole}
 import de.thm.ii.fbs.services.`export`.TaskExportService
-import de.thm.ii.fbs.services.persistence.{CourseRegistrationService, StorageService}
+import de.thm.ii.fbs.services.persistence.CourseRegistrationService
 import de.thm.ii.fbs.services.security.AuthService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.InputStreamResource
@@ -14,7 +14,7 @@ import java.nio.file.{Files, StandardOpenOption}
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
 @RestController
-@GetMapping(path = Array("/api/v1/export"), produces = Array(MediaType.APPLICATION_JSON_VALUE))
+@GetMapping(path = Array("/api/v1/courses/{cid}"), produces = Array(MediaType.APPLICATION_JSON_VALUE))
 class ExportController {
   @Autowired
   private val authService: AuthService = null
@@ -22,14 +22,12 @@ class ExportController {
   private val taskExportService: TaskExportService = null
   @Autowired
   private val courseRegistrationService: CourseRegistrationService = null
-  @Autowired
-  private val storageService: StorageService = null
 
-  @GetMapping(value = Array("/{cid}/{taskId}"))
+  @GetMapping(value = Array("/tasks/{taskId}/export"))
   @ResponseBody
-  def getTask(@PathVariable(value = "taskId", required = true) taskId: Int,
-              @PathVariable(value = "cid", required = true) cid: Int,
-              req: HttpServletRequest, res: HttpServletResponse): ResponseEntity[InputStreamResource] = {
+  def exportTask(@PathVariable(value = "taskId", required = true) taskId: Int,
+                 @PathVariable(value = "cid", required = true) cid: Int,
+                 req: HttpServletRequest, res: HttpServletResponse): ResponseEntity[InputStreamResource] = {
     val user = authService.authorize(req, res)
     val privilegedByCourse = courseRegistrationService.getParticipants(cid).find(_.user.id == user.id)
       .exists(p => p.role == CourseRole.DOCENT || p.role == CourseRole.TUTOR)
