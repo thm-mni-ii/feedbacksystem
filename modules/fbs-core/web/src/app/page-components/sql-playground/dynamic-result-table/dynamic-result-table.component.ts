0c258e2addb42a93c2e917353fe861b62dabeb57
Medienkunzt <monoxotix@gmail.com>
2022-12-09 16:43:48 +0100

    add Tabs for results

--- modules/fbs-core/web/src/app/page-components/sql-playground/dynamic-result-table/dynamic-result-table.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/dynamic-result-table/dynamic-result-table.component.ts
@@ -1,6 +1,14 @@
-import { Component, ViewChild, Input, OnChanges } from "@angular/core";
+import {
+  Component,
+  ViewChildren,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  QueryList,
+} from "@angular/core";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatTableDataSource } from "@angular/material/table";
+import { SQLResponse } from "../../../model/sql_playground/SQLResponse";
 
 export interface Content {
   head: any[];
@@ -13,28 +21,70 @@ export interface Content {
   styleUrls: ["./dynamic-result-table.component.scss"],
 })
 export class DynamicResultTableComponent implements OnChanges {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChildren(MatPaginator) paginator = new QueryList<MatPaginator>();
   @Input() resultset: any;
   @Input() isQueryPending: boolean = false;
   /*   dataSource = new MatTableDataSource<any[]>(bigTable.rows);
   displayedColumns = bigTable.head; */
   dataSource: MatTableDataSource<string[]>;
-  displayedColumns: any[] = [];
+  displayedColumns: string[] = [];
 
-  ngOnChanges() {
-    if (this.isQueryPending) {
-      this.resultset = undefined;
-      this.displayedColumns = [];
+  activeResId: number = 0;
+  tabCounter: number = 0;
+  tabs: any[] = [];
+
+  ngAfterViewInit() {
+    this.paginator.changes.subscribe(() => {
+      let dataSourceCounter = 0;
+      this.tabs.forEach((tab) => {
+        if (tab.dataSource !== undefined) {
+          tab.dataSource.paginator =
+            this.paginator.toArray()[dataSourceCounter];
+          dataSourceCounter++;
+        }
+      });
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      changes.isQueryPending &&
+      changes.isQueryPending.firstChange === false
+    ) {
+      // isQueryPending changed
     }
 
-    if (this.resultset !== undefined && this.resultset.error === false) {
-      this.displayedColumns = this.resultset.result[0].head;
-      this.dataSource = new MatTableDataSource<string[]>(
-        this.resultset.result[0].rows
-      );
-      setTimeout(() => {
-        this.dataSource.paginator = this.paginator;
-      }, 100);
+    if (changes.resultset && changes.resultset.firstChange === false) {
+      // isQueryPending changed
+      if (this.resultset !== undefined && this.resultset.error === false) {
+        this.dataSource = new MatTableDataSource<string[]>(
+          this.resultset.result[0].rows
+        );
+      }
+
+      let newTab: any = {};
+      newTab.id = this.tabCounter;
+      newTab.name = "Result";
+      newTab.error = this.resultset.error;
+      if (this.resultset.error == true) {
+        newTab.errorMsg = this.resultset.errorMsg;
+      } else if (this.resultset.error == false) {
+        newTab.dataSource = this.dataSource;
+        newTab.displayedColumns = this.resultset.result[0].head;
+      } else {
+        throw new Error("Unknown error");
+      }
+
+      this.tabs.push(newTab);
+
+      this.tabCounter++;
+      this.activeResId = this.tabs.length - 1;
     }
   }
+
+  closeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+
+  tabChanged(event: any) {}
 }
--- modules/fbs-core/web/src/app/page-components/task-detail/submission-text/submission-text.component.ts
+++ modules/fbs-core/web/src/app/page-components/task-detail/submission-text/submission-text.component.ts
@@ -1,15 +1,24 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
   selector: "app-submission-text",
   templateUrl: "./submission-text.component.html",
   styleUrls: ["./submission-text.component.scss"],
 })
-export class SubmissionTextComponent {
+export class SubmissionTextComponent implements OnInit {
   toSubmit = "";
+  @Input() title?: string;
   @Output() update: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {}
+
+  titleText: string = "Abgabe Text:";
+
+  ngOnInit() {
+    if (this.title != null) {
+      this.titleText = this.title;
+    }
+  }
 
   updateSubmission(event) {
     this.update.emit({ content: event });
