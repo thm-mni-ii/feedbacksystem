a6e905aae9da3ff9216c142791aaa584a8ae24a3
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2022-12-15 16:44:38 +0100

    feat(excel-checker): adds dependency graph generation and test

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/ReferenceGraph.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/ReferenceGraph.kt
@@ -8,8 +8,21 @@ class ReferenceGraph(references: Map<Int, Map<String, Set<String>>>) {
     val data: Graph<Cell, DefaultEdge> = DefaultDirectedGraph(DefaultEdge::class.java)
 
     init {
-        references.forEach {
-            cell -> data.addVertex() // TODO generate graph
+        references.forEach { (index, sheet) ->
+            sheet.forEach { (cell, refs) ->
+                val cellVertex = Cell(index, cell)
+                data.addVertex(cellVertex) // inserts vertex if it not already exists
+                refs.forEach { ref ->
+                    val externalReference = ref.split("!") // TODO implement delimiter for ODF
+                    val refVertex = if (externalReference.size == 2) {
+                        Cell(externalReference[0].toInt(), externalReference[1])
+                    } else {
+                        Cell(index, ref)
+                    }
+                    data.addVertex(refVertex) // inserts vertex if it not already exists
+                    data.addEdge(cellVertex, refVertex) // inserts edge if it not already exists (no multiple edges)
+                }
+            }
         }
     }
 }
