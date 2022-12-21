5f021c8ec957481ba53626f35508df25d40dd106
nicosoethe <104082549+nicosoethe@users.noreply.github.com>
2022-12-21 11:08:10 +0100

    sql playground submission of a task in sql playground (#1052)
    * started with interface
    
    * improved css and interface
    
    * css improvements
    
    * added task selecter
    
    * working on submit function
    
    * There is a bug
    
    * bug fixed
    
    * improvements
    
    * added more functionality
    
    * fixing lintin problems
    
    * fix margin
    
    * fix linting
    
    * minor fixes
    
    Co-authored-by: Johannes <monoxotix@gmail.com>
    Co-authored-by: Johannes Kunz <57532417+Medienkunzt@users.noreply.github.com>

--- modules/fbs-core/web/src/app/page-components/sql-playground/sql-input-tabs/sql-input-tabs.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/sql-input-tabs/sql-input-tabs.component.ts
@@ -10,6 +10,15 @@ import { MatDialog } from "@angular/material/dialog";
 import { ConfirmDialogComponent } from "src/app/dialogs/confirm-dialog/confirm-dialog.component";
 import { UntypedFormControl } from "@angular/forms";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { AuthService } from "src/app/service/auth.service";
+import { SqlPlaygroundService } from "src/app/service/sql-playground.service";
+import { Observable, of } from "rxjs";
+import { Course } from "src/app/model/Course";
+import { CourseRegistrationService } from "../../../service/course-registration.service";
+import { repeat, takeWhile } from "rxjs/operators";
+import { TaskService } from "src/app/service/task.service";
+import { Task } from "src/app/model/Task";
+import { SubmissionService } from "../../../service/submission.service";
 
 @Component({
   selector: "app-sql-input-tabs",
@@ -27,15 +36,37 @@ export class SqlInputTabsComponent implements OnInit {
     }
   }
 
-  constructor(private dialog: MatDialog, private snackbar: MatSnackBar) {}
+  constructor(
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar,
+    private authService: AuthService,
+    private sqlPlaygroundService: SqlPlaygroundService,
+    private courseRegistrationService: CourseRegistrationService,
+    private submissionService: SubmissionService,
+    private taskService: TaskService
+  ) {}
 
   fileName = "New_Query";
   tabs = [{ name: this.fileName, content: "" }];
   activeTabId = new UntypedFormControl(0);
   activeTab = this.tabs[this.activeTabId.value];
   pending: boolean = false;
+  courses: Observable<Course[]> = of();
+  control: UntypedFormControl = new UntypedFormControl();
+  isSubmitMode = false;
+  selectedCourseName: String = "Kurs";
+  selectedTaskName: String = "Aufgabe";
+  selectedCourse: Course;
+  selectedTask: Task;
+  allTasksFromCourse: Task[];
+  filteredTasksFromCourse: Task[] = [];
+  isDescriptionMode: boolean = false;
+  isSubCorr = false;
+  submitted = false;
 
   ngOnInit(): void {
+    const userID = this.authService.getToken().id;
+    this.courses = this.courseRegistrationService.getRegisteredCourses(userID);
     this.activeTabId.valueChanges.subscribe((value) => {
       this.activeTab = this.tabs[value];
     });
@@ -99,5 +130,134 @@ export class SqlInputTabsComponent implements OnInit {
       return;
     }
     this.submitStatement.emit(this.activeTab.content);
+  }
+
+  updateMode(value: boolean) {
+    this.isSubmitMode = value;
+  }
+
+  hasDeadlinePassed(task: Task = this.selectedTask): boolean {
+    if (task == null) {
+      return true;
+    }
+    return Date.now() > Date.parse(task.deadline);
+  }
+
+  emptyTask() {
+    this.selectedTask = null;
+    this.filteredTasksFromCourse = [];
+    this.selectedTaskName = "Aufgabe";
+  }
+
+  changeCourse(course: Course) {
+    this.selectedCourse = course;
+    this.selectedCourseName = this.selectedCourse.name;
+    //this.tasks = this.taskService.getAllTasks(this.selectedCourse.id);
+    this.getTasks();
+    this.emptyTask();
+  }
+
+  changeTask(task: Task) {
+    this.selectedTask = task;
+    this.selectedTaskName = this.selectedTask.name;
+    this.submitted = false;
+  }
+
+  getTasks() {
+    this.taskService.getAllTasks(this.selectedCourse.id).subscribe(
+      (allTasks) => {
+        this.allTasksFromCourse = allTasks;
+        this.filterTasks();
+      },
+      () => {}
+    );
+  }
+
+  private filterTasks() {
+    for (let task of this.allTasksFromCourse) {
+      if (task.mediaType == "text/plain" && !this.hasDeadlinePassed(task)) {
+        this.filteredTasksFromCourse.push(task);
+      }
+    }
+    if (this.filteredTasksFromCourse.length < 1) {
+      this.snackbar.open("Dieser Kurs hat keine SQL Aufgaben!", "OK", {
+        duration: 3000,
+      });
+    }
+  }
+
+  wasSubmissionCorrect(subResult: number) {
+    if (subResult != 0) {
+      this.isSubCorr = false;
+    } else {
+      this.isSubCorr = true;
+    }
+  }
+
+  waitForSubDone(sid: number) {
+    const token = this.authService.getToken();
+    this.submissionService
+      .getSubmission(
+        token.id,
+        this.selectedCourse.id,
+        this.selectedTask.id,
+        sid
+      )
+      .pipe(
+        repeat(),
+        takeWhile((sub) => !sub.done, true)
+      )
+      .subscribe(
+        (res) => {
+          if (res.done) {
+            this.wasSubmissionCorrect(res.results[0].exitCode);
+            this.pending = false;
+          }
+        },
+        () => {}, //handle error
+        () => console.log("Submission Request Complete")
+      );
+  }
+
+  private submitToTask() {
+    this.submitted = true;
+    this.pending = true;
+    const token = this.authService.getToken();
+    this.submissionService
+      .submitSolution(
+        token.id,
+        this.selectedCourse.id,
+        this.selectedTask.id,
+        this.activeTab.content
+      )
+      .subscribe(
+        (subResult) => {
+          this.snackbar.open("Deine Abgabe wird ausgewertet.", "OK", {
+            duration: 3000,
+          });
+          this.waitForSubDone(subResult.id);
+        },
+        (error) => {
+          console.error(error);
+          this.snackbar.open(
+            "Beim Versenden ist ein Fehler aufgetreten. Versuche es später erneut.",
+            "OK",
+            { duration: 3000 }
+          );
+          this.pending = false;
+        },
+        () => {
+          console.log("Request Complete");
+        }
+      );
+  }
+
+  submissionToTask(): void {
+    if (this.isSubmissionEmpty()) {
+      this.snackbar.open("Sie haben keine Lösung abgegeben", "Ups!");
+      return;
+    }
+    this.submitToTask();
+    //this.submissionService.emitFileSubmission();
   }
 }
