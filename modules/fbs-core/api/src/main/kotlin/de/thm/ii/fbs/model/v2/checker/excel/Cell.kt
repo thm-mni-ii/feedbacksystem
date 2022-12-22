81c0cf9925f057df7438cb4a916f218f4da6d099
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2022-12-22 15:11:44 +0100

    feat(excel-checker): Adds custom hash and equals function for cell data structure

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/Cell.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/Cell.kt
@@ -4,4 +4,24 @@ data class Cell(val sheet: Int, val cell: String, val value: String? = null) {
     companion object {
         const val SHEET_DELIMITER = "!"  // TODO implement delimiter for ODF
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Cell
+
+        if (sheet != other.sheet) return false
+        if (cell != other.cell) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = sheet
+        result = 31 * result + cell.hashCode()
+        return result
+    }
+
+
 }
