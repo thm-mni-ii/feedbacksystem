517523fbef6668605374c900c887000fe6d9f484
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2022-12-22 15:10:36 +0100

    feat(excel-checker): Adds service to find propagated errors

---
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/checker/excel/PropagatedErrorsService.kt
@@ -0,0 +1,66 @@
+package de.thm.ii.fbs.services.v2.checker.excel
+
+import de.thm.ii.fbs.model.v2.checker.excel.Cell
+import de.thm.ii.fbs.model.v2.checker.excel.ReferenceGraph
+import de.thm.ii.fbs.utils.v2.spreadsheet.SpreadsheetReferenceParser
+import org.apache.poi.ss.usermodel.FormulaEvaluator
+import org.apache.poi.ss.util.CellReference
+import org.apache.poi.xssf.usermodel.XSSFCell
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.jgrapht.Graphs
+
+
+class PropagatedErrorsService(private val workbook: XSSFWorkbook) {
+    private val evaluator: FormulaEvaluator = workbook.creationHelper.createFormulaEvaluator()
+    private val graph = ReferenceGraph(HashMap()) // TODO get dependency graph from solution entry in db
+    private val solution = HashMap<Cell, String>() // TODO get solution values from solution entry in db
+
+    fun findAllPropagatedErrors(invalidCells: List<Cell>): Set<Cell> {
+        val errors = HashSet<Cell>()
+        val visited = HashSet<Cell>()
+        for (invalidCell in invalidCells) {
+            findPropagatedErrors(invalidCell, errors, visited)
+        }
+        return errors
+    }
+
+    private fun findPropagatedErrors(cell: Cell, errors: Set<Cell>,
+                                     visited: Set<Cell>): Boolean {
+        visited.plus(cell)
+        val workbookCell = getCellFromWorkbook(cell)
+
+        // Base Case
+        // compare cell with solution cell (or it is an input)
+        if (cellEqualsSolution(cell, workbookCell)) {
+            return true
+        }
+
+        // Graph Construction - DFS
+        val references = Graphs.neighborSetOf(graph.data, cell)
+        for (reference in references) {
+            if (!visited.contains(reference)) {
+                findPropagatedErrors(reference, errors, visited)
+            }
+        }
+
+        // Graph Deconstruction
+        // eval cell again and compare again with solution cell
+        evaluator.evaluateFormulaCell(workbookCell)
+        val eval = cellEqualsSolution(cell, workbookCell)
+        if (!eval) {
+            errors.plus(cell) // add to original errors set
+            workbookCell.setCellValue(solution[cell]) // substitute cell value with solution value
+        }
+
+        return eval
+    }
+
+    private fun getCellFromWorkbook(cell: Cell): XSSFCell {
+        val cellRef = CellReference(cell.cell)
+        return workbook.getSheetAt(cell.sheet).getRow(cellRef.row).getCell(cellRef.col.toInt())
+    }
+
+    private fun cellEqualsSolution(cell: Cell, workbookCell: XSSFCell): Boolean {
+        return solution[cell].equals(SpreadsheetReferenceParser.valueOfCell(workbookCell))
+    }
+}
