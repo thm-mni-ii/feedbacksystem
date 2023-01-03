ba97a3edfa26ee17191c4c4420d8515f7c26fb19
Max Stephan <max.stephan@mni.thm.de>
2023-01-03 11:03:36 +0100

    refactor(SubmissionController): clean up

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/controller/SubmissionController.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/controller/SubmissionController.scala
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation._
 import org.springframework.web.multipart.MultipartFile
 
-import java.nio.file.Files
 import java.time.Instant
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
