5f80de6a3c51835428475940876ebd1343600f73
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-01-05 17:04:58 +0100

    feat(excel-checker): new test cases and refactoring

--- modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/services/v2/checker/excel/PropagatedErrorsServiceTest.kt
+++ modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/services/v2/checker/excel/PropagatedErrorsServiceTest.kt
@@ -10,94 +10,223 @@ import org.junit.jupiter.api.Assertions.*
 class PropagatedErrorsServiceTest {
     @Test
     fun originalErrorTest() {
-        val sc1 = Cell(0, "A1", "3")
-        val sc2 = Cell(0, "B1", "1")
-        val sc3 = Cell(0, "C1", "2")
+        val c1 = Cell(0, "A1", formula = "B1")
+        val c2 = BasicTestCase.c2
+        val c3 = BasicTestCase.c3
 
-        val sGraph = ReferenceGraph(
-            mapOf(
-                0 to mapOf(
-                    sc1.cell to Pair(sc1.value!!, setOf(sc2, sc3)),
-                    sc2.cell to Pair(sc2.value!!, setOf()),
-                    sc3.cell to Pair(sc3.value!!, setOf())
-                )
-            )
-        )
-
-
-        val c1 = Cell(0, "A1", "1", "B1")
-        val c2 = Cell(0, "B1", "1")
-        val c3 = Cell(0, "C1", "2")
-
-        val service = PropagatedErrorsService(workbook(c1, c2, c3), sGraph, solutionMap(listOf(sc1, sc2, sc3)))
+        val service = PropagatedErrorsService(workbook(c1, c2, c3), BasicTestCase.sGraph, BasicTestCase.sMap)
         val res = service.findAllPropagatedErrors(listOf(c1))
 
         assertEquals(setOf(c1), res)
     }
 
     @Test
+    fun noErrorTest() {
+        val c1 = Cell(0, "A1", formula = "SUM(B1:C1)")
+        val c2 = BasicTestCase.c2
+        val c3 = BasicTestCase.c3
+
+        val service = PropagatedErrorsService(workbook(c1, c2, c3), BasicTestCase.sGraph, BasicTestCase.sMap)
+        val res = service.findAllPropagatedErrors(listOf(c1))
+
+        assertEquals(setOf<Cell>(), res)
+    }
+
+    @Test
     fun propagatedErrorTest() {
-        val sc1 = Cell(0, "A1", "3")
-        val sc2 = Cell(0, "B1", "1")
-        val sc3 = Cell(0, "C1", "2")
-        val sc4 = Cell(0, "D1", "1")
-        val sc5 = Cell(0, "E1", "1")
-
-        val sGraph = ReferenceGraph(
-            mapOf(
-                0 to mapOf(
-                    sc1.cell to Pair(sc1.value!!, setOf(sc2, sc3)),
-                    sc2.cell to Pair(sc2.value!!, setOf()),
-                    sc3.cell to Pair(sc3.value!!, setOf(sc4, sc5)),
-                    sc4.cell to Pair(sc4.value!!, setOf()),
-                    sc5.cell to Pair(sc5.value!!, setOf())
-                )
-            )
-        )
-
-        val c1 = Cell(0, "A1", "2", "B1 + C1")
-        val c2 = Cell(0, "B1", "1")
-        val c3 = Cell(0, "C1", "1", "D1 + E1 - 1")
-        val c4 = Cell(0, "D1", "1")
-        val c5 = Cell(0, "E1", "1")
+        val c1 = Cell(0, "A1", formula = "B1 + C1")
+        val c2 = SimpleTestCase.c2
+        val c3 = Cell(0, "C1", formula = "D1 + E1 - 1")
+        val c4 = SimpleTestCase.c4
+        val c5 = SimpleTestCase.c5
 
         val service =
-            PropagatedErrorsService(workbook(c1, c2, c3, c4, c5), sGraph, solutionMap(listOf(sc1, sc2, sc3, sc4, sc5)))
+            PropagatedErrorsService(workbook(c1, c2, c3, c4, c5), SimpleTestCase.sGraph, SimpleTestCase.sMap)
         val res = service.findAllPropagatedErrors(listOf(c1))
 
         assertEquals(setOf(c3), res)
     }
 
     @Test
-    fun noErrorTest() {
-        val sc1 = Cell(0, "A1", "3")
-        val sc2 = Cell(0, "B1", "1")
-        val sc3 = Cell(0, "C1", "2")
-        val sc4 = Cell(0, "D1", "1")
-        val sc5 = Cell(0, "E1", "1")
-
-        val sGraph = ReferenceGraph(
-            mapOf(
-                0 to mapOf(
-                    sc1.cell to Pair(sc1.value!!, setOf(sc2, sc3)),
-                    sc2.cell to Pair(sc2.value!!, setOf()),
-                    sc3.cell to Pair(sc3.value!!, setOf(sc4, sc5)),
-                    sc4.cell to Pair(sc4.value!!, setOf()),
-                    sc5.cell to Pair(sc5.value!!, setOf())
-                )
-            )
-        )
-
-        val c1 = Cell(0, "A1", "3", "B1 + C1")
-        val c2 = Cell(0, "B1", "1")
-        val c3 = Cell(0, "C1", "2", "D1 + E1")
-        val c4 = Cell(0, "D1", "1")
-        val c5 = Cell(0, "E1", "1")
-
+    fun noErrorTest2() {
         val service =
-            PropagatedErrorsService(workbook(c1, c2, c3, c4, c5), sGraph, solutionMap(listOf(sc1, sc2, sc3, sc4, sc5)))
-        val res = service.findAllPropagatedErrors(listOf(c1))
+            PropagatedErrorsService(
+                workbook(
+                    SimpleTestCase.c1,
+                    SimpleTestCase.c2,
+                    SimpleTestCase.c3,
+                    SimpleTestCase.c4,
+                    SimpleTestCase.c5
+                ),
+                SimpleTestCase.sGraph, SimpleTestCase.sMap
+            )
+        val res = service.findAllPropagatedErrors(listOf(SimpleTestCase.c1))
 
         assertEquals(setOf<Cell>(), res)
+    }
+
+    @Test
+    fun manyPropagatedErrorsTest() {
+        val c1 = ComplexTestCase.c1
+        val c2 = Cell(0, "B1", formula = "SUM(B2:B3)")
+        val c3 = Cell(0, "C1", formula = "D1 + E1 - 1")
+        val c4 = ComplexTestCase.c4
+        val c5 = ComplexTestCase.c5
+        val c6 = ComplexTestCase.c6
+        val c7 = Cell(0, "B3", formula = "D3 - D4 + 7")
+        val c8 = ComplexTestCase.c8
+        val c9 = ComplexTestCase.c9
+        val c10 = Cell(0, "D4", formula = "1 + 2")
+
+        val service =
+            PropagatedErrorsService(
+                workbook(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10),
+                ComplexTestCase.sGraph,
+                ComplexTestCase.sMap
+            )
+        val res = service.findAllPropagatedErrors(listOf(c1))
+
+        assertEquals(setOf(c2, c3, c7), res)
+    }
+
+    @Test
+    fun manyPropagatedErrorsTest2() {
+        val c1 = Cell(0, "A1", formula = "B1 + C1 + 2")
+        val c2 = Cell(0, "B1", formula = "SUM(B2:B4)") //ComplexTestCase.c2
+        val c3 = Cell(0, "C1", formula = "D1 + E1 - 1")
+        val c4 = ComplexTestCase.c4
+        val c5 = ComplexTestCase.c5
+        val c6 = ComplexTestCase.c6
+        val c7 = Cell(0, "B3", formula = "D3 - D4 + 7")
+        val c8 = ComplexTestCase.c8
+        val c9 = ComplexTestCase.c9
+        val c10 = ComplexTestCase.c10
+
+        val service =
+            PropagatedErrorsService(
+                workbook(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10),
+                ComplexTestCase.sGraph,
+                ComplexTestCase.sMap
+            )
+        val res = service.findAllPropagatedErrors(listOf(c1))
+
+        assertEquals(setOf(c1, c3, c7), res)
+    }
+
+    @Test
+    fun sumTest() {
+        val c1 = Cell(0, "A1", formula = "SUM(B1:D1)")
+        val c2 = SumTestCase.c2
+        val c3 = SumTestCase.c3
+        val c4 = SumTestCase.c4
+        val c5 = SumTestCase.c5
+
+        val service =
+            PropagatedErrorsService(workbook(c1, c2, c3, c4, c5), SumTestCase.sGraph, SumTestCase.sMap)
+        val res = service.findAllPropagatedErrors(listOf(c1))
+
+        assertEquals(setOf(c1), res)
+    }
+
+    class BasicTestCase {
+        companion object {
+            val c1 = Cell(0, "A1", "3", "B1 + C1")
+            val c2 = Cell(0, "B1", "1")
+            val c3 = Cell(0, "C1", "2")
+
+            val sGraph = ReferenceGraph(
+                mapOf(
+                    0 to mapOf(
+                        c1.cell to Pair(c1.value!!, setOf(c2, c3)),
+                        c2.cell to Pair(c2.value!!, setOf()),
+                        c3.cell to Pair(c3.value!!, setOf())
+                    )
+                )
+            )
+
+            val sMap = solutionMap(listOf(c1, c2, c3))
+        }
+    }
+
+    class SimpleTestCase {
+        companion object {
+            val c1 = Cell(0, "A1", "3", "B1 + C1")
+            val c2 = Cell(0, "B1", "1")
+            val c3 = Cell(0, "C1", "2", "D1 + E1")
+            val c4 = Cell(0, "D1", "1")
+            val c5 = Cell(0, "E1", "1")
+
+            val sGraph = ReferenceGraph(
+                mapOf(
+                    0 to mapOf(
+                        c1.cell to Pair(c1.value!!, setOf(c2, c3)),
+                        c2.cell to Pair(c2.value!!, setOf()),
+                        c3.cell to Pair(c3.value!!, setOf(c4, c5)),
+                        c4.cell to Pair(c4.value!!, setOf()),
+                        c5.cell to Pair(c5.value!!, setOf())
+                    )
+                )
+            )
+
+            val sMap = solutionMap(listOf(c1, c2, c3, c4, c5))
+        }
+    }
+
+    class ComplexTestCase {
+        companion object {
+            val c1 = Cell(0, "A1", "13", "B1 + C1")
+            val c2 = Cell(0, "B1", "11", "SUM(B2:B4)")
+            val c3 = Cell(0, "C1", "2", "D1 + E1")
+            val c4 = Cell(0, "D1", "1")
+            val c5 = Cell(0, "E1", "1")
+            val c6 = Cell(0, "B2", "1")
+            val c7 = Cell(0, "B3", "9", "D3 - D4 + 5")
+            val c8 = Cell(0, "B4", "1")
+            val c9 = Cell(0, "D3", "7")
+            val c10 = Cell(0, "D4", "3")
+
+            val sGraph = ReferenceGraph(
+                mapOf(
+                    0 to mapOf(
+                        c1.cell to Pair(c1.value!!, setOf(c2, c3)),
+                        c2.cell to Pair(c2.value!!, setOf(c6, c7, c8)),
+                        c3.cell to Pair(c3.value!!, setOf(c4, c5)),
+                        c4.cell to Pair(c4.value!!, setOf()),
+                        c5.cell to Pair(c5.value!!, setOf()),
+                        c6.cell to Pair(c6.value!!, setOf()),
+                        c7.cell to Pair(c7.value!!, setOf(c9, c10)),
+                        c8.cell to Pair(c8.value!!, setOf()),
+                        c9.cell to Pair(c9.value!!, setOf()),
+                        c10.cell to Pair(c10.value!!, setOf())
+                    )
+                )
+            )
+
+            val sMap = solutionMap(listOf(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10))
+        }
+    }
+
+    class SumTestCase {
+        companion object {
+            val c1 = Cell(0, "A1", "10", "SUM(B1:E1)")
+            val c2 = Cell(0, "B1", "1")
+            val c3 = Cell(0, "C1", "2")
+            val c4 = Cell(0, "D1", "3")
+            val c5 = Cell(0, "E1", "4")
+
+            val sGraph = ReferenceGraph(
+                mapOf(
+                    0 to mapOf(
+                        c1.cell to Pair(c1.value!!, setOf(c2, c3, c4, c5)),
+                        c2.cell to Pair(c2.value!!, setOf()),
+                        c3.cell to Pair(c3.value!!, setOf()),
+                        c4.cell to Pair(c4.value!!, setOf()),
+                        c5.cell to Pair(c5.value!!, setOf())
+                    )
+                )
+            )
+
+            val sMap = solutionMap(listOf(c1, c2, c3, c4, c5))
+        }
     }
 }
