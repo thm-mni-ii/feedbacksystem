aa4ddb5abe7c0ea0fd575c29444e5896dd7231f4
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-01-10 11:19:17 +0100

    refactor(excel-checker): declare default properties in data class body
    Co-authored-by: Max Stephan <max.stephan@mni.thm.de>

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/Cell.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/Cell.kt
@@ -1,26 +1,16 @@
 package de.thm.ii.fbs.model.v2.checker.excel
 
-data class Cell(val sheet: Int, val cell: String, val value: String? = null, val formula: String? = null) {
+data class Cell(val sheet: Int, val cell: String) {
+    var value: String? = null
+    var formula: String? = null
+
     companion object {
         const val SHEET_DELIMITER = "!"  // TODO implement delimiter for ODF
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Cell
-
-        if (sheet != other.sheet) return false
-        if (cell != other.cell) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = sheet
-        result = 31 * result + cell.hashCode()
-        return result
+    constructor(sheet: Int, cell: String, value: String? = null, formula: String?) : this(sheet, cell) {
+        this.value = value
+        this.formula = formula
     }
 
 
