912cb9dc8467129f4f145e1b5795ec1e2af243aa
nicosoethe <ni.soethe@gmail.com>
2023-01-20 13:13:32 +0100

    fixing more linting problems

--- modules/fbs-core/web/src/app/page-components/course-preview/course-preview.component.ts
+++ modules/fbs-core/web/src/app/page-components/course-preview/course-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Course } from "../../model/Course";
 import { Router } from "@angular/router";
 
