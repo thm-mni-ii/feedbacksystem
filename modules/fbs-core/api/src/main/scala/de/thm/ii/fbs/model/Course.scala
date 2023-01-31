b512c9060d4af36318892732071d347be804b0fa
Timon Pellekoorne <timon.pellekoorne@mni.thm.de>
2023-01-31 11:59:48 +0100

    [Course] added password to constructor

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/model/Course.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/model/Course.scala
@@ -8,4 +8,4 @@ package de.thm.ii.fbs.model
   * @param visible The visibility of the course, false = invisible
   * @param id The id of the course, if 0, then none was assigned.
   */
-case class Course(name: String, description: String = "", visible: Boolean = true, id: Int = 0, semesterId: Option[Int] = None)
+case class Course(name: String, description: String = "", visible: Boolean = true, id: Int = 0, semesterId: Option[Int] = None, password: Option[String] = None)
