8f06d00d2107876f50561da7f64c2b876e31535a
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-02-09 13:01:50 +0100

    refactor(excel-checker): renames cell results

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/AnalysisResult.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/checker/excel/AnalysisResult.kt
@@ -1,12 +1,12 @@
 package de.thm.ii.fbs.model.v2.checker.excel
 
 data class AnalysisResult(
-    private val cellResults: HashMap<Cell, CellResult> = HashMap(),
+    private val errorCellResults: HashMap<Cell, CellResult> = HashMap(),
     private val subtasks: HashMap<Int, MutableSet<Cell>> = HashMap()
 ) {
 
     fun addCellResult(cell: Cell, isPropagated: Boolean = false) {
-        cellResults[cell] = CellResult(isPropagated)
+        errorCellResults[cell] = CellResult(isPropagated)
     }
 
     fun addCellToSubtask(id: Int, cell: Cell) {
@@ -14,14 +14,14 @@ data class AnalysisResult(
     }
 
     fun getAllErrorCells(): Collection<Cell> {
-        return cellResults.keys
+        return errorCellResults.keys
     }
 
     fun getErrorCells(): Collection<Cell> {
-        return cellResults.entries.filter { entry -> !entry.value.isPropagated }.map { entry -> entry.key }.toSet()
+        return errorCellResults.entries.filter { entry -> !entry.value.isPropagated }.map { entry -> entry.key }.toSet()
     }
 
     fun getPropagatedErrorCells(): Collection<Cell> {
-        return cellResults.entries.filter { entry -> entry.value.isPropagated }.map { entry -> entry.key }.toSet()
+        return errorCellResults.entries.filter { entry -> entry.value.isPropagated }.map { entry -> entry.key }.toSet()
     }
 }
