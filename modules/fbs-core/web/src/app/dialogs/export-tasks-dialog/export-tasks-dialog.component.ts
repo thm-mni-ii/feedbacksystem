d7f00d52445f9355168bf1746671ad3452cf2a1a
nicosoethe <104082549+nicosoethe@users.noreply.github.com>
2023-02-13 12:53:13 +0100

    tasks can be selected

--- modules/fbs-core/web/src/app/dialogs/export-tasks-dialog/export-tasks-dialog.component.ts
+++ modules/fbs-core/web/src/app/dialogs/export-tasks-dialog/export-tasks-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from "@angular/core";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { Task } from "../../model/Task";
 
 @Component({
@@ -8,10 +8,43 @@ import { Task } from "../../model/Task";
   styleUrls: ["./export-tasks-dialog.component.scss"],
 })
 export class ExportTasksDialogComponent implements OnInit {
-  tasks: Task[];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  tasks: any[] = [];
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<ExportTasksDialogComponent>
+  ) {}
 
   ngOnInit(): void {
-    this.tasks = this.data.tasks;
+    this.createList();
+  }
+
+  export() {
+    console.log("hallo");
+    this.dialogRef.close({ success: true });
+  }
+
+  closeDialog() {
+    this.dialogRef.close({ success: false });
+  }
+
+  createList() {
+    for (var task of this.data.tasks) {
+      this.tasks.push({
+        id: task.id,
+        name: task.name,
+        index: this.tasks.length,
+        selected: false,
+      });
+    }
+  }
+
+  select(index: number) {
+    console.log(this.tasks[index].selected);
+    if (this.tasks[index].selected) {
+      this.tasks[index].selected = false;
+    } else {
+      this.tasks[index].selected = true;
+    }
+    console.log(this.tasks[index].selected);
   }
 }
