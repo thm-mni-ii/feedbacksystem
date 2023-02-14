2bb782ac1532b3ca85591a3dd62fd6fe34b26b95
Max Stephan <max.stephan@mni.thm.de>
2023-02-14 14:41:19 +0100

    feat(fbs-core.api): added ExcelCheckerServiceV2

---
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/checker/excel/ExcelCheckerServiceV2.kt
@@ -0,0 +1,28 @@
+package de.thm.ii.fbs.services.v2.checker.excel
+
+import de.thm.ii.fbs.model.v2.checker.excel.AnalysisResult
+import de.thm.ii.fbs.model.v2.checker.excel.Cell
+import de.thm.ii.fbs.model.v2.checker.excel.ErrorAnalysisSolution
+import de.thm.ii.fbs.model.v2.checker.excel.handler.ErrorHandler
+import de.thm.ii.fbs.model.v2.checker.excel.handler.PropagatedErrorHandler
+import de.thm.ii.fbs.services.v2.handler.HandlerService
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.springframework.stereotype.Service
+
+@Service
+class ExcelCheckerServiceV2(private val errorAnalysisSolutionService: ErrorAnalysisSolutionService) {
+    fun check(configurationId: Int, submission: XSSFWorkbook): AnalysisResult? {
+        val solution = errorAnalysisSolutionService.getSolution(configurationId) ?: return null
+        val result = AnalysisResult()
+        val handlerService = HandlerService(ErrorHandler(result), PropagatedErrorHandler(result))
+
+        val errorAnalysisService = ErrorAnalysisService(submission, solution.graph, solution.solution, handlerService)
+        errorAnalysisService.findAllErrors(getOutputCells(solution))
+
+        return result
+    }
+
+    private fun getOutputCells(solution: ErrorAnalysisSolution): List<Cell> {
+        return solution.graph.data.vertexSet().filter { c -> solution.graph.isOutput(c) } // TODO: Improve
+    }
+}
