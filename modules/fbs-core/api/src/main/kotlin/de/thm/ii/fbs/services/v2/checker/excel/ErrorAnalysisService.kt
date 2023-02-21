d5676d64bad8deb5b172f2dc01dbed5bf2fe2d68
Max Stephan <max.stephan@mni.thm.de>
2023-02-21 07:34:41 +0100

    [Excel-Checker] Store dependency graph of solution file in database (#1172)
    * feat(fbs-core.api): added CheckerStorage
    
    * feat(fbs-core.api): clean up test api
    
    * feat(fbs-core.api): added ErrorAnalysisSolutionService
    
    * chore(fbs-core.api): allow to import kotlin classes into scala
    
    * feat(fbs-core.api): added ExcelCheckerServiceV2
    
    * feat(fbs-core.api.ExcelCheckerService): use v2 Service if experimental flag is present
    
    * feat(fbs-core.api.ErrorAnalysisService): allow cells to have null values
    
    * feat(fbs-core.api.ExcelChecker): store value of all cells in the ReferenceGraph
    
    * feat(fbs-core.api): added CheckerServiceOnSecondaryFileUpload trait
    
    * feat(fbs-core.api.ExcelChecker): Store the Solution if the Configuration Changes
    
    * feat(fbs-core.api.ReferenceGraph): implement equals
    
    * test(fbs-core.api): added ReferenceGraphSerialisation Test

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/checker/excel/ErrorAnalysisService.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/checker/excel/ErrorAnalysisService.kt
@@ -1,8 +1,8 @@
 package de.thm.ii.fbs.services.v2.checker.excel
 
-import de.thm.ii.fbs.model.v2.checker.excel.handler.context.ErrorAnalysisContext
 import de.thm.ii.fbs.model.v2.checker.excel.Cell
 import de.thm.ii.fbs.model.v2.checker.excel.ReferenceGraph
+import de.thm.ii.fbs.model.v2.checker.excel.handler.context.ErrorAnalysisContext
 import de.thm.ii.fbs.services.v2.handler.HandlerService
 import de.thm.ii.fbs.utils.v2.handler.When
 import de.thm.ii.fbs.utils.v2.spreadsheet.SpreadsheetValueParser.Companion.setValueOfCell
@@ -15,8 +15,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 class ErrorAnalysisService(
     private val workbook: XSSFWorkbook,
-    private val graph: ReferenceGraph, /* TODO get dependency graph from solution entry in db */
-    private val solution: Map<Cell, String>, /* TODO maybe usa a kotlin set with indexing; get solution values from solution entry in db */
+    private val graph: ReferenceGraph,
+    private val solution: Map<Cell, String?>, /* TODO maybe usa a kotlin set with indexing; */
     private val handleService: HandlerService<ErrorAnalysisContext, Unit>? = null
 ) {
     private val evaluator: FormulaEvaluator = workbook.creationHelper.createFormulaEvaluator()
@@ -59,7 +59,7 @@ class ErrorAnalysisService(
         if (!cellEqualsSolution(cell, workbookCell)) {
             errors.add(cell) // add to original errors set
             handleService?.runHandlers(ErrorAnalysisContext(errors, perrors, cell), When.ONERROR)
-            setValueOfCell(workbookCell, solution[cell]!!) // substitute cell value with solution value
+            setValueOfCell(workbookCell, solution[cell]) // substitute cell value with solution value
             evaluator.notifyUpdateCell(workbookCell)
         } else {
             perrors.add(cell)
