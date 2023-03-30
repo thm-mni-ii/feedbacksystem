896364374dfe1ca640a006aff6a06272d683934b
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2023-03-30 11:45:59 +0200

    test(api): add missing submission argument

--- modules/fbs-core/api/src/test/scala/de/thm/ii/fbs/services/persistence/SubmissionServiceTest.scala
+++ modules/fbs-core/api/src/test/scala/de/thm/ii/fbs/services/persistence/SubmissionServiceTest.scala
@@ -70,7 +70,9 @@ class SubmissionServiceTest {
 
   @Test
   def testGetOrHiddenWithoutHidden(): Unit = {
-    val submission = Submission(DateTime.now().toDate, done = true, 0, results = List(CheckResult(1, "Test", "test", 1, null)).toArray)
+    val submission = Submission(DateTime.now().toDate, done = true, 0,
+      results = List(CheckResult(1, "Test", "test", 1, null)).toArray,
+      additionalInformation = None)
     val res = submissionService.getOrHidden(submission, hideResult = false, adminPrivileged = false)
 
     Assert.assertFalse(submission.isHidden)
@@ -79,7 +81,9 @@ class SubmissionServiceTest {
 
   @Test
   def testGetOrHiddenWithHidden(): Unit = {
-    val submission = Submission(DateTime.now().toDate, done = true, 0, results = List(CheckResult(1, "Test", "test", 1, null)).toArray)
+    val submission = Submission(DateTime.now().toDate, done = true, 0,
+      results = List(CheckResult(1, "Test", "test", 1, null)).toArray,
+      additionalInformation = None)
     val res = submissionService.getOrHidden(submission, hideResult = true, adminPrivileged = false)
 
     Assert.assertTrue(res.isHidden)
@@ -88,7 +92,9 @@ class SubmissionServiceTest {
 
   @Test
   def testGetOrHiddenWithHiddenAdmin(): Unit = {
-    val submission = Submission(DateTime.now().toDate, done = true, 0, results = List(CheckResult(1, "Test", "test", 1, null)).toArray)
+    val submission = Submission(DateTime.now().toDate, done = true, 0,
+      results = List(CheckResult(1, "Test", "test", 1, null)).toArray,
+      additionalInformation = None)
     val res = submissionService.getOrHidden(submission, hideResult = true, adminPrivileged = true)
 
     Assert.assertFalse(submission.isHidden)
