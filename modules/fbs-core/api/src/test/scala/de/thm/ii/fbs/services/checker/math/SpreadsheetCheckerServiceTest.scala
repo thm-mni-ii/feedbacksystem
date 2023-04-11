0c69037b97ee900ed18148a9ba8393bcd5d5ee90
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2023-04-11 10:31:41 +0200

    test(math-checker): fix spreadsheet checker service test

--- modules/fbs-core/api/src/test/scala/de/thm/ii/fbs/services/checker/math/SpreadsheetCheckerServiceTest.scala
+++ modules/fbs-core/api/src/test/scala/de/thm/ii/fbs/services/checker/math/SpreadsheetCheckerServiceTest.scala
@@ -1,43 +1,62 @@
 package de.thm.ii.fbs.services.checker.math
 
 import de.thm.ii.fbs.TestApplication
+import de.thm.ii.fbs.mathParser.MathParserHelper
 import org.junit.{Assert, Test}
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.{ActiveProfiles, TestPropertySource}
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
 import scala.collection.immutable
 import scala.jdk.CollectionConverters.MapHasAsJava
+import java.util.{Map => UtilMap}
 
 @RunWith(classOf[SpringRunner])
 @SpringBootTest(classes = Array(classOf[TestApplication]))
 @ActiveProfiles(Array("test"))
 class SpreadsheetCheckerServiceTest {
   @Autowired
-  private val spreadsheetCheckerService: SpreadsheetCheckerService = null
+  val spreadsheetCheckerService: SpreadsheetCheckerService = null
+
+  private def toMathJson(inp: String): String = MathParserHelper.toMathJson(MathParserHelper.parse(inp))
+
+  private def compareMock(enteredValue: String, value: String, decimals: Int): Boolean = spreadsheetCheckerService.compare(
+    toMathJson(enteredValue),
+  toMathJson(value),
+    decimals
+  )
+
+  private def checkMock(fields: Seq[(String, String)], submittedFields: UtilMap[String, String], decimals: Int):
+    (Int, Seq[spreadsheetCheckerService.CheckResult]) =
+    spreadsheetCheckerService.check(
+      fields.map(t => (t._1, toMathJson(t._2))),
+      submittedFields.entrySet().toArray(Array[UtilMap.Entry[String, String]]())
+        .map((t: UtilMap.Entry[String, String]) => t.getKey -> toMathJson(t.getValue)).toMap.asJava,
+      decimals
+    )
 
   @Test
   def decimalCompareTest(): Unit = {
-    Assert.assertTrue(spreadsheetCheckerService.compare("1,234", "1,234", 3))
-    Assert.assertTrue(spreadsheetCheckerService.compare("1,23456", "1,23456", 3))
-    Assert.assertTrue(spreadsheetCheckerService.compare("1,23456", "1,235", 3))
-    Assert.assertFalse(spreadsheetCheckerService.compare("1,23456", "1,24", 4))
-    Assert.assertFalse(spreadsheetCheckerService.compare("1,23456", "1,23", 4))
-    Assert.assertFalse(spreadsheetCheckerService.compare("1,23456", "1,2345", 4))
+    Assert.assertTrue(compareMock("1,234", "1,234", 3))
+    Assert.assertTrue(compareMock("1,23456", "1,23456", 3))
+    Assert.assertTrue(compareMock("1,23456", "1,235", 3))
+    Assert.assertFalse(compareMock("1,23456", "1,24", 4))
+    Assert.assertFalse(compareMock("1,23456", "1,23", 4))
+    Assert.assertFalse(compareMock("1,23456", "1,2345", 4))
   }
 
   @Test
   def mathCompareTest(): Unit = {
-    Assert.assertTrue(spreadsheetCheckerService.compare("1+2+3", "3+2+1", 2))
-    Assert.assertTrue(spreadsheetCheckerService.compare("2+a", "2+1a", 2))
-    Assert.assertTrue(spreadsheetCheckerService.compare("abc", "a*b*c", 2))
-    Assert.assertTrue(spreadsheetCheckerService.compare("a*2,51", "2,5*a", 1))
-    Assert.assertFalse(spreadsheetCheckerService.compare("1+2+3", "3+2*1", 2))
-    Assert.assertFalse(spreadsheetCheckerService.compare("2+a", "2+2a", 2))
-    Assert.assertFalse(spreadsheetCheckerService.compare("abc", "a*b*d", 2))
-    Assert.assertFalse(spreadsheetCheckerService.compare("a*2,56", "2,5*a", 1))
+    Assert.assertTrue(compareMock("1+2+3", "3+2+1", 2))
+    Assert.assertTrue(compareMock("2+a", "2+1a", 2))
+    Assert.assertTrue(compareMock("abc", "a*b*c", 2))
+    Assert.assertTrue(compareMock("a*2,51", "2,5*a", 1))
+    Assert.assertFalse(compareMock("1+2+3", "3+2*1", 2))
+    Assert.assertFalse(compareMock("2+a", "2+2a", 2))
+    Assert.assertFalse(compareMock("abc", "a*b*d", 2))
+    Assert.assertFalse(compareMock("a*2,56", "2,5*a", 1))
   }
 
   @Test
@@ -45,12 +64,9 @@ class SpreadsheetCheckerServiceTest {
     val fields = immutable.Seq(("a)", "7a-8b"), ("b)", "4,8b-0,3a"))
     val submittedCorrect = immutable.Map("a)" -> "7a-8b", "b)" -> "4,8b-0,3a").asJava
     val submittedWrong = immutable.Map("a)" -> "7a-8b", "b)" -> "4,8b-0,4a").asJava
-    val submittedInvalid = immutable.Map("a)" -> "7a-#8b", "b)" -> "4,8b-0,3a").asJava
-    val (status1, _) = spreadsheetCheckerService.check(fields, submittedCorrect, 2)
+    val (status1, _) = checkMock(fields, submittedCorrect, 2)
     Assert.assertEquals(2, status1)
-    val (status2, _) = spreadsheetCheckerService.check(fields, submittedWrong, 2)
+    val (status2, _) = checkMock(fields, submittedWrong, 2)
     Assert.assertEquals(1, status2)
-    val (status3, _) = spreadsheetCheckerService.check(fields, submittedInvalid, 2)
-    Assert.assertEquals(1, status3)
   }
 }
