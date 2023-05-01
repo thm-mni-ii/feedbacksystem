0df29cb5ff295b3174d0078762ec7c4922a45745
SiarNoozai <siyer@outlook.de>
2023-05-01 14:59:04 +0200

    new interface files

---
+++ modules/fbs-core/web/src/app/model/ExcelCheckResult.ts
@@ -0,0 +1,15 @@
+export interface ExcelCheckResult {
+  excercises: ExcelExcercise[];
+}
+
+export interface ExcelExcercise {
+    name: string;
+    cells: ExcerciseCell[];
+    succesfulAttempt: boolean;
+}
+
+export interface ExcerciseCell {
+    identifier: string; // the identified cell
+    errorHint: string; // the tip/Hint for finding the error
+    isProperGated: boolean; // the propergated error 
+}
