8f9471e910e129453191f230cefd2513103c4f8b
Youssef Mellouli <youssef.mellouli@mni.thm.de>
2023-05-04 11:36:42 +0200

    added progress bar to my courses

--- modules/fbs-core/web/src/app/page-components/my-courses/my-courses.component.ts
+++ modules/fbs-core/web/src/app/page-components/my-courses/my-courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { TitlebarService } from "../../service/titlebar.service";
 import { Course } from "../../model/Course";
@@ -6,6 +6,8 @@ import { AuthService } from "../../service/auth.service";
 import { mergeMap, startWith } from "rxjs/operators";
 import { UntypedFormControl } from "@angular/forms";
 import { CourseRegistrationService } from "../../service/course-registration.service";
+import { TaskService } from "src/app/service/task.service";
+import { SubmissionService } from "src/app/service/submission.service";
 
 /**
  * Show all registered courses
@@ -16,25 +18,45 @@ import { CourseRegistrationService } from "../../service/course-registration.ser
   styleUrls: ["./my-courses.component.scss"],
 })
 export class MyCoursesComponent implements OnInit {
+  @Input() data: Course;
   constructor(
     private titlebar: TitlebarService,
     private courseRegistrationService: CourseRegistrationService,
-    private authService: AuthService
-  ) {}
+    private taskService: TaskService,
+    private submissionService: SubmissionService,
+    private authService: AuthService,
 
+  ) { }
+
+
+  userID: number;
+  totalTasks: any = 0;
   courses: Observable<Course[]> = of();
   filteredCourses: Observable<Course[]> = of();
   control: UntypedFormControl = new UntypedFormControl();
+  myTasks: any[] = [];
+  passed: number = 0;
+  failed:number = 0; 
+  offen:number ;
+  legends=["green","red","#1E457C"];
+ 
+
+
+
 
   ngOnInit() {
+    
     this.titlebar.emitTitle("Meine Kurse");
-    const userID = this.authService.getToken().id;
-    this.courses = this.courseRegistrationService.getRegisteredCourses(userID);
-
+    this.userID = this.authService.getToken().id;
+    this.courses = this.courseRegistrationService.getRegisteredCourses(this.userID);
+    
+    this.getTaskProgress();
     this.filteredCourses = this.control.valueChanges.pipe(
       startWith(""),
       mergeMap((value) => this._filter(value))
     );
+
+
   }
 
   private _filter(value: string): Observable<Course[]> {
@@ -54,7 +76,97 @@ export class MyCoursesComponent implements OnInit {
     );
   }
 
+
+
   private _normalizeValue(value: string): string {
     return value.toLowerCase().replace(/\s/g, "");
   }
+
+  countResults(allSubmissions: any[], failed: boolean) {
+    this.offen=0;
+    console.log("extern ",this.offen)
+    let aufchecker:boolean=false;
+    if (allSubmissions.length != 0) {
+      for (var submission of allSubmissions) {
+        if (!failed) {
+          this.passed++;
+          aufchecker=true;
+          
+          break;
+        }
+        for (var checker of submission.results) {
+          if (checker.exitCode == 0) {
+            
+            failed = false;
+            this.passed++;
+            aufchecker=true;
+            break;
+          }
+        }
+      }
+      if (failed) {
+        
+        this.failed++;
+        aufchecker=true;
+        
+      }
+    }
+    if(aufchecker==false){
+      this.offen++;
+    }
+    console.log("intern ",this.offen);
+  }
+
+
+  getTaskProgress() {
+    
+    this.courses.subscribe((courses) => {
+      courses.forEach((course) => {
+        this.getProgress(course.id);
+        
+
+        
+        this.taskService.getAllTasks(course.id).subscribe((tasks) => {
+          tasks.forEach((task) => {
+            this.myTasks.push(task);
+          });
+          this.totalTasks = this.myTasks.length;
+          
+        
+        
+        });
+          
+      });
+          
+    
+      
+    
+
+      
+    });
+  }
+
+  getProgress(courseId: number) {
+    this.taskService.getAllTasks(courseId).subscribe(
+      (allTasks) => {
+        
+        if (allTasks.length > 0) {
+          
+          for (var task of allTasks) {
+            let failed = true;
+            this.submissionService
+              .getAllSubmissions(this.userID, courseId, task.id)
+              .subscribe((allSubmissions) => {
+                this.countResults(allSubmissions, failed);
+                
+              });
+          }
+        }
+      },
+      () => { }
+    );
+    }
+
+
+
 }
