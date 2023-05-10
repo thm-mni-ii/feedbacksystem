89ce73082bc5b2583858daa89a446cc0f37d0d3d
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2023-05-10 09:05:23 +0200

    fix(core): repair query in submission service

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/persistence/SubmissionService.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/persistence/SubmissionService.scala
@@ -135,7 +135,7 @@ class SubmissionService {
     * @return The found submission
     */
   def getOneWithoutUser(id: Int, addExtInfo: Boolean = false): Option[Submission] = reduceSubmissions(DB.query(
-    s"SELECT submission_id, task_id, user_id, submission_time, configuration_id, exit_code, result_text, user_task_submission.is_in_block_storage, " +
+    s"SELECT submission_id, user_task_submission.task_id, user_id, submission_time, configuration_id, exit_code, result_text, user_task_submission.is_in_block_storage, " +
       s"checker_type${if (addExtInfo) ", ext_info" else ""} " +
       "FROM user_task_submission LEFT JOIN checker_result using (submission_id) LEFT JOIN checkrunner_configuration using (configuration_id) " +
       "WHERE submission_id = ?",
