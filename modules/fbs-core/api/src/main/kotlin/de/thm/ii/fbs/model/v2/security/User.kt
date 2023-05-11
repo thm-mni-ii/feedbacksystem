07e573d11051d9eb6c5a633d33680f4249e201b2
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-05-11 17:02:07 +0200

    [CI] Add linter for kotlin (#1260)
    * chore(ci): adds ktlint plugin to gradle
    
    * ci(fbs-core.api): run ktlint on linting
    
    * ci(fbs-core.api): problem matcher for ktlint
    
    * ci(fbs-core.api): fix add matcher
    
    * ci(fbs-core.api): fix add matcher
    
    * ci(fbs-core.api): fix add matcher
    
    * ci(fbs-core.api): typo
    
    * refactor: fix ktlint errors
    
    * fix(ci): typo

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/security/User.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/security/User.kt
@@ -1,3 +1,5 @@
+@file:Suppress("ktlint:no-wildcard-imports")
+
 package de.thm.ii.fbs.model.v2.security
 
 import javax.persistence.*
@@ -5,10 +7,10 @@ import javax.persistence.*
 @Entity
 @Table(name = "user")
 class User(
-        @Column(nullable = false)
-        var username: String,
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "user_id")
-        var id: Int? = null,
+    @Column(nullable = false)
+    var username: String,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    var id: Int? = null
 )
--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/security/TokenV2Service.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/security/TokenV2Service.kt
@@ -7,39 +7,38 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-
 import java.util.Date
 import javax.xml.bind.DatatypeConverter
 
 @Component
 class TokenV2Service(
-        @Value("\${jwt.secret}")
-        private val jwtSecret: String,
+    @Value("\${jwt.secret}")
+    private val jwtSecret: String
 ) {
     fun issue(subject: String, expires: Int) {
         Jwts.builder()
-                .setSubject(subject)
-                .setIssuedAt(Date())
-                .setExpiration(Date(Date().time + (1000 * expires)))
-                .signWith(SignatureAlgorithm.HS256, jwtSecretEncoding())
-                .compact()
+            .setSubject(subject)
+            .setIssuedAt(Date())
+            .setExpiration(Date(Date().time + (1000 * expires)))
+            .signWith(SignatureAlgorithm.HS256, jwtSecretEncoding())
+            .compact()
     }
 
     fun verify(token: String): String? =
-            try {
-                val claims: Claims = Jwts.parser().setSigningKey(jwtSecret.toByteArray()).parseClaimsJws(token).body
-                claims["sub"] as String
-            } catch (e: ExpiredJwtException) {
-                null
-            }
+        try {
+            val claims: Claims = Jwts.parser().setSigningKey(jwtSecret.toByteArray()).parseClaimsJws(token).body
+            claims["sub"] as String
+        } catch (e: ExpiredJwtException) {
+            null
+        }
 
     fun verifyLegacyToken(token: String): LegacyToken? =
-            try {
-                val claims: Claims = Jwts.parser().setSigningKey(jwtSecret.toByteArray()).parseClaimsJws(token).body
-                LegacyToken(claims["id"] as Int, claims["username"] as String)
-            } catch (e: ExpiredJwtException) {
-                null
-            }
+        try {
+            val claims: Claims = Jwts.parser().setSigningKey(jwtSecret.toByteArray()).parseClaimsJws(token).body
+            LegacyToken(claims["id"] as Int, claims["username"] as String)
+        } catch (e: ExpiredJwtException) {
+            null
+        }
 
     fun jwtSecretEncoding(): String = DatatypeConverter.printBase64Binary(jwtSecret.toByteArray())
 }
