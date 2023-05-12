5a3dbe889ef82d03c1be3999ed9eb2ba5134467e
Tim Steinmüller <tim.steinmueller@online.de>
2023-05-12 11:08:01 +0200

    linting errors

--- modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/security/TokenV2ServiceTest.kt
+++ modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/security/TokenV2ServiceTest.kt
@@ -1,15 +1,7 @@
 package de.thm.ii.fbs.security
 
-import de.thm.ii.fbs.model.v2.checker.excel.CellResult
 import de.thm.ii.fbs.services.v2.security.TokenV2Service
-import org.junit.Before
 import org.junit.Test
-import org.junit.jupiter.api.Assertions
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.test.context.SpringBootTest
-import javax.annotation.PostConstruct
-
 
 class TokenV2ServiceTest() {
     private val tokenV2Service: TokenV2Service = TokenV2Service("123")
@@ -22,6 +14,7 @@ class TokenV2ServiceTest() {
         assert(verified != null)
         assert(verifiedLegacy != null)
     }
+    
     @Test
     fun expiredToken() {
         val token = tokenV2Service.issue("Test", 2)
