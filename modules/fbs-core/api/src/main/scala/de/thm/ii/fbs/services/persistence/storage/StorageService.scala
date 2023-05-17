357b2e76e0686d28a4a0e9e8e12e5303e682ceb7
timst01 <102797880+timst01@users.noreply.github.com>
2023-05-17 10:57:24 +0200

    [API] Allow Users to Download the Submitted files (#1183)
    * refactor(api): split StorageService into FsStorageService and MinioStorageService
    
    * test(api): fix SubmissionServiceTest
    
    * fix(core): fix task_id in submission queries
    
    * test(core): fix submission service test
    
    * fix(core): repair query in submission service
    
    * style(core): fix long line
    
    * fix(api): use correct file extension for content disposition when downloading submissions
    
    * fix(core): clean up tmp files when downloading course submissions
    
    * fix(api): use correct file extensions in course archive
    
    * fix(api): change course submission export format to zip
    
    * fix(core): use correct extension for excel files
    
    * fix(api): use zip for task submission export
    
    * feat(core): create task submission zip
    
    * fix(core): correct export extensions
    
    * fix(api): remove dupplicated extension dot in submission download file name
    
    * fix(core): clean up tempfile for task submission export
    
    ---------
    
    Co-authored-by: Jonas Kuche <jonas-ian.kuche@mni.thm.de>

---
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/persistence/storage/StorageService.scala
@@ -0,0 +1,198 @@
+package de.thm.ii.fbs.services.persistence.storage
+
+import _root_.org.springframework.beans.factory.annotation.{Autowired, Value}
+import _root_.org.springframework.stereotype.Component
+import _root_.org.springframework.web.multipart.MultipartFile
+import de.thm.ii.fbs.controller.exception.ResourceNotFoundException
+import de.thm.ii.fbs.model.{CheckrunnerConfiguration, Submission, storageBucketName, storageFileName}
+import de.thm.ii.fbs.services.checker.CheckerServiceFactoryService
+import de.thm.ii.fbs.services.checker.`trait`.CheckerServiceOnDelete
+import de.thm.ii.fbs.services.persistence.{MinioService, TaskService}
+
+import java.io._
+import java.nio.file._
+
+/**
+  * Handles file of tasks and submissions.
+  */
+@Component
+class StorageService extends App {
+  @Autowired
+  private val minioService: MinioService = null
+  @Autowired
+  private val checkerService: CheckerServiceFactoryService = null
+  @Autowired
+  private val taskService: TaskService = null
+  @Autowired
+  private val fsStorageService: FsStorageService = null
+  @Autowired
+  private val minioStorageService: MinioStorageService = null
+
+  /**
+    * gets the content of a the main File
+    *
+    * @param cc the Checkerunner Configuration
+    */
+  def getMainFileContent(cc: CheckrunnerConfiguration): String = {
+    if (cc.isInBlockStorage) {
+      minioStorageService.getMainFileFromBucket(cc.id)
+    } else {
+      fsStorageService.getMainFile(cc.id)
+    }
+  }
+
+  /**
+    * gets the content of a the secondary File
+    *
+    * @param cc the Checkerunner Configuration
+    */
+  def getSecondaryFileContent(cc: CheckrunnerConfiguration): String = {
+    if (cc.isInBlockStorage) {
+      minioStorageService.getSecondaryFileFromBucket(cc.id)
+    } else {
+      fsStorageService.getSecondaryFile(cc.id)
+    }
+  }
+
+  /**
+    * gets the content of a file depending on the source
+    *
+    * @param isInBlockStorage True if the content is the Minio
+    * @param submissionId     submission id
+    * @return
+    */
+  def getSolutionFileContent(isInBlockStorage: Boolean, submissionId: Int): String = {
+    if (isInBlockStorage) {
+      minioStorageService.getSolutionFileFromBucket(submissionId)
+    } else {
+      fsStorageService.getSolutionFile(submissionId)
+    }
+  }
+
+  /**
+    * Deletes the configuration files from minio or FS and the DB entry
+    *
+    * @param tid task id
+    * @param cid course id
+    * @param cc  checker configuration
+    * @throws IOException If the i/o operation fails
+    * @return
+    */
+  @throws[IOException]
+  def deleteAllConfigurations(tid: Int, cid: Int, cc: CheckrunnerConfiguration): Boolean = {
+    try {
+      if (cc.isInBlockStorage) {
+        minioStorageService.deleteConfigurationFromBucket(cc.id)
+      } else {
+        fsStorageService.deleteConfiguration(cc.id)
+      }
+      notifyCheckerDelete(tid, cc)
+      true
+    }
+    catch {
+      case e: Exception => false
+    }
+  }
+
+  /**
+    * returns a main-file depending whether it is in the bucket or not
+    *
+    * @param config CheckrunnerConfiguration
+    * @return
+    */
+  def getFileMainFile(config: CheckrunnerConfiguration): File = {
+    if (config.isInBlockStorage) {
+      minioStorageService.getFileFromBucket(storageBucketName.CHECKER_CONFIGURATION_BUCKET, storageFileName.getMainFilePath(config.id))
+    } else {
+      fsStorageService.pathToMainFile(config.id).get.toFile
+    }
+  }
+
+  /**
+    * returns a main-file depending whether it is in the bucket or not
+    *
+    * @param config CheckrunnerConfiguration
+    * @return
+    */
+  def getFileScondaryFile(config: CheckrunnerConfiguration): File = {
+    if (config.isInBlockStorage) {
+      minioStorageService.getFileFromBucket(storageBucketName.CHECKER_CONFIGURATION_BUCKET, storageFileName.getSecondaryFilePath(config.id))
+    } else {
+      fsStorageService.pathToSecondaryFile(config.id).get.toFile
+    }
+  }
+
+  /**
+    * returns a secondary-file depending whether it is in the bucket or not
+    *
+    * @param submission the Submission
+    * @return
+    */
+  def getFileSolutionFile(submission: Submission): File = {
+    if (submission.isInBlockStorage) {
+      minioStorageService.getFileFromBucket(storageBucketName.SUBMISSIONS_BUCKET, storageFileName.getSolutionFilePath(submission.id))
+    } else {
+      fsStorageService.pathToSolutionFile(submission.id).get.toFile
+    }
+  }
+
+  /**
+    * returns a String which represents the content type
+    *
+    * @param submission the Submission
+    * @return
+    */
+  def getContentTypeSolutionFile(submission: Submission): String = {
+    if (submission.isInBlockStorage) {
+      minioService.getStatsOfObject(storageBucketName.SUBMISSIONS_BUCKET, storageFileName.getSolutionFilePath(submission.id))
+    } else {
+      Files.probeContentType(fsStorageService.pathToSolutionFile(submission.id).get.toFile.toPath)
+    }
+  }
+
+  /**
+    * returns a input stream depending whether it is in the bucket or not
+    *
+    */
+  def getFileContentStream(pathFn: Int => Option[Path])(isInBlockStorage: Boolean, ccid: Int, fileName: String): InputStream = {
+    if (isInBlockStorage) {
+      new ByteArrayInputStream(minioService.getObjectAsBytes(storageBucketName.CHECKER_CONFIGURATION_BUCKET, storageFileName.getFilePath(ccid, fileName)))
+    } else {
+      pathFn(ccid) match {
+        case Some(mainFilePath) =>
+          new FileInputStream(mainFilePath.toFile)
+        case _ => throw new ResourceNotFoundException()
+      }
+    }
+  }
+
+  private def notifyCheckerDelete(tid: Int, cc: CheckrunnerConfiguration): Unit = {
+    val checker = checkerService(cc.checkerType)
+    checker match {
+      case change: CheckerServiceOnDelete =>
+        change.onCheckerConfigurationDelete(taskService.getOne(tid).get, cc)
+      case _ =>
+    }
+  }
+
+  /**
+    * Stores the Configuration File
+    *
+    * @param cc       the Check runner Configuration
+    * @param file     the File to Store
+    * @param fileName the name of the file
+    */
+  def storeConfigurationFile(cc: CheckrunnerConfiguration, file: MultipartFile, fileName: String): Unit = {
+    if (cc.isInBlockStorage) {
+      minioStorageService.storeConfigurationFileInBucket(cc.id, file, fileName)
+    } else {
+      val tempDesc = Files.createTempFile("fbs", ".tmp")
+      file.transferTo(tempDesc)
+      fsStorageService.storeConfigurationFile(cc.id, tempDesc, fileName)
+    }
+  }
+
+  def deleteSolution(sid: Int): Boolean = {
+    minioStorageService.deleteSolutionFileFromBucket(sid) || fsStorageService.deleteSolutionFile(sid)
+  }
+}
--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/util/Archiver.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/util/Archiver.scala
@@ -1,21 +1,44 @@
 package de.thm.ii.fbs.util
 
-import de.thm.ii.fbs.model.Task
+import de.thm.ii.fbs.model.{Task, User}
+import org.apache.commons.compress.archivers.ArchiveOutputStream
 import org.apache.commons.compress.archivers.tar.{TarArchiveEntry, TarArchiveOutputStream}
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream
 import org.apache.commons.compress.utils.IOUtils
 
 import java.io._
 import java.nio.file.Files
 import scala.collection.mutable.ListBuffer
+import org.apache.tika.mime.MimeType
+import org.apache.tika.mime.MimeTypeException
+import org.apache.tika.mime.MimeTypes
 
 object Archiver {
   @throws[IOException]
-  def pack(name: File, files: ArchiveFile*): Unit = {
-    val out = new TarArchiveOutputStream(new BufferedOutputStream(Files.newOutputStream(name.toPath)))
-    for (archiveFile <- files) {
-      addToArchive(out, archiveFile.file, ".", archiveFile.filename.getOrElse(archiveFile.file.getName))
+  def packSubmissions(name: File, files: List[File], users: List[User], fileExts: List[String]): Unit = {
+    val out = new ZipArchiveOutputStream(new BufferedOutputStream(Files.newOutputStream(name.toPath)))
+    for ((file, index) <- files.zipWithIndex) {
+      addToArchive(out, file, ".", s"${users(index).getName}${fileExts(index)}")
     }
     out.close()
+  }
+
+  @throws[IOException]
+  def packSubmissionsInDir(name: File, files: ListBuffer[List[File]], users: ListBuffer[List[User]], fileExts: ListBuffer[List[String]],
+                           listTaskId: List[Int]): Unit = {
+    val out = new ZipArchiveOutputStream(new BufferedOutputStream(Files.newOutputStream(name.toPath)))
+    files.zipWithIndex.foreach(listFiles => {
+      listFiles._1.zipWithIndex.foreach(f => {
+        val fileExt = fileExts(listFiles._2)(f._2)
+        addToArchive(out, f._1, s"./${listTaskId(listFiles._2)}", s"${users(listFiles._2)(f._2).getName}$fileExt")
+      })
+    })
+    out.close()
+  }
+
+  private def fileExtensionFromContentType(contentType: String): String = {
+    val allTypes = MimeTypes.getDefaultMimeTypes
+    allTypes.forName(contentType).getExtension
   }
 
   @throws[IOException]
@@ -30,10 +53,10 @@ object Archiver {
     out.close()
   }
 
-  def addToArchive(out: TarArchiveOutputStream, file: File, dir: String, name: String): Unit = {
+  def addToArchive(out: ArchiveOutputStream, file: File, dir: String, name: String): Unit = {
     val entry = dir + File.separator + name
     if (file.isFile) {
-      out.putArchiveEntry(new TarArchiveEntry(file, entry))
+      out.putArchiveEntry(out.createArchiveEntry(file, entry))
       val in = new FileInputStream(file)
       try IOUtils.copy(in, out)
       finally if (in != null) in.close()
