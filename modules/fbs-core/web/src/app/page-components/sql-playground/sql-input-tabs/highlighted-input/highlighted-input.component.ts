067bf67ccf6e2909037768b73eba303ca1ee56e0
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2023-05-18 21:56:02 +0200

    fix(playground): replace newlines with whitespaces

--- modules/fbs-core/web/src/app/page-components/sql-playground/sql-input-tabs/highlighted-input/highlighted-input.component.ts
+++ modules/fbs-core/web/src/app/page-components/sql-playground/sql-input-tabs/highlighted-input/highlighted-input.component.ts
@@ -79,7 +79,7 @@ export class HighlightedInputComponent
   }
 
   cleanUpTextAreaRegx(sqlInput: String) {
-    let temp = sqlInput.replace(/[\n\t]/g, "");
+    let temp = sqlInput.replace(/[\n\t]/g, " ");
     return temp;
   }
 
