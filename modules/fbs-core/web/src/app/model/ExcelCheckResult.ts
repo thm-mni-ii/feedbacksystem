95d7b2a5512000a19ac6c9ab7a0eca5e38080cab
SiarNoozai <siyer@outlook.de>
2023-05-19 19:16:17 +0200

    lot of changes but no success

--- modules/fbs-core/web/src/app/model/ExcelCheckResult.ts
+++ modules/fbs-core/web/src/app/model/ExcelCheckResult.ts
@@ -1,16 +1,34 @@
+// export interface ExcelCheckResult {
+//   exercises: ExcelExcercise[];
+//   table: string;
+// }
+
+// export interface ExcelExcercise {
+//   name: string;
+//   cells: ExcerciseCell[];
+//   successfulAttempt: boolean;
+// }
+
+// export interface ExcerciseCell {
+//   identifier: string; // the identified cell
+//   errorHint: string; // the tip/Hint for finding the error
+//   isProperGated: boolean; // the propergated error
+// }
+
+
 export interface ExcelCheckResult {
-  excercises: ExcelExcercise[];
-  table: object;
+  exercises: ExcelExercise[];
 }
 
-export interface ExcelExcercise {
+export interface ExcelExercise {
   name: string;
-  cells: ExcerciseCell[];
-  succesfulAttempt: boolean;
+  errorCell?: ExcelCell[];
+  table: string;
+  result: boolean;
 }
 
-export interface ExcerciseCell {
-  identifier: string; // the identified cell
-  errorHint: string; // the tip/Hint for finding the error
-  isProperGated: boolean; // the propergated error
+export interface ExcelCell {
+  cellName: string;
+  errorHint: string;
+  consequentErrorCell?: ExcelCell[];
 }
--- modules/fbs-core/web/src/app/page-components/excel-results/excel-results.component.ts
+++ modules/fbs-core/web/src/app/page-components/excel-results/excel-results.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { ExcelCheckResult } from 'src/app/model/ExcelCheckResult';
-import { Submission } from 'src/app/model/Submission';
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { ExcelCheckResult } from "src/app/model/ExcelCheckResult";
+import { Submission } from "src/app/model/Submission";
 
 @Component({
   selector: "app-excel-results",
@@ -9,21 +9,45 @@ import { Submission } from 'src/app/model/Submission';
   styleUrls: ["./excel-results.component.scss"],
 })
 export class ExcelResultsComponent implements OnInit {
-  dataSource = new MatTableDataSource<ExcelCheckResult>();
-  columns = ["name", "identifier", "errorHint", "table", "succesfulAttempt"];
-  tableViewAsGrid: boolean;
-  index: number;
-  allSubmissions: Submission[];
+  displayedColumns = ["name", "cellName", "errorHint", "table", "result"];
+  dataSource = new MatTableDataSource<ExcelCheckResult>([EXCEL_DATA]);
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  toggleTableView() {
-    this.tableViewAsGrid = !this.tableViewAsGrid;
-  }
+  // toggleTableView() {
+  //   this.tableViewAsGrid = !this.tableViewAsGrid;
+  // }
 
-  selectLast() {
-    setTimeout(() => (this.index = this.allSubmissions.length), 1);
-  }
+  // selectLast() {
+  //   setTimeout(() => (this.index = this.allSubmissions.length), 1);
+  // }
 }
+
+const EXCEL_DATA: ExcelCheckResult = {
+  exercises: [
+    {
+      name: "Aufgabe 1",
+      errorCell: [
+        {
+          cellName: "B1",
+          errorHint: "B1 ist Falsch!",
+          consequentErrorCell: [
+            {
+              cellName: "B2",
+              errorHint: "B2 ist Falsch!",
+            },
+          ],
+        },
+      ],
+      table: "Table1",
+      result: false,
+    },
+    {
+      name: "Aufgabe 2",
+      table: "Table1",
+      result: true,
+    },
+  ],
+};
