4425104068089a347825488cb987b400c753476c
Jonas Wölfer <jonas.woelfer@mnd.thm.de>
2023-05-21 10:39:53 +0200

    Add title for page of analytics tool

--- modules/fbs-core/web/src/app/page-components/analytics-tool/analytics-tool.component.ts
+++ modules/fbs-core/web/src/app/page-components/analytics-tool/analytics-tool.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { TitlebarService } from "../../service/titlebar.service";
 
 @Component({
   selector: "app-analytics-tool",
   templateUrl: "./analytics-tool.component.html",
-  styleUrls: ["./analytics-tool.component.scss"]
+  styleUrls: ["./analytics-tool.component.scss"],
 })
-export class AnalyticsToolComponent {
-
+export class AnalyticsToolComponent implements OnInit {
+  constructor(private titlebar: TitlebarService) {}
+  ngOnInit() {
+    this.titlebar.emitTitle("Analyse Plattform");
+  }
 }
