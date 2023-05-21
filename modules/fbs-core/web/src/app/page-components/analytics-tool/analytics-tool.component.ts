2c709c5bd31690abf1dff5f72edbf648deb25bd2
Jonas Wölfer <jonas.woelfer@mnd.thm.de>
2023-05-21 08:22:58 +0200

    add page for the analytics tool

---
+++ modules/fbs-core/web/src/app/page-components/analytics-tool/analytics-tool.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-tool',
+  templateUrl: './analytics-tool.component.html',
+  styleUrls: ['./analytics-tool.component.css']
+})
+export class AnalyticsToolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
