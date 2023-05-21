c5778e2af4f650b45e41cfdd9441c1aba9fed19a
Jonas Wölfer <jonas.woelfer@mnd.thm.de>
2023-05-21 08:57:03 +0200

    Small fix

--- modules/fbs-core/web/src/app/page-components/analytics-tool/analytics-tool.component.ts
+++ modules/fbs-core/web/src/app/page-components/analytics-tool/analytics-tool.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-analytics-tool',
-  templateUrl: './analytics-tool.component.html',
-  styleUrls: ['./analytics-tool.component.css']
+  selector: "app-analytics-tool",
+  templateUrl: "./analytics-tool.component.html",
+  styleUrls: ["./analytics-tool.component.scss"]
 })
-export class AnalyticsToolComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
+export class AnalyticsToolComponent {
 
 }
