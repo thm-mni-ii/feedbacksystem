a60762cc01890531b572fac46a31aedd8e3c5b5c
Tim Steinmüller <tim.steinmueller@online.de>
2023-05-23 11:29:19 +0200

    small changes

--- modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/services/v2/persistence/MinioServiceTest.kt
+++ modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/services/v2/persistence/MinioServiceTest.kt
@@ -4,56 +4,57 @@ import org.apache.commons.io.FileUtils
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.FileInputStream
 
 class MinioServiceTest {
-    private val minioServiceV2: MinioServiceV2 = MinioServiceV2()
+    lateinit var minioServiceV2: MinioServiceV2
     val str = "Hello World"
+    val filename = "test.txt"
 
     @Before
-    fun initialMinio(): Unit {
+    fun initialMinio() {
+        minioServiceV2 = MinioServiceV2()
         minioServiceV2.initialMinio()
-        val file = File("test.txt")
+        val file = File(filename)
         file.bufferedWriter().use { out ->
             out.write(str)
         }
-        val multipartFile: MultipartFile = MockMultipartFile("test.txt", FileInputStream(file))
+        val multipartFile: MultipartFile = MockMultipartFile(filename, FileInputStream(file))
         minioServiceV2.createBucketIfNotExists("test")
-        minioServiceV2.putObject(multipartFile, "test.txt", "test")
-        file.delete()
+        minioServiceV2.putObject(multipartFile, filename, "test")
     }
 
     @Test
     fun testString() {
-        val obj = minioServiceV2.getObjectAsString("test", "test.txt")
+        val obj = minioServiceV2.getObjectAsString("test", filename)
         assert(obj == str)
     }
 
     @Test
     fun testBytes() {
-        val obj = minioServiceV2.getObjectAsBytes("test", "test.txt")
+        val obj = minioServiceV2.getObjectAsBytes("test", filename)
         assert(obj.contentEquals(str.toByteArray().toTypedArray()))
     }
 
     @Test
     fun testFile() {
-        val file = File("test.txt")
+        val file = File(filename)
         file.bufferedWriter().use { out ->
             out.write(str)
         }
-        val obj = minioServiceV2.getObjectAsFile("test", "test.txt")
+        val obj = minioServiceV2.getObjectAsFile("test", filename)
         // compare content
         assert(FileUtils.contentEquals(obj, file))
         file.delete()
     }
 
     @After
-    fun deleteFile(): Unit {
-        minioServiceV2.deleteObject("test", "test.txt")
+    fun deleteFile() {
+        val file = File(filename)
+        minioServiceV2.deleteObject("test", filename)
+        file.delete()
     }
 }
