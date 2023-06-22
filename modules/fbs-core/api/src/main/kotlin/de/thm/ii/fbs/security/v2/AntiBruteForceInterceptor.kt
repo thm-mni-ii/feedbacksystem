c48355169399a36f2e060a574bffaea5b2ee03b8
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2023-06-22 10:47:39 +0200

    feat(core): add allow list to brute force protection

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/security/v2/AntiBruteForceInterceptor.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/security/v2/AntiBruteForceInterceptor.kt
@@ -1,5 +1,7 @@
 package de.thm.ii.fbs.security.v2
 
+import ch.qos.logback.classic.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
@@ -18,16 +20,21 @@ class AntiBruteForceInterceptor(
     @Value("\${security.antiBruteForce.maxAttempts}")
     private val maxAttempts: Int = 10,
     @Value("\${security.antiBruteForce.protectedPaths}")
-    private val protectedPathsString: String = "/api/v1/login/ldap,/api/v1/login/local,/api/v1/login/unified"
+    private val protectedPathsString: String = "/api/v1/login/ldap,/api/v1/login/local,/api/v1/login/unified",
+    @Value("\${security.antiBruteForce.allowList}")
+    private val allowListString: String = ""
 ) : HandlerInterceptor {
     private data class LoginAttempts(val attempts: Int = 0, val lastAttempt: Date? = null)
 
+    private val logger = LoggerFactory.getLogger(AntiBruteForceInterceptor::class.java)
     private val logins = ConcurrentHashMap<String, LoginAttempts>()
     private val lock = ReentrantLock(true)
     private var lastClean = Date()
 
     private val protectedPaths
         get() = protectedPathsString.split(",")
+    private val allowList
+        get() = allowListString.split(",")
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         super.preHandle(request, response, handler)
@@ -39,8 +46,11 @@ class AntiBruteForceInterceptor(
 
         val forwardForHeader = request.getHeader("X-FORWARDED-FOR") ?: ""
         val proxies = forwardForHeader.split(", ") + request.remoteAddr
-        val filteredProxies = proxies.slice(0 until proxies.size - trustedProxyCount)
+        val filteredProxies = proxies
+            .slice(0 until proxies.size - trustedProxyCount)
+            .filter { !allowList.contains(it) }
         val ip = filteredProxies.last()
+        if (ip.isNullOrBlank()) return true
 
         lock.lock()
         var lastLoginAttempt = logins[ip] ?: LoginAttempts()
@@ -49,6 +59,7 @@ class AntiBruteForceInterceptor(
         }
 
         if (lastLoginAttempt.attempts >= maxAttempts) {
+            logger.warn("Blocked request from $ip: Too Many Requests")
             response.sendError(429, "Too Many Requests")
             lock.unlock()
             return false
