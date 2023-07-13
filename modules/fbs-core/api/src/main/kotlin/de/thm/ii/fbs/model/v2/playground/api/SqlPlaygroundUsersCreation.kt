4b107aa82a9625fed8c33491cb51772f3a3a0455
Max Stephan <max.stephan@mni.thm.de>
2023-07-13 13:02:25 +0200

    refactor(Playground): fix linting errors

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/playground/api/SqlPlaygroundUsersCreation.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/playground/api/SqlPlaygroundUsersCreation.kt
@@ -3,8 +3,8 @@ package de.thm.ii.fbs.model.v2.playground.api
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class SqlPlaygroundUsersCreation(
-        @JsonProperty("userId")
-        var userId: Int,
-        @JsonProperty("dbId")
-        var dbId: Int,
+    @JsonProperty("userId")
+    var userId: Int,
+    @JsonProperty("dbId")
+    var dbId: Int
 )
