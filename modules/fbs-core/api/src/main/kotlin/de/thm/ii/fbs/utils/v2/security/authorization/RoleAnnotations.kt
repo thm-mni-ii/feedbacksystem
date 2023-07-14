e96db1f833af4e119e41370e5ae7f8bf1f6f157a
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-07-14 13:46:51 +0200

    feat(api): adds meta annotations for authorization

---
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/utils/v2/security/authorization/RoleAnnotations.kt
@@ -0,0 +1,17 @@
+package de.thm.ii.fbs.utils.v2.security.authorization
+import org.springframework.security.access.prepost.PreAuthorize
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('ADMIN')")
+annotation class IsAdmin
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('MODERATOR')")
+annotation class IsModerator
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('USER')")
+annotation class IsUser
