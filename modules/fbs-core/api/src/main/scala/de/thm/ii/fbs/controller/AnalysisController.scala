f19259553b810319bc37efa6965f71b552e71a9b
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-07-18 11:41:39 +0200

    feat(api): uses security annotations in the analysis controller

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/controller/AnalysisController.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/controller/AnalysisController.scala
@@ -1,12 +1,10 @@
 package de.thm.ii.fbs.controller
 
-import de.thm.ii.fbs.controller.exception.ForbiddenException
-import de.thm.ii.fbs.model.v2.security.authorization.{CourseRole, GlobalRole}
 import de.thm.ii.fbs.model.AnalysisCourseResult
-import de.thm.ii.fbs.services.persistence.{CourseRegistrationService, CourseResultService, EvaluationContainerService}
-import de.thm.ii.fbs.services.security.AuthService
+import de.thm.ii.fbs.services.persistence.CourseResultService
+import de.thm.ii.fbs.utils.v2.security.authorization.IsModeratorOrCourseTutor
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.{CrossOrigin, GetMapping, PathVariable, RequestMapping, ResponseBody, RestController}
+import org.springframework.web.bind.annotation._
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
@@ -18,32 +16,21 @@ import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 @RequestMapping(path = Array("/api/v1/analysis"))
 class AnalysisController {
   @Autowired
-  private val authService: AuthService = null
-  @Autowired
   private val courseResultService: CourseResultService = null
-  @Autowired
-  private val courseRegistration: CourseRegistrationService = null
 
   /**
-    * Get anonymised results of a course from an task.
-    * @param cid Course id
-    * @param tid Task id
-    * @param req request
-    * @param res response
+    * Get anonymized results of a course from an task.
+    *
+    * @param courseId Course id
+    * @param taskId      Task id
+    * @param req      request
+    * @param res      response
     * @return A list of course results
     */
-  @GetMapping(value = Array("/courses/{cid}/results/{tid}"))
+  @GetMapping(value = Array("/courses/{courseId}/results/{taskId}"))
   @ResponseBody
-  def getCourseResultsByTask(@PathVariable cid: Int, @PathVariable tid: Int, req: HttpServletRequest, res: HttpServletResponse): List[AnalysisCourseResult] = {
-    val user = authService.authorize(req, res)
-
-    val privilegedByCourse = courseRegistration.getParticipants(cid).find(_.getUser.getId == user.getId)
-      .exists(p => p.getRole == CourseRole.DOCENT || p.getRole == CourseRole.TUTOR)
-
-    if (privilegedByCourse || user.getGlobalRole == GlobalRole.ADMIN || user.getGlobalRole == GlobalRole.MODERATOR) {
-      courseResultService.getAllByTask(cid, tid)
-    } else {
-      throw new ForbiddenException()
-    }
-  }
+  @IsModeratorOrCourseTutor
+  def getCourseResultsByTask(@PathVariable courseId: Int, @PathVariable taskId: Int, req: HttpServletRequest, res: HttpServletResponse)
+  : List[AnalysisCourseResult] =
+    courseResultService.getAllByTask(courseId, taskId)
 }
