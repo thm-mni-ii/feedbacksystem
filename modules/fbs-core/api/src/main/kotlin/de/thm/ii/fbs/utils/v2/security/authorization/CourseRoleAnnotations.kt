e6ae32414ce10cbea9f0207bdebdad3230d23553
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2023-07-20 13:58:07 +0200

    feat(api): more permission checks and annotations

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/utils/v2/security/authorization/CourseRoleAnnotations.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/utils/v2/security/authorization/CourseRoleAnnotations.kt
@@ -3,10 +3,12 @@ package de.thm.ii.fbs.utils.v2.security.authorization
 import org.springframework.security.access.prepost.PreAuthorize
 
 /**
- * Annotation to check weather the current user is a moderator (global role)
- * or a docent in the course with the [courseID] of the annotated function.
+ * Annotation to check weather the current user is at least a moderator (global role)
+ * or a docent in the course with the [courseId] of the annotated function.
  *
  * Note: The annotated function needs a parameter `courseId: Int` otherwise an internal server error occurs.
+ *
+ * @see PermissionEvaluator
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
@@ -14,10 +16,25 @@ import org.springframework.security.access.prepost.PreAuthorize
 annotation class IsModeratorOrCourseDocent
 
 /**
- * Annotation to check weather the current user is a moderator (global role)
- * or a tutor in the course with the [courseID] of the annotated function.
+ * Annotation to check weather the current user is  at least a moderator (global role)
+ * or a docent in the course with the [courseId] or the current user has the same id as [userId] of the annotated function.
+ *
+ * Note: The annotated function needs the parameters `courseId: Int` and `userId: Int` otherwise an internal server error occurs.
+ *
+ * @see PermissionEvaluator
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('MODERATOR') || @permissions.hasCourseRole(#courseId, 'DOCENT') || @permissions.isSelf(#userId)")
+annotation class IsModeratorOrCourseDocentOrSelf
+
+/**
+ * Annotation to check weather the current user is at least a moderator (global role)
+ * or  at least a tutor in the course with the [courseId] of the annotated function.
  *
  * Note: The annotated function needs a parameter `courseId: Int` otherwise an internal server error occurs.
+ *
+ * @see PermissionEvaluator
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
@@ -25,12 +42,28 @@ annotation class IsModeratorOrCourseDocent
 annotation class IsModeratorOrCourseTutor
 
 /**
- * Annotation to check weather the current user is a moderator (global role)
- * or a docent in the course with the [courseID] or the current user has the same id as [userId] of the annotated function.
+ * Annotation to check weather the current user is  at least a moderator (global role)
+ * or at least a tutor in the course with the [courseId] or the current user has the same id as [userId] of the annotated function.
  *
  * Note: The annotated function needs the parameters `courseId: Int` and `userId: Int` otherwise an internal server error occurs.
+ *
+ * @see PermissionEvaluator
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("hasRole('MODERATOR') || @permissions.hasCourseRole(#courseId, 'DOCENT') || @permissions.isSelf(#userId)")
-annotation class IsModeratorOrCourseDocentOrSelf
+@PreAuthorize("hasRole('MODERATOR') || @permissions.hasCourseRole(#courseId, 'TUTOR') || @permissions.isSelf(#userId)")
+annotation class IsModeratorOrCourseTutorOrSelf
+
+/**
+ * Annotation to check weather the current user is at least a moderator (global role)
+ * or at least a tutor in the course of the task with the [taskId] of the annotated function.
+ *
+ * Note: The annotated function needs a parameter `taskId: Int` otherwise an internal server error occurs.
+ *
+ * @see PermissionEvaluator
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('MODERATOR') || @permissions.hasCourseRoleOfTask(#taskId, 'TUTOR')")
+annotation class IsModeratorOrCourseTutorOfTask
+
--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/security/PermissionEvaluator.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/security/PermissionEvaluator.scala
@@ -2,46 +2,134 @@ package de.thm.ii.fbs.security
 
 import de.thm.ii.fbs.model.v2.security.authentication.User
 import de.thm.ii.fbs.model.v2.security.authorization.{CourseRole, GlobalRole}
-import de.thm.ii.fbs.services.persistence.CourseRegistrationService
+import de.thm.ii.fbs.services.persistence.{CourseRegistrationService, TaskService}
 import de.thm.ii.fbs.services.v2.security.authentication.UserService
+import jakarta.ws.rs.ForbiddenException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 
+import scala.annotation.unused
 import scala.jdk.CollectionConverters.SeqHasAsJava
 
+/**
+  * A spring component used to check permissions of the currently logged in user.
+  */
 @Component("permissions")
+@unused
 object PermissionEvaluator {
   @Autowired
   private val courseRegistrationService: CourseRegistrationService = null // TODO get course permissions from jwt token
   @Autowired
   private val userService: UserService = null // TODO get user information from jwt token
 
-  def hasCourseRole(courseId: Int, role: String): Boolean = hasCourseRole(courseId, CourseRole.parse(role))
+  @Autowired
+  private val taskService: TaskService = null
 
-
+  /**
+    * Returns true if the current user has at least the `role` in the course with `courseId`, false otherwise.
+    *
+    * @param courseId The course to check the user's role in.
+    * @param role     The role to check on the user.
+    */
+  @unused
   def hasCourseRole(courseId: Int, role: CourseRole): Boolean = {
     val user = getUser
-    val participant = courseRegistrationService.getParticipants(courseId).find(_.getUser.getId == user.getId)
+    val participant = courseRegistrationService.getCourseRoleOfUser(courseId, user.getId)
     participant match {
-      case Some(p) => CourseRole.roleHierarchy().getReachableGrantedAuthorities(List(p.getRole).asJava).contains(role)
+      case Some(userRole) => CourseRole.roleHierarchy().getReachableGrantedAuthorities(List(userRole).asJava).contains(role)
       case None => false
     }
   }
 
-  def subscribed(courseId: Int): Boolean = {
-    val user = getUser
-    courseRegistrationService.getParticipants(courseId).exists(_.getUser.getId == user.getId)
+  /**
+    * Returns true if the current user has at least the `role` in the course with `courseId`, false otherwise.
+    *
+    * @param courseId The course to check the user's role in.
+    * @param role     The role as a string to check on the user.
+    * @see hasCourseRole(Int, CourseRole)
+    */
+  @unused
+  def hasCourseRole(courseId: Int, role: String): Boolean = hasCourseRole(courseId, CourseRole.parse(role))
+
+  /**
+    * Returns true if the current user has at least the `role` in the course of the task with `taskId`, false otherwise.
+    *
+    * @param taskId The task of the course to check the user's role in.
+    * @param role   The role as a string to check on the user.
+    * @see hasCourseRole(Int, CourseRole)
+    */
+  @unused
+  def hasCourseRoleOfTask(taskId: Int, role: String): Boolean = {
+    val task = taskService.getOne(taskId)
+    task match {
+      case Some(task) => hasCourseRole(task.courseID, role)
+      case _ => false
+    }
   }
 
+  /**
+    * Returns true if the current user is docent in any course, false otherwise.
+    */
+  @unused
+  def hasDocentRole: Boolean = {
+    courseRegistrationService.getCoursePrivileges(getUser.getId).exists(privileges => privileges._2 == CourseRole.DOCENT)
+  }
+
+  /**
+    * Returns true if the current user is subscribed to the course with `courseId` (with any role), false otherwise.
+    *
+    * @param courseId The course to check the user's subscription to.
+    */
+  @unused
+  def subscribed(courseId: Int): Boolean = courseRegistrationService.getCourseRoleOfUser(courseId, getUser.getId).isDefined
+
+  /**
+    * Returns true if the current user has the same `userId`, false otherwise.
+    *
+    * @param userId The user id to check with the current user.
+    */
+  @unused
   def isSelf(userId: Int): Boolean = {
     val user = getUser
     userId == user.getId
   }
 
-  private def getUser: User = {
-    val userDetails = SecurityContextHolder.getContext.getAuthentication.getPrincipal.asInstanceOf[UserDetails] // TODO User as UserDetails
+  /**
+    * Get the current logged in user.
+    */
+  @unused
+  def getUser: User = {
+    val userDetails = SecurityContextHolder.getContext.getAuthentication match {
+      case null => throw new ForbiddenException()
+      case authentication: Authentication => authentication.getPrincipal.asInstanceOf[UserDetails] // TODO User as UserDetails
+    }
     userService.find(userDetails.getUsername)
+  }
+
+  /**
+    * Get the current logged in user.
+    */
+  @unused
+  def hasRole(role: GlobalRole): Boolean = {
+    SecurityContextHolder.getContext.getAuthentication.getAuthorities.contains(role)
+  }
+
+  @unused
+  def taskIsPrivate(taskId: Int): Boolean = {
+    taskService.getOne(taskId) match {
+      case Some(task) => task.isPrivate
+      case None => false
+    }
+  }
+
+  @unused
+  def taskHideResults(taskId: Int): Boolean = {
+    taskService.getOne(taskId) match {
+      case Some(task) => task.hideResult
+      case None => false
+    }
   }
 }
