0bb9a349bde827440d267eb241a793d0f3e6636f
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2023-09-06 10:04:21 +0200

    fix(core): prevent mysql caching the RANDOM_BYTES output

--- modules/fbs-core/api/src/main/resources/migrations/18_unique_username.sql
+++ modules/fbs-core/api/src/main/resources/migrations/18_unique_username.sql
@@ -1,7 +1,32 @@
 BEGIN;
 
 -- When multiple users with the same username exist append a random uuid to all expect the first users (this will not break user logins as currently only the first user can login with their username)
-UPDATE user u2 JOIN user u1 ON u1.username = u2.username AND u1.user_id < u2.user_id SET u2.username = CONCAT(u2.username, "-", HEX(RANDOM_BYTES(4)));
+DELIMITER //
+CREATE PROCEDURE MakeUsernamesUnique()
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE a INT;
+  DECLARE cur CURSOR FOR SELECT user_id FROM user;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+OPEN cur;
+
+read_loop: LOOP
+    FETCH cur INTO a;
+    IF done THEN
+      LEAVE read_loop;
+END IF;
+
+UPDATE user SET username = CONCAT(username, "-", HEX(RANDOM_BYTES(4))) WHERE user_id = a;
+END LOOP;
+
+CLOSE cur;
+END;
+//
+DELIMITER ;
+
+CALL MakeUsernamesUnique();
+DROP PROCEDURE MakeUsernamesUnique;
 
 ALTER TABLE user ADD UNIQUE (username);
 
