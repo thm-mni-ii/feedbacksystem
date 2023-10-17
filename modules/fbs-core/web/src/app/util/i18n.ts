aa6f0acdc8abf9aae213cce70590ff5fe37bc12d
Jonas Kuche <jonas-ian.kuche@mni.thm.de>
2023-10-17 13:27:53 +0200

    feat(i18n): add complete english and german i18n

--- modules/fbs-core/web/src/app/util/i18n.ts
+++ modules/fbs-core/web/src/app/util/i18n.ts
@@ -1,34 +1,29 @@
-import {I18NEXT_SERVICE, ITranslationService} from "angular-i18next";
-import {APP_INITIALIZER, LOCALE_ID} from "@angular/core";
-import {germanTranslation} from "../../i18n/de";
-import {englishTranslation} from "../../i18n/en";
-import LanguageDetector from 'i18next-browser-languagedetector';
+import { I18NEXT_SERVICE, ITranslationService } from "angular-i18next";
+import { APP_INITIALIZER, LOCALE_ID } from "@angular/core";
+import { germanTranslation } from "../../i18n/de";
+import { englishTranslation } from "../../i18n/en";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 export function appInit(i18next: ITranslationService) {
-  return () => i18next
-    .use(LanguageDetector)
-    .init({
-      supportedLngs: ['en', 'de'],
-      fallbackLng: 'en',
+  return () =>
+    i18next.use(LanguageDetector).init({
+      supportedLngs: ["en", "de"],
+      fallbackLng: "de",
       debug: true,
       returnEmptyString: false,
-      ns: [
-        'translation',
-        'validation',
-        'error'
-      ],
+      ns: ["translation", "validation", "error"],
       resources: {
         en: {
-          translation: englishTranslation
+          translation: englishTranslation,
         },
         de: {
-          translation: germanTranslation
+          translation: germanTranslation,
         },
-      }
-  });
+      },
+    });
 }
 
-export function localeIdFactory(i18next: ITranslationService)  {
+export function localeIdFactory(i18next: ITranslationService) {
   return i18next.language;
 }
 
@@ -37,10 +32,11 @@ export const I18N_PROVIDERS = [
     provide: APP_INITIALIZER,
     useFactory: appInit,
     deps: [I18NEXT_SERVICE],
-    multi: true
+    multi: true,
   },
   {
     provide: LOCALE_ID,
     deps: [I18NEXT_SERVICE],
-    useFactory: localeIdFactory
-  }];
+    useFactory: localeIdFactory,
+  },
+];
