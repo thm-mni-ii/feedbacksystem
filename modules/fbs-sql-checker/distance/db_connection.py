a6f22abd92e43eef227495d6a921a2ab5f7eae02
Khaled <tr.khaled@gmail.com>
2024-01-06 03:58:17 +0100

    code refactor

--- modules/fbs-sql-checker/distance/db_connection.py
+++ modules/fbs-sql-checker/distance/db_connection.py
@@ -13,26 +13,9 @@ def setup_db(att_list):
             port=c.PORT_ID
     ) as connection:
         cursor = connection.cursor()
-
-        for i, att in enumerate(att_list):
-            sql_script = f"CREATE TABLE IF NOT EXISTS {chr(65 + i)} (x INTEGER, CONSTRAINT {chr(97 + i)}_unique_x UNIQUE (x));"
-            cursor.execute(sql_script)
-
-        bits = len(att_list)
-        for i in range(2 ** bits):
-            binary_number = format(i, f'0{bits}b')
-            decimal_number = int(binary_number, 2)
-            offset = -1
-            for j in range(len(binary_number)):
-                if binary_number[offset] == '1':
-                    select_query = f"SELECT * FROM {chr(65 + j)} WHERE x = {decimal_number};"
-                    cursor.execute(select_query)
-                    result = cursor.fetchone()
-
-                    if result is None:
-                        insert_query = f"INSERT INTO {chr(65 + j)} (x) VALUES ({decimal_number});"
-                        cursor.execute(insert_query)
-                offset -= 1
+        print(type(cursor))
+        _create_db(att_list, cursor)
+        _populate_db(att_list, cursor)
         connection.commit()
     return connection
 
@@ -44,3 +27,26 @@ def execute_query(query, connection: psycopg2):
     res_set = set(frozenset(s) for s in result)
     connection.commit()
     return res_set
+
+
+def _create_db(att_list, cursor):
+    for i, att in enumerate(att_list):
+        sql_script = f"CREATE TABLE IF NOT EXISTS {chr(65 + i)} (x INTEGER, CONSTRAINT {chr(97 + i)}_unique_x UNIQUE (x));"
+        cursor.execute(sql_script)
+
+
+def _populate_db(att_list, cursor):
+    bits = len(att_list)
+    for i in range(2 ** bits):
+        binary_number = format(i, f'0{bits}b')
+        decimal_number = int(binary_number, 2)
+        offset = -1
+        for j in range(len(binary_number)):
+            if binary_number[offset] == '1':
+                select_query = f"SELECT * FROM {chr(65 + j)} WHERE x = {decimal_number};"
+                cursor.execute(select_query)
+                result = cursor.fetchone()
+                if result is None:
+                    insert_query = f"INSERT INTO {chr(65 + j)} (x) VALUES ({decimal_number});"
+                    cursor.execute(insert_query)
+            offset -= 1
