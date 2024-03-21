f7986f56c637b488116dc3a480c578a5b333430e
Johannes Kunz <monoxotix@gmail.com>
2024-03-21 13:43:34 +0100

    Check for icons

--- modules/fbs-core/mf_web/vite-spa/src/App.vue
+++ modules/fbs-core/mf_web/vite-spa/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <v-container>
     <v-card>
-      <v-card-title> Vuetify + Vue 3 + Vite + TypeScript </v-card-title>
+      <v-card-title>
+        Vuetify <v-icon> mdi-vuetify </v-icon>
+        + Vue 3 <v-icon> mdi-vuejs </v-icon>
+        + Vite <v-icon> mdi-one-up </v-icon>
+        + TypeScript <v-icon> mdi-language-typescript </v-icon>
+      </v-card-title>
       <v-card-text>Ich bin ein Text</v-card-text>
       <v-card-actions>
         <v-btn color="primary">Primary</v-btn>
