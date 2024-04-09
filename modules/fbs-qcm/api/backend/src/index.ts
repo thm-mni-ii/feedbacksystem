f4d0da97608a08de831f725f42bd31b0a5a18bbf
jhws42 <jann.wissbach@mni.thm.de>
2024-04-09 11:18:47 +0200

    fixed Issue with Docker Container not starting

--- modules/fbs-qcm/api/backend/src/index.ts
+++ modules/fbs-qcm/api/backend/src/index.ts
@@ -3,7 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.send("Wasfafafafhsfrshshsssssssssss-------------------sadsdagasWWW");
+    return res.send("Wassassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssfafafafhsfrshshsssssssssss-------------------sadsdagasWWW");
+});
+app.get("/test", (req, res) => {
+    return res.send("this is test");
 });
 
-app.listen(3000, () => console.log("LISTENING"));
+app.listen(3000, () => console.log("LISTENING on port 3000"));
