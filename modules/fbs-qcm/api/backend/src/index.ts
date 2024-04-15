1a405e993d46950619cfc7f325e2ac698f06a7b7
jhws42 <jann.wissbach@mni.thm.de>
2024-04-15 19:41:17 +0200

    added new mongodb to docker file

--- modules/fbs-qcm/api/backend/src/index.ts
+++ modules/fbs-qcm/api/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
@@ -6,7 +7,30 @@ app.get("/", (req, res) => {
     return res.send("Wassassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssfafafafhsfrshshsssssssssss-------------------sadsdagasWWW");
 });
 app.get("/test", (req, res) => {
-    return res.send("this is test");
+       return connect()
+
 });
+
+async function connect() {
+    const uri = 'mongodb://mongodb:27017';
+    const client = new MongoClient(uri)
+     try {
+        // Connect to MongoDB
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        // Perform operations
+        // Example: List all databases
+        const databasesList = await client.db().admin().listDatabases();
+        console.log('Databases:');
+        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    } finally {
+        // Close the connection
+        await client.close();
+        return "AAA";
+    }
+}
 
 app.listen(3000, () => console.log("LISTENING on port 3000"));
