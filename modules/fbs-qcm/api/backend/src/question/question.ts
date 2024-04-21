26d178168480b351f524a4593497f1d224c8ff2b
jhws42 <jann.wissbach@web.de>
2024-04-21 17:17:07 +0200

    added prototypes for new Routes

--- modules/fbs-qcm/api/backend/src/question/question.ts
+++ modules/fbs-qcm/api/backend/src/question/question.ts
@@ -2,10 +2,31 @@ import { connect } from "../mongo/mongo";
 import * as mongoDB from "mongodb";
 
 export async function getQuestionById(questionId: string) {
-    console.log("creating connection to MOngoDB");
     const query  = {_id:new mongoDB.ObjectId(questionId)};
     const database: mongoDB.Db = await connect();
     const collection: mongoDB.Collection = database.collection("question");
     const data = await collection.findOne(query);
     return data;
+}
+
+export async function deleteQuestion(questionId: string) {
+    const query  = {_id:new mongoDB.ObjectId(questionId)};
+    const database: mongoDB.Db = await connect();
+    const collection: mongoDB.Collection = database.collection("question");
+    const data = await collection.deleteOne(query);
+    return data;
+}
+
+export async function putQuestion(questionId: string, data: JSON) {
+    const database: mongoDB.Db = await connect();
+    const collection: mongoDB.Collection = database.collection("question");
+    await collection.updateOne({_id: new mongoDB.ObjectId(questionId)}, data); 
+    return 0;
+}
+
+export async function postQuestion(data: JSON) {
+    const database: mongoDB.Db = await connect();
+    const collection: mongoDB.Collection = database.collection("question");
+    const response = collection.insertOne(data);
+    return response;
 }
