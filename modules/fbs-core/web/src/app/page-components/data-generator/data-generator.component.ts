1bd393083cc7e846a725db11e175b1581d21cbf1
Jann Wißbach <jann.wissbach@web.de>
2024-06-28 21:14:22 +0200

    fixed small error

--- modules/fbs-core/web/src/app/page-components/data-generator/data-generator.component.ts
+++ modules/fbs-core/web/src/app/page-components/data-generator/data-generator.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-data-generator',
   templateUrl: './data-generator.component.html',
   styleUrls: ['./data-generator.component.scss']
 })
-export class DataGeneratorComponent implements OnInit {
+export class DataGeneratorComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
