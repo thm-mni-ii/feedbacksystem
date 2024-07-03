4acd8c03d9f0dc6fcf08f520695517c014259ccb
Sophie Methe <sophie.methe@freenet.de>
2024-07-03 10:55:17 +0200

    Add new page-components (#1675)

---
+++ modules/fbs-core/web/src/app/page-components/course-detail/group-selection/group-selection.component.ts
@@ -0,0 +1,89 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { AuthService } from "../../../service/auth.service";
+import { Observable } from "rxjs";
+import { Requirement } from "../../../model/Requirement";
+import { MatDialog } from "@angular/material/dialog";
+import { Roles } from "../../../model/Roles";
+import { NewGroupDialogComponent } from "../../../dialogs/new-group-dialog/new-group-dialog.component";
+import { Group } from "../../../model/Group";
+import { GroupService } from "../../../service/group.service";
+import { GroupRegistrationService } from "../../../service/group-registration.sevice";
+
+@Component({
+  selector: "app-group-selection",
+  templateUrl: "./group-selection.component.html",
+  styleUrls: ["./group-selection.component.scss"],
+})
+export class GroupSelectionComponent implements OnInit {
+  @Input() requirements: Observable<Requirement[]>;
+
+  groups: Observable<Group[]>;
+  selectedGroup: Group;
+  courseId: number;
+  editGroups: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private auth: AuthService,
+    private dialog: MatDialog,
+    private groupService: GroupService,
+    private groupRegistrationService: GroupRegistrationService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((param) => {
+      this.courseId = param.id;
+    });
+    this.loadGroups();
+  }
+
+  public isAuthorized(ignoreTutor: boolean = false) {
+    const token = this.auth.getToken();
+    const courseRole = token.courseRoles[this.courseId];
+    const globalRole = token.globalRole;
+    return (
+      Roles.GlobalRole.isAdmin(globalRole) ||
+      Roles.GlobalRole.isModerator(globalRole) ||
+      Roles.CourseRole.isDocent(courseRole) ||
+      (Roles.CourseRole.isTutor(courseRole) && !ignoreTutor)
+    );
+  }
+
+  loadGroups(): void {
+    this.groups = this.groupService.getGroupList(this.courseId);
+  }
+
+  createGroup() {
+    this.dialog
+      .open(NewGroupDialogComponent, {
+        data: { cid: this.courseId },
+        height: "auto",
+        width: "50%",
+      })
+      .afterClosed()
+      .subscribe(
+        () => {
+          this.loadGroups();
+        },
+        (error) => console.error(error)
+      );
+  }
+
+  joinGroup(): void {
+    if (this.selectedGroup) {
+      this.groupRegistrationService
+        .registerGroup(
+          this.courseId,
+          this.selectedGroup.id,
+          this.auth.getToken().id
+        )
+        .subscribe(
+          () => {
+            this.loadGroups();
+          },
+          (error) => console.error(error)
+        );
+    } else console.error("No group selected");
+  }
+}
---
+++ modules/fbs-core/web/src/app/page-components/group-detail/group-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-group-detail",
+  templateUrl: "./group-detail.component.html",
+  styleUrls: ["./group-detail.component.scss"],
+})
+export class GroupDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
