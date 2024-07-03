199f09d01842aad55aec339f94187fcd38c56d08
Sophie Methe <sophie.methe@freenet.de>
2024-07-03 10:50:11 +0200

    Update app-routing, app.module, and i18n files (#1675)

--- modules/fbs-core/web/src/app/app-routing.module.ts
+++ modules/fbs-core/web/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ChangePasswordComponent } from "./page-components/change-password/chang
 import { MyCoursesComponent } from "./page-components/my-courses/my-courses.component";
 import { SearchCoursesComponent } from "./page-components/search-courses/search-courses.component";
 import { CourseDetailComponent } from "./page-components/course-detail/course-detail.component";
+import { MyGroupsComponent } from "./page-components/my-groups/my-groups.component";
 import { UserManagementComponent } from "./page-components/user-management/user-management.component";
 import { NotFoundComponent } from "./page-components/not-found/not-found.component";
 import { LoginComponent } from "./page-components/login/login.component";
@@ -20,6 +21,7 @@ import { SqlCheckerResultsComponent } from "./page-components/sql-checker/sql-ch
 import { SqlPlaygroundComponent } from "./page-components/sql-playground/sql-playground.component";
 import { AnalyticsToolComponent } from "./page-components/analytics-tool/analytics-tool.component";
 import { FbsModellingComponent } from "./page-components/fbs-modelling/fbs-modelling.component";
+import { GroupDetailComponent } from "./page-components/group-detail/group-detail.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
@@ -71,6 +73,19 @@ const routes: Routes = [
       {
         path: "courses/:id/sql-checker-results/:tid",
         component: SqlCheckerResultsComponent,
+        canActivate: [AuthGuard],
+      },
+
+      //groups
+      {
+        path: "groups",
+        component: MyGroupsComponent,
+        canActivate: [AuthGuard],
+      },
+
+      {
+        path: "groups/:courseId/:id",
+        component: GroupDetailComponent,
         canActivate: [AuthGuard],
       },
 
--- modules/fbs-core/web/src/app/app.module.ts
+++ modules/fbs-core/web/src/app/app.module.ts
@@ -117,6 +117,11 @@ import { LanguageMenuComponent } from "./page-components/sidebar/language-menu/l
 import { registerLocaleData } from "@angular/common";
 import localeDe from "@angular/common/locales/de";
 import localeDeExtra from "@angular/common/locales/extra/de";
+import { MyGroupsComponent } from "./page-components/my-groups/my-groups.component";
+import { GroupSelectionComponent } from "./page-components/course-detail/group-selection/group-selection.component";
+import { NewGroupDialogComponent } from "./dialogs/new-group-dialog/new-group-dialog.component";
+import { GroupPreviewComponent } from "./page-components/group-preview/group-preview.component";
+import { GroupDetailComponent } from "./page-components/group-detail/group-detail.component";
 
 @Injectable()
 export class ApiURIHttpInterceptor implements HttpInterceptor {
@@ -217,6 +222,12 @@ export const httpInterceptorProviders = [
     SharePlaygroundLinkDialogComponent,
     FbsModellingComponent,
     LanguageMenuComponent,
+    MyGroupsComponent,
+    GroupSelectionComponent,
+    NewGroupDialogComponent,
+    GroupPreviewComponent,
+    GroupPreviewComponent,
+    GroupDetailComponent,
   ],
   imports: [
     BrowserModule,
