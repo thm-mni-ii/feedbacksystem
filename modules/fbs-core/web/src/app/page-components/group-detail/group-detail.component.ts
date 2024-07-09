9dd95e46d53ed7dea69926572bf863ae16fb456a
Sophie Methe <sophie.methe@freenet.de>
2024-07-09 10:54:45 +0200

    Update group-detail component (#1675)

--- modules/fbs-core/web/src/app/page-components/group-detail/group-detail.component.ts
+++ modules/fbs-core/web/src/app/page-components/group-detail/group-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Group } from "../../model/group";
+import { GroupService } from "../../service/group.service";
+import { ActivatedRoute } from "@angular/router";
+import { TitlebarService } from "../../service/titlebar.service";
 
 @Component({
   selector: "app-group-detail",
@@ -6,7 +11,31 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./group-detail.component.scss"],
 })
 export class GroupDetailComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private groupService: GroupService,
+    private route: ActivatedRoute,
+    private titlebar: TitlebarService
+  ) {}
+  courseID: number;
+  groupID: number;
+  group$: Observable<Group> = of();
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((param) => {
+      this.courseID = param.courseId;
+      this.groupID = param.id;
+      this.loadGroup();
+    });
+  }
+
+  loadGroup(): void {
+    this.group$ = this.groupService.getGroup(this.courseID, this.groupID);
+    this.group$.subscribe((group) => {
+      this.titlebar.emitTitle(group.name);
+    });
+  }
+
+  navigateToKanban() {
+    window.location.href = "http://localhost:3000/";
+  }
 }
