fceeb49b9a22f95fab269fba648b1220e5de2704
jhws42 <jann.wissbach@mni.thm.de>
2024-08-12 16:38:57 +0200

    started overhaul of question evaluation

--- modules/fbs-qcm/api/backend/src/submission/submission.ts
+++ modules/fbs-qcm/api/backend/src/submission/submission.ts
@@ -3,6 +3,9 @@ import { checkQuestionAccess, getCurrentSession, getUserCourseRoles} from "../ut
 import { connect } from "../mongo/mongo";
 import { AnswerScore } from "../utils/enum";
 import * as mongoDB from "mongodb";
+import { Question } from "../model/Question";
+import QuestionType from "../enums/QuestionType";
+import FillInTheBlanks from "../model/questionTypes/FillInTheBlanks";
 
 export async function submitSessionAnswer(tokenData: JwtPayload, requestData: any) {
     const session = getCurrentSession(tokenData.user);
@@ -24,7 +27,7 @@ export async function submit(tokenData: JwtPayload, requestData: any) {
     if(catalog === false) {
         return -1;
     }
-    const correct = await checkAnswer(requestData.answer, questionId, questionCollection);
+    const correct = await checkAnswer(requestData, questionId, questionCollection);
     const submission = {
         user: tokenData.id,
         question: questionId,
@@ -41,25 +44,35 @@ async function checkAnswer(answer: any[], questionId: mongoDB.ObjectId,
     const questionQuery = {
         _id: questionId
     }
-    const question = await questionCollection.findOne(questionQuery);
-    const correct = checkSubmission(answer, question); 
-    return correct;
+    const result: any  = await questionCollection.findOne(questionQuery);
+    if(result === null) {
+        return -1;
+    }
+    if(result !== null) {
+        const question: Question = result as Question;
+        const correct = checkSubmission(answer, question); 
+        return correct;
+    }
 }
 
-function checkSubmission(answer: any[], question: any) {
+function checkSubmission(answer: any[], question: Question) {
     const questionType = question.questiontype;
-    if(questionType == "Single-Choice") {
-        return checkSingleChoice(answer[0], question);
-    } else if(questionType == "Multiple-Choice") {
-        return checkMultipleChoice(answer, question);
-    } else if(questionType == "Clozetext") {
-        return checkClozeText(answer, question);
+    if(questionType == QuestionType.Choice) {
+        return checkChoice(answer[0], question);
+    } else if(questionType == QuestionType.FillInTheBlanks) {
+        return checkClozeText(answer, question as FillInTheBlanks);
+    } else if(questionType == QuestionType.SQL) {
+        return checkSQL(answer, question);
     } else {
         return AnswerScore.incorrect;
     }
 }
 
-function checkSingleChoice(answer: any, question: any) {
+function checkSQL(answer: any, question: Question) {
+    return 0;
+}
+
+function checkChoice(answer: any, question: any) {
     for(let i = 0; i < question.answers.length; i++) {
         if(question.answers[i].text == answer) {
             if( question.answers[i].isCorrect) {
@@ -94,24 +107,31 @@ function checkMultipleChoice(answer: any[], question: any) {
     }
 }
 
-function checkClozeText(answer: any[], question: any) {
-    let correctInQuestion = 0;
-    let correctInAnswer = 0;
-    for(let i = 0;i < question.answers.length; i++) {
-        if(question.answers[i].isCorrect) {
-            correctInQuestion++;
+function checkClozeText(answer: any[], question: FillInTheBlanks) {
+    let blankFields = [];
+    let results = [];
+    let numberOfCorrectAnswers = 0;
+    for(let i = 0; i < question.textParts.length; i++) {
+        if(question.textParts[i].isBlank === true) {
+            blankFields.push(question.textParts[i]);
         }
     }
-    for(let i = 0;i < answer.length; i++) {
-        for(let j = 0; j < question.answers.length; j++) {
-            if(question.answers[j].text == answer[i] && question.answers[j].position === i + 1) {
-                correctInAnswer++;
-            }
+    for(let j = 0; j < blankFields.length; j++) {
+        const res = checkSingleWord(answer, blankFields[j]);
+        if(res) {
+            numberOfCorrectAnswers++;
+        }
+        results.push(res);
+    }
+    console.log(results);
+    console.log(numberOfCorrectAnswers);
+}
+
+function checkSingleWord(answer: any[], blankFields: any) {
+    for(let k = 0; k < answer.length; k++) {
+        if(answer[k].text === blankFields.text) {
+            return true;
         }
     }
-    if(correctInAnswer === correctInQuestion) {
-        return AnswerScore.correct;
-    } else {
-        return AnswerScore.incorrect;
-    }
+    return false;
 }
