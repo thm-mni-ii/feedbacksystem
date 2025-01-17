d6a233e78500765f92033a4fed52632a479e6479
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2025-01-17 07:20:39 +0100

    fix(playground): repair trigger display

--- modules/fbs-collab/src/app.ts
+++ modules/fbs-collab/src/app.ts
@@ -6,19 +6,24 @@ const FBS_ROOT_URL = process.env.FBS_ROOT_URL ?? 'https://feedback.mni.thm.de';
 const server = new Hocuspocus({
   port: 1234,
   async onAuthenticate(data: onAuthenticatePayload): Promise<any> {
-    let userId;
     try {
-      userId = jose.decodeJwt(data.token).id;
+      let userId;
+      try {
+        userId = jose.decodeJwt(data.token).id;
+      } catch (e) {
+        throw new Error("Unauthorized");
+      }
+      if (!userId) throw new Error("Unauthorized");
+      const resp = await fetch(`${FBS_ROOT_URL}/api/v1/users/${userId}/groups`, {headers: {'Authorization': `Bearer ${data.token}`}});
+      if (resp.status !== 200) throw new Error("Unauthorized");
+      const body = await resp.json();
+      const ok = Boolean(body.find(({id}: { id: string }) => id.toString() === data.documentName));
+      if (!ok) throw new Error("Forbidden");
+      return {user: {id: userId}};
     } catch (e) {
-      throw new Error("Unauthorized");
+      console.log(e)
+      throw e
     }
-    if (!userId) throw new Error("Unauthorized");
-    const resp = await fetch(`${FBS_ROOT_URL}/api/v1/users/${userId}/groups`, {headers: {'Authorization': `Bearer ${data.token}`}});
-    if (resp.status !== 200) throw new Error("Unauthorized");
-    const body = await resp.json();
-    const ok = Boolean(body.find(({id}: {id: string}) => id.toString() === data.documentName));
-    if (!ok) throw new Error("Forbidden");
-    return {user: {id: userId}};
   },
 });
 
