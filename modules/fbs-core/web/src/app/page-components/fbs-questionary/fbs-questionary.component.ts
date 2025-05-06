de6f769f5a348453f99554a3bd668ff05df9f412
Jonas Kuche <Zitrone44@users.noreply.github.com>
2025-05-06 13:58:20 +0200

    chore(cqm): update fbs-questionary.component.ts to production url

--- modules/fbs-core/web/src/app/page-components/fbs-questionary/fbs-questionary.component.ts
+++ modules/fbs-core/web/src/app/page-components/fbs-questionary/fbs-questionary.component.ts
@@ -25,7 +25,7 @@ export class FbsQuestionaryComponent implements OnInit {
   }
 
   getURL(): SafeResourceUrl {
-    const url = `http://localhost:8086?jsessionid=${this.token}`;
+    const url = `https://feedback.mni.thm.de/questionary?jsessionid=${this.token}`;
     this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
     this.cdr.detach(); // stops iframe from reloading
     return this.safeUrl;
