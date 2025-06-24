9946ed53429f26f5fbc3dfaba726a1c4b8faad6c
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2025-06-24 13:50:22 +0200

    fix(core): resolve errors in PdfCheckerService

--- modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/checker/PdfCheckerService.scala
+++ modules/fbs-core/api/src/main/scala/de/thm/ii/fbs/services/checker/PdfCheckerService.scala
@@ -5,15 +5,16 @@ import de.thm.ii.fbs.model.{CheckrunnerConfiguration, User}
 import de.thm.ii.fbs.services.checker.`trait`.{CheckerService, CheckerServiceOnMainFileUpload}
 import de.thm.ii.fbs.services.persistence.TaskService
 import de.thm.ii.fbs.services.persistence.storage.MinioStorageService
+import de.thm.ii.fbs.util.RestTemplateFactory
 import org.json.JSONObject
 import org.springframework.beans.factory.annotation.{Autowired, Value}
+import org.springframework.http.{HttpEntity, HttpHeaders}
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
 @Service
 class PdfCheckerService extends CheckerService with CheckerServiceOnMainFileUpload {
-  @Autowired
-  private val restTemplate: RestTemplate = null
+  private val restTemplate: RestTemplate = RestTemplateFactory.makeRestTemplate(false)
   @Autowired
   private val taskService: TaskService = null
   @Autowired
@@ -25,21 +26,25 @@ class PdfCheckerService extends CheckerService with CheckerServiceOnMainFileUplo
   override def notify(taskID: Int, submissionID: Int, cc: CheckrunnerConfiguration, fu: User): Unit = {
     val task = taskService.getOne(taskID).get
     sendRequest("/submission/", new JSONObject()
-      .put("submission_id", submissionID)
-      .put("student_id", fu.id)
-      .put("course_id", task.courseID)
-      .put("task_id", task.id)
+      .put("submission_id", submissionID.toString)
+      .put("student_id", fu.id.toString)
+      .put("course_id", task.courseID.toString)
+      .put("task_id", task.id.toString)
       .put("abgabe", storageService.getSolutionFileFromBucket(submissionID)))
   }
 
   override def onCheckerMainFileUpload(cid: Int, task: Task, checkerConfiguration: CheckrunnerConfiguration): Unit = {
     sendRequest("/submission/solution", new JSONObject()
-      .put("course_id", cid)
-      .put("task_id", task.id)
+      .put("course_id", cid.toString)
+      .put("task_id", task.id.toString)
       .put("abgabe", storageService.getMainFileFromBucket(checkerConfiguration.id)))
   }
 
   private def sendRequest(urlSuffix: String, body: JSONObject): JSONObject = {
-    new JSONObject(restTemplate.postForEntity(baseUrl + urlSuffix, body.toString, classOf[String]))
+    val headers = new HttpHeaders()
+    headers.set("Content-Type", "application/json")
+    new JSONObject(
+      restTemplate.postForEntity(baseUrl + urlSuffix, new HttpEntity(body.toString, headers), classOf[String])
+    )
   }
 }
