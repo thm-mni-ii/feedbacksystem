3f0067fa4b111b54507fb8d90ec027afa74db3a0
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2025-07-30 14:56:06 +0200

    fix(playground): revert mvc configuration changes that broke scala api

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/playground/api/MongoIndexDTO.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/playground/api/MongoIndexDTO.kt
@@ -1,6 +1,10 @@
 package de.thm.ii.fbs.model.v2.playground.api
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class MongoIndexDTO(
+    @JsonProperty("collection")
     val collection: String,
+    @JsonProperty("index")
     val index: Map<String, Int>
 )
--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/playground/api/MongoViewDTO.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/model/v2/playground/api/MongoViewDTO.kt
@@ -1,9 +1,13 @@
 package de.thm.ii.fbs.model.v2.playground.api
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.bson.Document
 
 data class MongoViewDTO(
+    @JsonProperty("viewName")
     val viewName: String,
+    @JsonProperty("collectionSource")
     val collectionSource: String,
+    @JsonProperty("pipeline")
     val pipeline: List<Document>
 )
