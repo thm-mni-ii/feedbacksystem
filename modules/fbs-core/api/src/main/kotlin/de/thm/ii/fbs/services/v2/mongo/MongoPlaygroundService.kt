7d828b0221a2ba9ed8e7d853cf00e42a03b194e0
Jonas Kuche <Zitrone44@users.noreply.github.com>
2025-08-21 09:55:28 +0200

    fix(playground): add auth source configuration (#1769)
    * fix(playground): add auth source configuration
    
    * chore(playground): fix linting errors

--- modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/mongo/MongoPlaygroundService.kt
+++ modules/fbs-core/api/src/main/kotlin/de/thm/ii/fbs/services/v2/mongo/MongoPlaygroundService.kt
@@ -9,18 +9,36 @@ import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
 import org.springframework.stereotype.Service
+import java.net.URI
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 @Service
 class MongoPlaygroundService {
     @Value("\${spring.data.mongodb.playground.uri}")
     private lateinit var mongoUri: String
 
+    @Value("\${spring.data.mongodb.playground.authSource}")
+    private lateinit var authSource: String
+
     fun createMongoTemplate(currentToken: LegacyToken, dbId: String): MongoTemplate {
-        val fullUri = "$mongoUri/$dbId"
+        val fullUri = buildMongoUri(mongoUri, dbId, authSource)
         val factory: MongoDatabaseFactory = SimpleMongoClientDatabaseFactory(ConnectionString(fullUri))
 
         return MongoTemplate(factory)
     }
 
     fun getMongoClient(): MongoClient = MongoClients.create(mongoUri)
+
+    private fun buildMongoUri(url: String, useDbName: String, authDbName: String): String {
+        val uri = URI(url)
+
+        return URI(
+            uri.scheme,
+            uri.authority,
+            "/$useDbName",
+            "authSource=${URLEncoder.encode(authDbName, StandardCharsets.UTF_8.toString())}",
+            uri.fragment
+        ).toString()
+    }
 }
