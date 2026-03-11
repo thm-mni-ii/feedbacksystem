73c8339e9f1e4212b2687599921a1adc0245c318
Maxime Haas <maxime.haas@viacesi.fr>
2026-03-11 11:08:32 +0100

    upgrade Angular cli + core to version 18 and refactor HttpClient import in AppModule

--- modules/sql-playground/web/src/app/app.module.ts
+++ modules/sql-playground/web/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { I18NextModule } from 'angular-i18next';
@@ -17,21 +17,14 @@ export function tokenGetter(): string | null {
   return localStorage.getItem('token');
 }
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    MaterialComponentsModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-    I18NextModule.forRoot(),
-    JwtModule.forRoot({ config: { tokenGetter } }),
-    SqlPlaygroundModule,
-  ],
-  providers: [...I18N_PROVIDERS],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MaterialComponentsModule,
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+        I18NextModule.forRoot(),
+        JwtModule.forRoot({ config: { tokenGetter } }),
+        SqlPlaygroundModule], providers: [...I18N_PROVIDERS, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
