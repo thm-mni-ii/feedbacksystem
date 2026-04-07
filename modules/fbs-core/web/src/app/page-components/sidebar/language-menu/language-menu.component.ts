f1554e044efa57780afdf7b0a52ae910a3353d39
zainelabdin alzoubi <alzoubi.zainelabdin@gmail.com>
2026-04-07 14:32:30 +0200

    i1775-core-automaticaly-remove-inactive-user-after-configurable-time V2

--- modules/fbs-core/web/src/app/page-components/sidebar/language-menu/language-menu.component.ts
+++ modules/fbs-core/web/src/app/page-components/sidebar/language-menu/language-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, Inject, ViewChild } from "@angular/core";
+import { Component, Inject, ViewChild } from "@angular/core";
+import { MatMenu } from "@angular/material/menu";
 import { I18NEXT_SERVICE, ITranslationService } from "angular-i18next";
 
 /**
@@ -11,8 +12,8 @@ import { I18NEXT_SERVICE, ITranslationService } from "angular-i18next";
   exportAs: "languageMenu",
 })
 export class LanguageMenuComponent {
-  @ViewChild("menu")
-  menu: ElementRef;
+  @ViewChild("menu", { static: true })
+  menu!: MatMenu;
   constructor(
     @Inject(I18NEXT_SERVICE) private i18NextService: ITranslationService
   ) {}
