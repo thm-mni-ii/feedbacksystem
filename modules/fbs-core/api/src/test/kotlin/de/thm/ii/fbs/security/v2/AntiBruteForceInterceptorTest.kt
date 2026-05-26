e1d228f485b7c969efa8153af749f88726d7536c
zainelabdin alzoubi <alzoubi.zainelabdin@gmail.com>
2026-05-26 15:59:43 +0200

    fix: avoid concurrent modification during anti brute force cleanup

--- modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/security/v2/AntiBruteForceInterceptorTest.kt
+++ modules/fbs-core/api/src/test/kotlin/de/thm/ii/fbs/security/v2/AntiBruteForceInterceptorTest.kt
@@ -1,5 +1,6 @@
 package de.thm.ii.fbs.security.v2
 
+import java.util.Date
 import org.junit.Assert
 import org.junit.Test
 
@@ -31,5 +32,35 @@ class AntiBruteForceInterceptorTest {
         Assert.assertEquals("10.44.0.1", abfi.getRealIp(listOf("10.44.0.1", "10.44.0.2")))
         Assert.assertEquals("10.44.0.2", abfi.getRealIp(listOf("10.44.0.1", "10.44.0.2", "10.44.0.3")))
         Assert.assertEquals("10.44.0.1", abfi.getRealIp(listOf("10.44.0.1", "10.44.0.4", "10.44.0.3")))
+    }
+
+    @Test
+    fun cleanIfNeededHandlesMultipleExpiredLogins() {
+        val abfi = AntiBruteForceInterceptor(interval = 1)
+        val expiredDate = Date(System.currentTimeMillis() - 2000)
+        val loginAttemptsClass = Class.forName("${AntiBruteForceInterceptor::class.java.name}\$LoginAttempts")
+        val loginAttemptsConstructor = loginAttemptsClass.getDeclaredConstructor(
+            Int::class.javaPrimitiveType!!,
+            Date::class.java
+        )
+        loginAttemptsConstructor.isAccessible = true
+        val expiredLoginAttempts = loginAttemptsConstructor.newInstance(1, expiredDate)
+
+        val loginsField = AntiBruteForceInterceptor::class.java.getDeclaredField("logins")
+        loginsField.isAccessible = true
+        @Suppress("UNCHECKED_CAST")
+        val logins = loginsField.get(abfi) as MutableMap<String, Any>
+        logins["10.44.0.1"] = expiredLoginAttempts
+        logins["10.44.0.2"] = expiredLoginAttempts
+
+        val lastCleanField = AntiBruteForceInterceptor::class.java.getDeclaredField("lastClean")
+        lastCleanField.isAccessible = true
+        lastCleanField.set(abfi, expiredDate)
+
+        val cleanIfNeeded = AntiBruteForceInterceptor::class.java.getDeclaredMethod("cleanIfNeeded")
+        cleanIfNeeded.isAccessible = true
+        cleanIfNeeded.invoke(abfi)
+
+        Assert.assertTrue(logins.isEmpty())
     }
 }
