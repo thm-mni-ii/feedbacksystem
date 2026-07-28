82b7b17e85fddd0241fa1bd4cf0f3e7c08d5d706
Dein Name <deine@emailadresse.com>
2026-07-29 00:05:13 +0200

    add brute force protection for local oidc login

---
+++ modules/fbs-identity-service/api/src/main/kotlin/de/thm/ii/fbs/fbs_identity_service/security/config/SchedulingConfig.kt
@@ -0,0 +1,8 @@
+package de.thm.ii.fbs.fbs_identity_service.security.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig
