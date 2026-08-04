a4e1acc6c2a897fc49f471da7dfa42549117db47
Dein Name <deine@emailadresse.com>
2026-08-04 21:09:36 +0200

    Remove duplicated legacy files from merged identity service

--- modules/fbs-identity-service/api/src/main/kotlin/de/thm/ii/fbs/fbs_identity_service/model/auth/SamlUser.kt
+++
@@ -1,8 +0,0 @@
-package de.thm.ii.fbs.fbs_identity_service.model.auth
-
-data class SamlUser(
-    val username: String,
-    val prename: String,
-    val surname: String,
-    val email: String
-)
--- modules/fbs-identity-service/api/src/main/kotlin/de/thm/ii/fbs/fbs_identity_service/service/UserSearchResult.kt
+++
@@ -1,8 +0,0 @@
-package de.thm.ii.fbs.fbs_identity_service.service
-
-import de.thm.ii.fbs.fbs_identity_service.model.User
-
-data class UserSearchResult(
-    val items: List<User>,
-    val totalCount: Int
-)
