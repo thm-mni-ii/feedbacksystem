1a403a777fc7066bd928f383a69c3c531c25fcf2
Niklas_Euler <Zitrone44@users.noreply.github.com>
2026-08-13 10:32:12 +0200

    Externalize OIDC client and issuer configuration (#1856)

--- modules/fbs-identity-service/api/src/main/kotlin/de/thm/ii/fbs/fbs_identity_service/security/oidc/AuthServerSettingsConfig.kt
+++ modules/fbs-identity-service/api/src/main/kotlin/de/thm/ii/fbs/fbs_identity_service/security/oidc/AuthServerSettingsConfig.kt
@@ -1,16 +1,20 @@
 package de.thm.ii.fbs.fbs_identity_service.security.oidc
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
 
 @Configuration
-class AuthServerSettingsConfig {
+class AuthServerSettingsConfig(
+    @param:Value("\${security.oidc.issuer}")
+    private val issuer: String
+) {
 
     @Bean
     fun authorizationServerSettings(): AuthorizationServerSettings {
         return AuthorizationServerSettings.builder()
-            .issuer("http://localhost:8080")
+            .issuer(issuer)
             .build()
     }
 }
